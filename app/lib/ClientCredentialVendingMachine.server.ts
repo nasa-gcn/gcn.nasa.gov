35355f29afe60b866b10865309dc4778c4ab9375
Leo Singer <leo.singer@ligo.org>
2022-01-11 23:58:19 -0500

    Minor refactoring, more parallelism

--- app/lib/ClientCredentialVendingMachine.server.ts
+++ app/lib/ClientCredentialVendingMachine.server.ts
@@ -15,39 +15,6 @@ const cognitoIdentityProviderClient = new CognitoIdentityProviderClient({
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-async function cognitoCreateUserPoolClient(sub: string) {
-  if (isProduction) {
-    const command = new CreateUserPoolClientCommand({
-      AllowedOAuthFlows: ['client_credentials'],
-      AllowedOAuthFlowsUserPoolClient: true,
-      AllowedOAuthScopes: ['gcn-tokens/kafka-consumer'],
-      ClientName: sub,
-      GenerateSecret: true,
-      UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    })
-    const response = await cognitoIdentityProviderClient.send(command)
-    const client_id = response.UserPoolClient?.ClientId
-    const client_secret = response.UserPoolClient?.ClientSecret
-    if (!client_id) throw new Error('AWS SDK must return ClientId')
-    if (!client_secret) throw new Error('AWS SDK must return ClientSecret')
-    return { client_id, client_secret }
-  } else {
-    const client_id = generate({ length: 26 })
-    const client_secret = generate({ length: 51 })
-    return { client_id, client_secret }
-  }
-}
-
-async function cognitoDeleteUserPooolClient(client_id: string) {
-  if (isProduction) {
-    const command = new DeleteUserPoolClientCommand({
-      ClientId: client_id,
-      UserPoolId: process.env.COGNITO_USER_POOL_ID,
-    })
-    await cognitoIdentityProviderClient.send(command)
-  }
-}
-
 export class ClientCredentialVendingMachine {
   #sub: string
 
@@ -79,24 +46,57 @@ export class ClientCredentialVendingMachine {
 
     if (cred?.sub !== this.#sub) throw new Response(null, { status: 403 })
 
-    await cognitoDeleteUserPooolClient(client_id)
-
-    await db.clientCredential.delete({
-      where: { client_id },
-    })
+    await Promise.all([
+      this.#deleteClientCredentialInternal(client_id),
+      db.clientCredential.delete({
+        where: { client_id },
+      }),
+    ])
   }
 
   async createClientCredential(name?: string) {
     if (!name) throw new Response('name must not be empty', { status: 400 })
 
-    const { client_id, client_secret } = await cognitoCreateUserPoolClient(
-      this.#sub
-    )
+    const { client_id, client_secret } =
+      await this.#createClientCredentialInternal()
 
     await db.clientCredential.create({
       data: { name, client_id, sub: this.#sub },
     })
 
     return { name, client_id, client_secret }
+  }
+
+  async #createClientCredentialInternal() {
+    if (isProduction) {
+      const command = new CreateUserPoolClientCommand({
+        AllowedOAuthFlows: ['client_credentials'],
+        AllowedOAuthFlowsUserPoolClient: true,
+        AllowedOAuthScopes: ['gcn-tokens/kafka-consumer'],
+        ClientName: this.#sub,
+        GenerateSecret: true,
+        UserPoolId: process.env.COGNITO_USER_POOL_ID,
+      })
+      const response = await cognitoIdentityProviderClient.send(command)
+      const client_id = response.UserPoolClient?.ClientId
+      const client_secret = response.UserPoolClient?.ClientSecret
+      if (!client_id) throw new Error('AWS SDK must return ClientId')
+      if (!client_secret) throw new Error('AWS SDK must return ClientSecret')
+      return { client_id, client_secret }
+    } else {
+      const client_id = generate({ length: 26 })
+      const client_secret = generate({ length: 51 })
+      return { client_id, client_secret }
+    }
+  }
+
+  async #deleteClientCredentialInternal(client_id: string) {
+    if (isProduction) {
+      const command = new DeleteUserPoolClientCommand({
+        ClientId: client_id,
+        UserPoolId: process.env.COGNITO_USER_POOL_ID,
+      })
+      await cognitoIdentityProviderClient.send(command)
+    }
   }
 }
--- app/routes/api/client_credentials/index.ts
+++ app/routes/api/client_credentials/index.ts
@@ -10,7 +10,9 @@ export const loader: LoaderFunction = async ({ request }) => {
 export const action: ActionFunction = async ({ request }) => {
   if (request.method != 'POST') throw new Response(null, { status: 405 })
 
-  const machine = await ClientCredentialVendingMachine.create(request)
-  const body = await request.json()
+  const [machine, body] = await Promise.all([
+    ClientCredentialVendingMachine.create(request),
+    request.json(),
+  ])
   return await machine.createClientCredential(body.name)
 }
