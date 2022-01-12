122146223e302fa3965c2b908a64c04cf56a0603
Leo Singer <leo.singer@ligo.org>
2022-01-11 21:38:06 -0500

    Remove unnecessary command line argument

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,md,mdx,ts,tsx}': 'eslint --cache --fix',
-  '*.{ts,tsx}': () => 'tsc --noEmit -p .',
+  '*.{ts,tsx}': () => 'tsc -p .',
   '*.{css,js,json,jsx,md,mdx,ts,tsx}': 'prettier --write',
 }
