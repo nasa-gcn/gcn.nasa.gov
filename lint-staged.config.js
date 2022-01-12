dbc903a32169ead55be282d4edde9e1676e3f3cb
Leo Singer <leo.singer@ligo.org>
2022-01-11 21:45:35 -0500

    Add GitHub Actions workflow

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,jsx,md,mdx,ts,tsx}': 'eslint --cache --fix',
+  '*.{js,jsx,mdx,ts,tsx}': 'eslint --cache --fix --max-warnings 0',
   '*.{ts,tsx}': () => 'tsc -p .',
   '*.{css,js,json,jsx,md,mdx,ts,tsx}': 'prettier --write',
 }
