aa3b5b103fcabf8e4a44ca0984a55b9f3d93166c
Leo Singer <leo.singer@ligo.org>
2022-01-13 02:55:47 -0500

    Temporarily disable eslint for mdx files
    See mdx-js/eslint-mdx#367.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  '*.{js,jsx,mdx,ts,tsx}': 'eslint --cache --fix --max-warnings 0',
+  // Note, this rule should include *.mdx, but mdx linting is broken.
+  // See https://github.com/mdx-js/eslint-mdx/issues/367.
+  '*.{js,jsx,ts,tsx}': 'eslint --cache --fix --max-warnings 0',
   '*.{ts,tsx}': () => 'tsc -p .',
   '*.{css,js,json,jsx,md,mdx,ts,tsx}': 'prettier --write',
 }
