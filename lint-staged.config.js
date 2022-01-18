65497becce9c12357c32755d9b1c8da508a31074
Leo Singer <leo.singer@ligo.org>
2022-01-18 16:07:06 -0500

    Pre-commit hook uses separate functions to evaluate ignore for prettier and eslint# Please enter the commit message for your changes. Lines starting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
-const ESLint = require('eslint').ESLint
-
 // Adapted from https://github.com/okonet/lint-staged#how-can-i-ignore-files-from-eslintignore
-async function removeIgnoredFiles(files) {
+async function eslintRemoveIgnoredFiles(files) {
+  const ESLint = require('eslint').ESLint
   const eslint = new ESLint()
   const isIgnored = await Promise.all(
     files.map((file) => {
@@ -12,12 +11,25 @@ async function removeIgnoredFiles(files) {
   return filteredFiles.join(' ')
 }
 
+// Adapted from https://github.com/okonet/lint-staged#how-can-i-ignore-files-from-eslintignore
+function prettierRemoveIgnoredFiles(files) {
+  const getFileInfo = require('prettier').getFileInfo
+  const filteredFiles = files.filter((file) => !getFileInfo(file).isIgnored)
+  return filteredFiles.join(' ')
+}
+
 module.exports = {
   // Note, this rule should include *.mdx, but mdx linting is broken.
   // See https://github.com/mdx-js/eslint-mdx/issues/367.
-  '*.{js,jsx,ts,tsx}': async (files) =>
-    `eslint --cache --fix --max-warnings 0 ${await removeIgnoredFiles(files)}`,
+  '*.{js,jsx,ts,tsx}': async (files) => {
+    const filteredFiles = await eslintRemoveIgnoredFiles(files)
+    if (!files) return []
+    return `eslint --cache --fix --max-warnings 0 ${filteredFiles}`
+  },
   '*.{ts,tsx}': () => 'tsc -p .',
-  '*.{css,js,json,jsx,md,mdx,ts,tsx}': async (files) =>
-    `prettier --write ${await removeIgnoredFiles(files)}`,
+  '*.{css,js,json,jsx,md,mdx,ts,tsx}': async (files) => {
+    const filteredFiles = prettierRemoveIgnoredFiles(files)
+    if (!files) return []
+    return `prettier --write ${filteredFiles}`
+  },
 }
