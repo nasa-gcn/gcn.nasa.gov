ce81f01ca5aeb19d095df4fae7ad1791a00e4b80
Leo Singer <leo.singer@ligo.org>
2022-01-18 15:15:37 -0500

    Pre-commit hook ignores files in .eslintignore

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,23 @@
+const ESLint = require('eslint').ESLint
+
+// Adapted from https://github.com/okonet/lint-staged#how-can-i-ignore-files-from-eslintignore
+async function removeIgnoredFiles(files) {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    })
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
+
 module.exports = {
   // Note, this rule should include *.mdx, but mdx linting is broken.
   // See https://github.com/mdx-js/eslint-mdx/issues/367.
-  '*.{js,jsx,ts,tsx}': 'eslint --cache --fix --max-warnings 0',
+  '*.{js,jsx,ts,tsx}': async (files) =>
+    `eslint --cache --fix --max-warnings 0 ${await removeIgnoredFiles(files)}`,
   '*.{ts,tsx}': () => 'tsc -p .',
-  '*.{css,js,json,jsx,md,mdx,ts,tsx}': 'prettier --write',
+  '*.{css,js,json,jsx,md,mdx,ts,tsx}': async (files) =>
+    `prettier --write ${await removeIgnoredFiles(files)}`,
 }
