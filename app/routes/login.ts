6c477999280f981319b5197187587ddad0eed0a8
Leo Singer <leo.singer@ligo.org>
2022-02-09 20:47:11 -0500

    Remove unnecessary default function

--- app/routes/login.ts
+++ app/routes/login.ts
@@ -1,9 +1,6 @@
 import type { LoaderFunction } from 'remix'
 import { login } from '~/lib/auth.server'
 export const loader: LoaderFunction = ({ request }) => login(request)
-export default function () {
-  return null
-}
 
 // FIXME: workaround for https://github.com/remix-run/remix/issues/1828.
 // Resource routes (without browser code) don't get server pruning done.
