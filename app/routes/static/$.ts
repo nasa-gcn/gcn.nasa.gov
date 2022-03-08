315b6ae621e213d95f42a710a5a46b3256f486d5
Leo Singer <leo.singer@ligo.org>
2022-03-08 09:47:45 -0500

    Add license

--- app/routes/static/$.ts
+++ app/routes/static/$.ts
@@ -1,3 +1,11 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
 /* Proxy requests to S3 bucket.
  *
  * This is a quick and dirty workaround because Mission Cloud Platform does not
--- src/macros/permissionsBoundary.js
+++ src/macros/permissionsBoundary.js
@@ -1,3 +1,11 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
 // Add required permissions boundary for working on the Mission Cloud Platform
 module.exports = function permissionsBoundary(arc, sam) {
   sam.Resources.Role.Properties.PermissionsBoundary = {
