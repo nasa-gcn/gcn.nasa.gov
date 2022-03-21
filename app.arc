786199c350af8b172d55f666a81740150808ecd4
Leo Singer <leo.singer@ligo.org>
2022-03-21 15:11:48 -0400

    Use https URL for static bucket proxy

--- app.arc
+++ app.arc
@@ -24,6 +24,7 @@ architecture arm64
 memory 256
 
 @macros
+httpsBucketUrl  # Use https URL for static bucket proxy
 lambdaCognitoPermissions  # Grant the Lambda function access to Cognito to run the credential vending machine.
 lambdaMayNotWriteToStaticBucket  # the Lambda function should not be able to modify the static bucket
 manualStaticBucketPolicy  # Mission Cloud Platform requires bucket policies to be set manually
---
+++ src/macros/httpsBucketUrl.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+// Use https URL for static bucket proxy
+module.exports = function httpsBucketUrl(arc, sam) {
+  sam.Resources.HTTP.Properties.DefinitionBody.paths['/_static/{proxy+}'].get[
+    'x-amazon-apigateway-integration'
+  ].uri['Fn::Sub'][0] = sam.Resources.HTTP.Properties.DefinitionBody.paths[
+    '/_static/{proxy+}'
+  ].get['x-amazon-apigateway-integration'].uri['Fn::Sub'][0].replace(
+    'http://',
+    'https://'
+  )
+  return sam
+}
