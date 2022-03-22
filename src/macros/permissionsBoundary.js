5db473eba5ba0a4b3f93640e8b957975469265fb
Leo Singer <leo.singer@ligo.org>
2022-03-22 11:11:23 -0400

    Update deps, update remix to 1.3.2

--- src/macros/permissionsBoundary.js
+++ src/macros/permissionsBoundary.js
@@ -9,6 +9,7 @@
 // Add required permissions boundary for working on the Mission Cloud Platform
 module.exports = function permissionsBoundary(arc, sam) {
   sam.Resources.Role.Properties.PermissionsBoundary = {
+    // eslint-disable-next-line no-template-curly-in-string
     'Fn::Sub': 'arn:aws:iam::${AWS::AccountId}:policy/mcp-tenantOperator',
   }
   return sam
