5d683f714a497a124763ff28c1a87ea6b11f6dbe
jracusin <judith.racusin@gmail.com>
2022-05-06 11:49:25 -0400

    Add Missions page, just Fermi and Swift filled in for now (#63)
    * mission page structure and Fermi as a template
    
    * adding swift missions page
    
    * fixed images to work with public/img
    
    * added other GCN missions to missions.tsx
    
    * trying to fix failed build on missions/index.ts
    
    * Gehrels rather than Gehrel's on missions.tsx

---
+++ app/routes/missions.tsx
@@ -0,0 +1,68 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+import { SideNav } from '@trussworks/react-uswds'
+import { NavLink, Outlet } from 'remix'
+
+export default function Docs() {
+  return (
+    <div className="grid-row grid-gap">
+      <div className="desktop:grid-col-4">
+        <SideNav
+          items={[
+            <NavLink key="gcnmissions" to="gcnmissions">
+              Missions/Instruments/Facilities
+            </NavLink>,
+            <NavLink key="fermi" to="fermi">
+              Fermi Gamma-ray Space Telescope
+            </NavLink>,
+            <NavLink key="swift" to="swift">
+              Neil Gehrels Swift Observatory
+            </NavLink>,
+            <NavLink key="lvk" to="lvk">
+              LIGO/Virgo/KAGRA
+            </NavLink>,
+            <NavLink key="icecube" to="icecube">
+              IceCube
+            </NavLink>,
+            <NavLink key="hawc" to="hawc">
+              HAWC
+            </NavLink>,
+            <NavLink key="calet" to="calet">
+              CALET
+            </NavLink>,
+            <NavLink key="maxi" to="maxi">
+              MAXI
+            </NavLink>,
+            <NavLink key="integral" to="integral">
+              INTEGRAL
+            </NavLink>,
+            <NavLink key="agile" to="agile">
+              AGILE
+            </NavLink>,
+            <NavLink key="konus" to="konus">
+              Wind-KONUS
+            </NavLink>,
+            <NavLink key="moa" to="moa">
+              MOA
+            </NavLink>,
+            <NavLink key="snews" to="snews">
+              SNEWS
+            </NavLink>,
+            <NavLink key="sksn" to="sksn">
+              SKSN
+            </NavLink>,
+          ]}
+        />
+      </div>
+      <div className="desktop:grid-col-8 usa-prose">
+        <Outlet />
+      </div>
+    </div>
+  )
+}
---
+++ app/routes/missions/gcnmissions.md
@@ -0,0 +1,8 @@
+---
+meta:
+  title: GCN - Missions/Instruments/Facilities
+---
+
+# Missions/Instruments/Facilities
+
+Observatories that generate GCN notices for transient alerts, as consumed by the science community. Explore mission resources and GCN notice details in left menu.
