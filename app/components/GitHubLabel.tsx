c9553ad39b7ff66ee93c68442a5f3b70a11e80d7
Leo Singer <leo.singer@ligo.org>
2022-06-01 14:58:26 -0400

    Invert GitHub label foreground color for readability

--- app/components/GitHubLabel.tsx
+++ app/components/GitHubLabel.tsx
@@ -1,3 +1,4 @@
+import { hex } from 'color-convert'
 import { Octokit } from 'octokit'
 import { useEffect, useRef } from 'react'
 
@@ -14,14 +15,21 @@ export function GitHubLabel({
 }) {
   const ref = useRef<HTMLAnchorElement>(null)
 
+  async function setColor() {
+    if (!ref.current) return
+
+    const {
+      data: { color },
+    } = await octokit.rest.issues.getLabel({ owner, repo, name })
+
+    const inverse = hex.gray(color)[0] < 60
+
+    ref.current.style.backgroundColor = `#${color}`
+    ref.current.style.color = inverse ? 'white' : 'black'
+  }
+
   useEffect(() => {
-    ;(async () => {
-      if (!ref.current) return
-      const {
-        data: { color },
-      } = await octokit.rest.issues.getLabel({ owner, repo, name })
-      ref.current.style.backgroundColor = `#${color}`
-    })()
+    setColor()
   })
 
   const url = new URL(`https://github.com/${owner}/${repo}/issues`)
@@ -31,7 +39,7 @@ export function GitHubLabel({
     <a
       href={url.toString()}
       ref={ref}
-      className="text-white text-semibold font-body-3xs padding-x-1 radius-pill"
+      className="text-semibold font-body-3xs padding-x-1 radius-pill"
       style={{
         backgroundColor: 'gray',
         textDecoration: 'none',
