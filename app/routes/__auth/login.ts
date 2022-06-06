4bf02f855ba2885790c94906dd3bff6b6f4b517c
Leo Singer <leo.singer@ligo.org>
2022-06-06 12:19:25 -0400

    WIP: Add sandbox, no-op OIDC implementation for local development
    Replace this with an architect plugin that implements an IdP using
     https://github.com/panva/node-oidc-provider.

--- app/routes/__auth/login.ts
+++ app/routes/__auth/login.ts
@@ -8,99 +8,22 @@
 
 import type { LoaderFunction } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
-import type { TokenSet } from 'openid-client'
-import { generators } from 'openid-client'
-import { getOpenIDClient, oidcStorage, storage } from './auth.server'
+import { storage } from './auth.server'
 import type { getUser } from './user.server'
 
-function userFromTokenSet(
-  tokenSet: TokenSet
-): NonNullable<Awaited<ReturnType<typeof getUser>>> {
-  const claims = tokenSet.claims()
-  const sub = claims.sub
-  const email = claims.email as string
+export const loader: LoaderFunction = async () => {
+  const session = await storage.getSession()
 
-  const idp =
-    claims.identities instanceof Array && claims.identities.length > 0
-      ? (claims.identities[0].providerName as string)
-      : null
-
-  const groups = ((claims['cognito:groups'] ?? []) as string[]).filter(
-    (group) => group.startsWith('gcn.nasa.gov')
-  )
-
-  return { sub, email, groups, idp }
-}
-
-export const loader: LoaderFunction = async ({ request: { headers, url } }) => {
-  const parsedUrl = new URL(url)
-  if (!parsedUrl.searchParams.get('code')) {
-    const [client, { cookie, ...props }] = await Promise.all([
-      getOpenIDClient(),
-      (async () => {
-        const oidcSessionPromise = oidcStorage.getSession()
-
-        const nonce = generators.nonce()
-        const state = generators.state()
-        const code_verifier = generators.codeVerifier()
-        const code_challenge = generators.codeChallenge(code_verifier)
-
-        const oidcSession = await oidcSessionPromise
-        oidcSession.set('code_verifier', code_verifier)
-        oidcSession.set('nonce', nonce)
-        oidcSession.set('state', state)
-
-        const cookie = await oidcStorage.commitSession(oidcSession)
-
-        return { cookie, nonce, state, code_challenge }
-      })(),
-    ])
-
-    const authorizationUrl = client.authorizationUrl({
-      scope: 'openid',
-      code_challenge_method: 'S256',
-      redirect_uri: url,
-      ...props,
-    })
-
-    return redirect(authorizationUrl, { headers: { 'Set-Cookie': cookie } })
-  } else {
-    const sessionPromise = await storage.getSession()
-    const [client, { oidcSessionDestroyPromise, ...checks }] =
-      await Promise.all([
-        getOpenIDClient(),
-        (async () => {
-          const cookie = headers.get('Cookie')
-          const oidcSession = await oidcStorage.getSession(cookie)
-          const code_verifier = oidcSession.get('code_verifier')
-          const nonce = oidcSession.get('nonce')
-          const state = oidcSession.get('state')
-          if (!code_verifier || !nonce || !state)
-            throw new Response(
-              'Your session timed out. Please try logging in again.',
-              { status: 400 }
-            )
-          const oidcSessionDestroyPromise =
-            oidcStorage.destroySession(oidcSession)
-          return { oidcSessionDestroyPromise, code_verifier, nonce, state }
-        })(),
-      ])
-
-    const params = client.callbackParams(parsedUrl.search)
-    parsedUrl.search = ''
-    const tokenSet = await client.callback(parsedUrl.toString(), params, checks)
-
-    const user = userFromTokenSet(tokenSet)
-    const session = await sessionPromise
-    Object.entries(user).forEach(([key, value]) => {
-      session.set(key, value)
-    })
-
-    const [cookie] = await Promise.all([
-      storage.commitSession(session),
-      oidcSessionDestroyPromise,
-    ])
-
-    return redirect('/user', { headers: { 'Set-Cookie': cookie } })
+  const user: NonNullable<Awaited<ReturnType<typeof getUser>>> = {
+    sub: 'albert.einstein@example.edu',
+    email: 'albert.einstein@example.edu',
+    idp: null,
+    groups: ['gcn.nasa.gov/kafka-public-consumer'],
   }
+  Object.entries(user).forEach(([key, value]) => {
+    session.set(key, value)
+  })
+
+  const cookie = await storage.commitSession(session)
+  return redirect('/user', { headers: { 'Set-Cookie': cookie } })
 }
--- app/routes/__auth/logout.ts
+++ app/routes/__auth/logout.ts
@@ -8,30 +8,13 @@
 
 import type { LoaderFunction } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
-import { getOpenIDClient, storage } from './auth.server'
+import { storage } from './auth.server'
 
-export const loader: LoaderFunction = async ({ request: { headers, url } }) => {
-  const [client, cookie] = await Promise.all([
-    getOpenIDClient(),
-    (async () => {
-      const session = await storage.getSession(headers.get('Cookie'))
-      return await storage.destroySession(session)
-    })(),
-  ])
+export const loader: LoaderFunction = async ({ request: { headers } }) => {
+  const session = await storage.getSession(headers.get('Cookie'))
+  const cookie = await storage.destroySession(session)
 
-  // Determine the Cognito logout URI endpoint
-  const auth_endpoint = client.issuer.metadata.authorization_endpoint
-  if (!auth_endpoint)
-    throw new Error(
-      'client.issuer.metadata.authorization_endpoint must be present'
-    )
-  const logoutUrl = new URL(auth_endpoint)
-  logoutUrl.pathname = '/logout'
-
-  logoutUrl.searchParams.set('client_id', client.metadata.client_id)
-  logoutUrl.searchParams.set('logout_uri', new URL(url).origin)
-
-  return redirect(logoutUrl.toString(), {
+  return redirect('/', {
     headers: {
       'Set-Cookie': cookie,
     },
