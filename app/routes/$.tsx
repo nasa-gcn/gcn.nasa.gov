c03b78a4e5cd890eb785adfe5bd3de84e8e1da88
Leo Singer <leo.singer@ligo.org>
2022-06-09 12:20:20 -0400

    Remove '404'

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -9,7 +9,7 @@ export const loader: LoaderFunction = function () {
 export function CatchBoundary() {
   return (
     <>
-      <h1>Error 404: Page not found</h1>
+      <h1>Page not found</h1>
       <p className="usa-intro">
         We're sorry, we can't find the page you're looking for. It might have
         been removed, changed its name, or is otherwise unavailable.
