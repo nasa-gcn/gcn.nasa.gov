5995d5b908343fe31a64a3e737a79fae2d59ce7a
Leo Singer <leo.singer@ligo.org>
2022-06-30 15:53:25 -0400

    Add license headers

--- app/components/GitHubLabel.tsx
+++ app/components/GitHubLabel.tsx
@@ -1,3 +1,11 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
 import { hex } from 'color-convert'
 import { Octokit } from 'octokit'
 import { useEffect, useRef } from 'react'
