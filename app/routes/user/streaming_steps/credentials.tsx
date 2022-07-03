9d0a2b8d3aedc752e967050845c6bb39f92c679b
Leo Singer <leo.singer@ligo.org>
2022-07-03 11:08:21 -0400

    Quick start: add button to delete credential

--- app/routes/user/streaming_steps/credentials.tsx
+++ app/routes/user/streaming_steps/credentials.tsx
@@ -6,20 +6,27 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 
+import type { ModalRef } from '@trussworks/react-uswds'
 import {
   Button,
   Dropdown,
   Grid,
   Label,
+  Modal,
+  ModalFooter,
+  ModalHeading,
+  ModalToggleButton,
   TextInput,
 } from '@trussworks/react-uswds'
 import { Form, Link, useLoaderData } from '@remix-run/react'
+import type { RedactedClientCredential } from '../client_credentials.server'
 import { ClientCredentialVendingMachine } from '../client_credentials.server'
 import type { DataFunctionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 import moment from 'moment'
 import type { ReactNode } from 'react'
-import { useState } from 'react'
+import { useRef } from 'react'
+import React, { useState } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
 import { getEnvOrDieInProduction } from '~/lib/env'
 
@@ -46,18 +53,38 @@ async function verifyRecaptcha(response: string) {
   if (!success) throw new Response('ReCAPTCHA was invalid', { status: 400 })
 }
 
+function getFormDataString(formData: FormData, key: string) {
+  const value = formData.get(key)
+  if (typeof value !== 'string')
+    throw new Response(`expected ${key} to be a string`, { status: 400 })
+  return value
+}
+
 export async function action({ request }: DataFunctionArgs) {
-  const {
-    name,
-    scope,
-    'g-recaptcha-response': recaptchaResponse,
-  } = Object.fromEntries(await request.formData()) as { [k: string]: string }
-  await verifyRecaptcha(recaptchaResponse)
-  const machine = await ClientCredentialVendingMachine.create(request)
-  const { client_id } = await machine.createClientCredential(name, scope)
-  return redirect(
-    `/user/streaming_steps/alerts?clientId=${encodeURIComponent(client_id)}`
-  )
+  const [data, machine] = await Promise.all([
+    request.formData(),
+    ClientCredentialVendingMachine.create(request),
+  ])
+
+  switch (getFormDataString(data, 'intent')) {
+    case 'create':
+      const name = getFormDataString(data, 'name')
+      const scope = getFormDataString(data, 'scope')
+      const recaptchaResponse = getFormDataString(data, 'g-recaptcha-response')
+      await verifyRecaptcha(recaptchaResponse)
+      const { client_id } = await machine.createClientCredential(name, scope)
+      return redirect(
+        `/user/streaming_steps/alerts?clientId=${encodeURIComponent(client_id)}`
+      )
+
+    case 'delete':
+      const clientId = getFormDataString(data, 'clientId')
+      await machine.deleteClientCredential(clientId)
+      return null
+
+    default:
+      throw new Response('unknown intent', { status: 400 })
+  }
 }
 
 function SegmentedCard({ children }: { children: ReactNode[] }) {
@@ -87,6 +114,7 @@ export function NewCredentialForm() {
 
   return (
     <Form method="post">
+      <input type="hidden" name="intent" value="create" />
       <div className="usa-prose">
         <p>Choose a name for your new client credential.</p>
         <p className="text-base">
@@ -143,6 +171,75 @@ export function NewCredentialForm() {
   )
 }
 
+function Credential({ name, client_id, created }: RedactedClientCredential) {
+  const ref = useRef<ModalRef>(null)
+  return (
+    <>
+      <Grid row>
+        <div className="grid-col flex-fill">
+          <div>
+            <strong>{name}</strong>{' '}
+            <small className="text-base">
+              (created {moment.utc(created).fromNow()})
+            </small>
+          </div>
+          <div>
+            <small>
+              client ID: <code>{client_id}</code>
+            </small>
+          </div>
+        </div>
+        <div className="grid-col flex-auto">
+          <ModalToggleButton
+            opener
+            modalRef={ref}
+            type="button"
+            className="usa-button--secondary"
+          >
+            Delete
+          </ModalToggleButton>
+          <Link
+            className="usa-button"
+            to={`../alerts?clientId=${encodeURIComponent(client_id)}`}
+          >
+            Select
+          </Link>
+        </div>
+      </Grid>
+      <Modal
+        id="modal-delete"
+        ref={ref}
+        aria-labelledby="modal-delete-heading"
+        aria-describedby="modal-delete-description"
+        renderToPortal={false} // FIXME: https://github.com/trussworks/react-uswds/pull/1890#issuecomment-1023730448
+      >
+        <Form method="post">
+          <input type="hidden" name="intent" value="delete" />
+          <input type="hidden" name="clientId" value={client_id} />
+          <ModalHeading id="modal-delete-heading">
+            Delete Client Credential
+          </ModalHeading>
+          <div className="usa-prose">
+            <p id="modal-delete-description">
+              Are you sure that you want to delete the client credential named “
+              {name}” with client ID <code>{client_id}</code>?
+            </p>
+            <p>This action cannot be undone.</p>
+          </div>
+          <ModalFooter>
+            <ModalToggleButton modalRef={ref} closer outline>
+              Cancel
+            </ModalToggleButton>
+            <Button data-close-modal type="submit">
+              Delete
+            </Button>
+          </ModalFooter>
+        </Form>
+      </Modal>
+    </>
+  )
+}
+
 export default function Credentials() {
   const { client_credentials } =
     useLoaderData<Awaited<ReturnType<typeof loader>>>()
@@ -162,31 +259,8 @@ export default function Credentials() {
             create a new one.
           </p>
           <SegmentedCard>
-            {client_credentials.map(({ name, client_id, created }) => (
-              <Link
-                className="text-no-underline text-ink"
-                key={client_id}
-                to={`../alerts?clientId=${encodeURIComponent(client_id)}`}
-              >
-                <Grid row>
-                  <div className="grid-col flex-fill">
-                    <div>
-                      <strong>{name}</strong>{' '}
-                      <small className="text-base">
-                        (created {moment.utc(created).fromNow()})
-                      </small>
-                    </div>
-                    <div>
-                      <small>
-                        client ID: <code>{client_id}</code>
-                      </small>
-                    </div>
-                  </div>
-                  <div className="grid-col flex-auto">
-                    <Button type="button">Select</Button>
-                  </div>
-                </Grid>
-              </Link>
+            {client_credentials.map((credential) => (
+              <Credential key={credential.client_id} {...credential} />
             ))}
           </SegmentedCard>
           <div className="padding-2" key="new">
