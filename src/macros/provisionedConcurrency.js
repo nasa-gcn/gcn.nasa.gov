bdd15973b1b77c6693595634c546e95c5d3391d4
Leo Singer <leo.singer@ligo.org>
2022-07-14 09:24:02 -0400

    Set AutoPublishAlias

--- src/macros/provisionedConcurrency.js
+++ src/macros/provisionedConcurrency.js
@@ -12,5 +12,7 @@ module.exports = function provisionedConcurrency(arc, sam) {
     {
       ProvisionedConcurrentExecutions: 1,
     }
+  sam.Resources.AnyCatchallHTTPLambda.Properties.AutoPublishAlias =
+    'ProvisionedConcurrencyAlias'
   return sam
 }
