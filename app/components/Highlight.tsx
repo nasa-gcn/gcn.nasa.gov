b057f9bbf0ea89c0e8b05b79341ad50596468acf
Leo Singer <leo.singer@ligo.org>
2022-07-14 18:42:53 -0400

    Add download button to code samples

--- app/components/Highlight.tsx
+++ app/components/Highlight.tsx
@@ -6,27 +6,42 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 
-import { Button, IconContentCopy } from '@trussworks/react-uswds'
+import {
+  Button,
+  IconContentCopy,
+  IconFileDownload,
+} from '@trussworks/react-uswds'
 import hljs from 'highlight.js'
 import CopyToClipboard from 'react-copy-to-clipboard'
 
 export function Highlight({
   code,
   language,
+  filename,
 }: {
   code: string
   language: string
+  filename?: string
 }) {
   return (
     <div className="position-relative">
-      <CopyToClipboard text={code}>
-        <Button
-          type="button"
-          className="position-absolute pin-right pin-top radius-0"
-        >
-          Copy <IconContentCopy />
-        </Button>
-      </CopyToClipboard>
+      <div className="position-absolute pin-right pin-top">
+        {filename && (
+          <a
+            download={filename}
+            type="button"
+            className="usa-button radius-0"
+            href={`data:,${encodeURIComponent(code)}`}
+          >
+            Download {filename} <IconFileDownload />
+          </a>
+        )}
+        <CopyToClipboard text={code}>
+          <Button type="button" className="radius-0">
+            Copy <IconContentCopy />
+          </Button>
+        </CopyToClipboard>
+      </div>
       <pre>
         <code
           className={`hljs language-${language}`}
