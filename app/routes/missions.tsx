484111021c216d5191a8caf8ffa62df5f8832ecb
dakota002 <dakota.d@live.com>
2022-07-18 12:49:06 -0400

    whitespace

--- app/routes/missions.tsx
+++ app/routes/missions.tsx
@@ -12,7 +12,7 @@ import { NavLink, Outlet } from '@remix-run/react'
 export default function Missions() {
   return (
     <div className="grid-row grid-gap">
-      <div className="desktop:grid-col-4 ">
+      <div className="desktop:grid-col-4">
         <SideNav
           items={[
             <NavLink key="." to="." end>
