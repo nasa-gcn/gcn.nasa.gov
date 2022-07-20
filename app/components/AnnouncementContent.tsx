aaeedf3dac28ca92f3e2e0e76c28c5fd38e7634c
Dakota Dutko <dakota.d@live.com>
2022-07-20 19:54:58 -0400

    Remove duplicate text

--- app/components/AnnouncementContent.tsx
+++ app/components/AnnouncementContent.tsx
@@ -11,25 +11,27 @@ import { Link } from '@trussworks/react-uswds'
 export default function AnnouncementContent() {
   return (
     <section>
-      <strong>
-        We invite you to join one of our three public Zoom webinars for an
-        overview of the new GCN.
-      </strong>
       <div className="grid-row">
         <div className="grid-col-4">
           August 1, 2022 12:00-13:00 UTC <br /> (best for Atlantic):
           <br />
-          <Link rel="external" href="https://bit.ly/3Pt2TH9">https://bit.ly/3Pt2TH9</Link>
+          <Link rel="external" href="https://bit.ly/3Pt2TH9">
+            https://bit.ly/3Pt2TH9
+          </Link>
         </div>
         <div className="grid-col-4">
           August 1, 2022 20:00-21:00 UTC <br /> (best for Pacific):
           <br />
-          <Link rel="external" href="https://bit.ly/3IT7Qqc">https://bit.ly/3IT7Qqc</Link>
+          <Link rel="external" href="https://bit.ly/3IT7Qqc">
+            https://bit.ly/3IT7Qqc
+          </Link>
         </div>
         <div className="grid-col-4">
           August 2, 2022 04:00-05:00 UTC <br /> (best for Asia and Oceania):
           <br />
-          <Link rel="external" href="https://bit.ly/3v2pNwV">https://bit.ly/3v2pNwV</Link>
+          <Link rel="external" href="https://bit.ly/3v2pNwV">
+            https://bit.ly/3v2pNwV
+          </Link>
         </div>
       </div>
     </section>
