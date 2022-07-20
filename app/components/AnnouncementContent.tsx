5ec1b2be21e48331ea286d58945aa45849bb4cd1
Dakota Dutko <dakota.d@live.com>
2022-07-20 19:52:44 -0400

    Update app/components/AnnouncementContent.tsx
    Co-authored-by: Leo Singer <leo.p.singer@nasa.gov>

--- app/components/AnnouncementContent.tsx
+++ app/components/AnnouncementContent.tsx
@@ -19,7 +19,7 @@ export default function AnnouncementContent() {
         <div className="grid-col-4">
           August 1, 2022 12:00-13:00 UTC <br /> (best for Atlantic):
           <br />
-          <Link href={'https://bit.ly/3Pt2TH9'}>https://bit.ly/3Pt2TH9</Link>
+          <Link rel="external" href="https://bit.ly/3Pt2TH9">https://bit.ly/3Pt2TH9</Link>
         </div>
         <div className="grid-col-4">
           August 1, 2022 20:00-21:00 UTC <br /> (best for Pacific):
