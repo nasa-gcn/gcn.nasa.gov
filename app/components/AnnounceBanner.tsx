c79750cb7e9eb29e3202f3d70d180426aaa359c4
Leo Singer <leo.singer@ligo.org>
2022-08-01 12:32:14 -0400

    Move announcement content back into banner

--- app/components/AnnounceBanner.tsx
+++ app/components/AnnounceBanner.tsx
@@ -6,9 +6,8 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 
-import { Button } from '@trussworks/react-uswds'
+import { Button, Link } from '@trussworks/react-uswds'
 import { useState } from 'react'
-import AnnouncementContent from './AnnouncementContent'
 
 export default function AnnounceBanner() {
   const [showFullBanner, setShowFullBanner] = useState(true)
@@ -48,7 +47,30 @@ export default function AnnounceBanner() {
             className="usa-banner__content usa-accordion__content padding-top-2"
             id="announcement-banner-blue"
           >
-            <AnnouncementContent />
+            <div className="grid-row">
+              <div className="mobile-lg:grid-col-4">
+                August 1, 2022 12:00-13:00 UTC
+                <div>(best for Atlantic):</div>
+                <Link rel="external" href="https://bit.ly/3Pt2TH9">
+                  https://bit.ly/3Pt2TH9
+                </Link>
+              </div>
+              <div className="mobile-lg:grid-col-4">
+                August 1, 2022 20:00-21:00 UTC <br /> (best for Pacific):
+                <br />
+                <Link rel="external" href="https://bit.ly/3IT7Qqc">
+                  https://bit.ly/3IT7Qqc
+                </Link>
+              </div>
+              <div className="mobile-lg:grid-col-4">
+                August 2, 2022 04:00-05:00 UTC <br /> (best for Asia and
+                Oceania):
+                <br />
+                <Link rel="external" href="https://bit.ly/3v2pNwV">
+                  https://bit.ly/3v2pNwV
+                </Link>
+              </div>
+            </div>
           </div>
         ) : null}
       </div>
