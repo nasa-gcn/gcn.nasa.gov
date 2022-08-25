b4f76abf25695ac8881032f1c0c471b8995d0091
dakota002 <dakota.d@live.com>
2022-08-25 10:18:52 -0400

    One column

--- app/components/NewsSection.tsx
+++ app/components/NewsSection.tsx
@@ -4,7 +4,6 @@ import {
   CollectionDescription,
   CollectionHeading,
   CollectionItem,
-  Grid,
 } from '@trussworks/react-uswds'
 import moment from 'moment'
 import SectionWrapper from './SectionWrapper'
@@ -39,78 +38,28 @@ export default function NewsSection({ news }: { news?: NewsProps[] }) {
       congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
     },
   ]
-  const eventsList: NewsProps[] = [
-    {
-      Date: Date.now(),
-      Title: 'Article 1 Something new ',
-      BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
-    },
-    {
-      Date: Date.now(),
-      Title: 'Article 2 Another',
-      BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
-    },
-    {
-      Date: Date.now(),
-      Title: 'Article 3, This could be a link too',
-      BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
-    },
-  ]
+
   return (
     <>
       <SectionWrapper className="usa-section--light">
-        <Grid row gap>
-          <Grid tablet={{ col: 6 }}>
-            <h2 className="">News</h2>
-            <Collection>
-              {newsList?.map((article) => (
-                <CollectionItem
-                  key={article.Title}
-                  variantComponent={
-                    <CollectionCalendarDate
-                      datetime={moment.utc(article.Date).toString()}
-                    />
-                  }
-                >
-                  <CollectionHeading headingLevel="h3">
-                    {article.Title}
-                  </CollectionHeading>
-                  <CollectionDescription>
-                    {article.BodyText}
-                  </CollectionDescription>
-                </CollectionItem>
-              ))}
-            </Collection>
-          </Grid>
-          <Grid tablet={{ col: 6 }}>
-            <h2 className="">Upcoming Events</h2>
-            <Collection>
-              {eventsList?.map((article) => (
-                <CollectionItem
-                  key={article.Title}
-                  variantComponent={
-                    <CollectionCalendarDate
-                      datetime={moment.utc(article.Date).toString()}
-                    />
-                  }
-                >
-                  <CollectionHeading headingLevel="h3">
-                    {article.Title}
-                  </CollectionHeading>
-                  <CollectionDescription>
-                    {article.BodyText}
-                  </CollectionDescription>
-                </CollectionItem>
-              ))}
-            </Collection>
-          </Grid>
-        </Grid>
+        <h2 className="">News and Events</h2>
+        <Collection>
+          {newsList?.map((article) => (
+            <CollectionItem
+              key={article.Title}
+              variantComponent={
+                <CollectionCalendarDate
+                  datetime={moment.utc(article.Date).toString()}
+                />
+              }
+            >
+              <CollectionHeading headingLevel="h3">
+                {article.Title}
+              </CollectionHeading>
+              <CollectionDescription>{article.BodyText}</CollectionDescription>
+            </CollectionItem>
+          ))}
+        </Collection>
       </SectionWrapper>
     </>
   )
