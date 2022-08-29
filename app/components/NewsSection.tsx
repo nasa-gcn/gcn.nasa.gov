1d3038304435d9c0690ca11504bfe1e85833e923
dakota002 <dakota.d@live.com>
2022-08-29 13:14:23 -0400

    Updates the news section

--- app/components/NewsSection.tsx
+++ app/components/NewsSection.tsx
@@ -4,38 +4,62 @@ import {
   CollectionDescription,
   CollectionHeading,
   CollectionItem,
+  Link,
 } from '@trussworks/react-uswds'
-import moment from 'moment'
+import type { ReactNode } from 'react'
 import SectionWrapper from './SectionWrapper'
 
 export interface NewsProps {
-  Date: number
+  Date: string
   Title: string
-  BodyText: string
+  BodyText: ReactNode
 }
 
 export default function NewsSection({ news }: { news?: NewsProps[] }) {
   const newsList: NewsProps[] = [
     {
-      Date: Date.now(),
-      Title: 'Article 1 Something new ',
+      Date: 'August/September ?, 2022',
+      Title: 'GECAM New Notice Type Available',
       BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
+        'The GCN system has been modified to incorporate the distribution of the GECAM Notices.\
+         [link to GECAM missions page]  GECAM notices are available via GCN Classic distribution\
+          methods and formats (binary socket, VOEvent xml socket, text via email and SMS) as well as GCN Classic Over Kafka, and the newly released self-service configuration of GCN Notices over email.',
     },
     {
-      Date: Date.now(),
-      Title: 'Article 2 Another',
+      Date: 'August/September ?, 2022',
+      Title:
+        ' Self-Service Configuration of GCN Notices over Email for Transient Alerts',
       BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
+        'This new feature will greatly simplify and enhance the user experience for subscribing to GCN Notices.  Using the same accounts on https://gcn.nasa.gov as GCN Classic Over Kafka, users can now sign up to subscribe to any of the public GCN Notice types in any of the 3 legacy formats (text, binary, VOEvent), and edit their settings at will.',
     },
     {
-      Date: Date.now(),
-      Title: 'Article 3, This could be a link too',
-      BodyText:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse cursus, orci sed \
-      congue molestie, orci diam pharetra ipsum, eu laoreet justo magna vel dui. Sed ut mattis dui. Ut id commodo dui.',
+      Date: 'August 1, 2022',
+      Title: 'New GCN Webinars',
+      BodyText: (
+        <>
+          GCN held three public webinars to introduce the new GCN, the GCN
+          Classic over Kafka service, and plans for new features and feedback.
+          The{' '}
+          <Link href="https://nasa-gcn.github.io/gcn-presentation/">
+            slides
+          </Link>{' '}
+          and a recording are available.
+        </>
+      ),
+    },
+    {
+      Date: 'July 20, 2022',
+      Title: 'GCN Classic over Kafka Now Available',
+      BodyText: (
+        <>
+          All three classic GCN Notice formats (text, VOEvent, 160-byte binary
+          packet) are now also available over Kafka. This new Kafka streaming
+          service can be used as a drop-in replacement for GCN Classic socket
+          and VOEvent subscribers, with an upcoming release serving email
+          subscribers. (GCN Circ{' '}
+          <Link href="https://gcn.gsfc.nasa.gov/gcn3/32419.gcn3">#32419</Link>),
+        </>
+      ),
     },
   ]
 
@@ -48,9 +72,7 @@ export default function NewsSection({ news }: { news?: NewsProps[] }) {
             <CollectionItem
               key={article.Title}
               variantComponent={
-                <CollectionCalendarDate
-                  datetime={moment.utc(article.Date).toString()}
-                />
+                <CollectionCalendarDate datetime={article.Date} />
               }
             >
               <CollectionHeading headingLevel="h3">
