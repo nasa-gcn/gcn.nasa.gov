97775102087870748148c561f6c10ae8cb71cd6a
dakota002 <dakota.d@live.com>
2022-10-25 14:47:55 -0400

    Peer Endorsement Breakout PR 2, Auth token refresh

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -6,6 +6,12 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 
+import type { CognitoIdentityProviderServiceException } from '@aws-sdk/client-cognito-identity-provider'
+import { getOpenIDClient } from '~/routes/__auth/auth.server'
+import { userFromTokenSet } from '~/routes/__auth/login'
+import type { getUser } from '~/routes/__auth/user.server'
+import { updateSession } from '~/routes/__auth/user.server'
+
 export function formatAndNoticeTypeToTopic(
   noticeFormat: string,
   noticeType: string
@@ -29,5 +35,34 @@ export function getFormDataString(formData: FormData, key: string) {
     return undefined
   } else {
     throw new Response(`expected ${key} to be a string`, { status: 400 })
+  }
+}
+
+export async function getLatestUserGroups(
+  user: NonNullable<Awaited<ReturnType<typeof getUser>>>
+) {
+  const client = await getOpenIDClient()
+  const refreshedTokenSet = await client.refresh(user.refreshToken)
+  const user_new = userFromTokenSet(refreshedTokenSet)
+  await updateSession(user_new)
+  user.groups = user_new.groups
+}
+
+export function maybeThrow(e: any, warning: string) {
+  const errorsAllowedInDev = [
+    'ExpiredTokenException',
+    'UnrecognizedClientException',
+  ]
+  const { name } = e as CognitoIdentityProviderServiceException
+
+  if (
+    !errorsAllowedInDev.includes(name) ||
+    process.env.NODE_ENV === 'production'
+  ) {
+    throw e
+  } else {
+    console.warn(
+      `Cognito threw ${name}. This would be an error in production. Since we are in ${process.env.NODE_ENV}, ${warning}.`
+    )
   }
 }
--- app/routes/__auth/user.server.ts
+++ app/routes/__auth/user.server.ts
@@ -6,6 +6,11 @@
  * SPDX-License-Identifier: NASA-1.3
  */
 
+import {
+  CognitoIdentityProviderClient,
+  AdminAddUserToGroupCommand,
+} from '@aws-sdk/client-cognito-identity-provider'
+import { getLatestUserGroups, maybeThrow } from '~/lib/utils'
 import { storage } from './auth.server'
 
 export async function getUser({ headers }: Request) {
@@ -14,8 +19,39 @@ export async function getUser({ headers }: Request) {
   const email = session.get('email') as string
   const groups = session.get('groups') as string[]
   const idp = session.get('idp') as string | null
-
+  const refreshToken = session.get('refreshToken') as string
+  const cognitoUserName = session.get('cognitoUserName') as string
   if (!sub) return null
+  const user = { sub, email, groups, idp, refreshToken, cognitoUserName }
+  await getLatestUserGroups(user)
+  return user
+}
 
-  return { sub, email, groups, idp }
+export async function updateSession(user: any) {
+  const session = await storage.getSession()
+  Object.entries(user).forEach(([key, value]) => {
+    session.set(key, value)
+  })
+  await storage.commitSession(session)
+}
+
+export async function addUserToCircularSubmitterGroup(user: {
+  sub: string
+  email: string
+  groups: string[]
+  idp: string | null
+  refreshToken: string
+  cognitoUserName: string
+}) {
+  try {
+    const cognitoIdentityProviderClient = new CognitoIdentityProviderClient({})
+    const command = new AdminAddUserToGroupCommand({
+      Username: user.cognitoUserName,
+      UserPoolId: process.env.COGNITO_USER_POOL_ID,
+      GroupName: 'gcn.nasa.gov/circular-submitter',
+    })
+    await cognitoIdentityProviderClient.send(command)
+  } catch (error) {
+    maybeThrow(error, 'no group added, yet.')
+  }
 }
