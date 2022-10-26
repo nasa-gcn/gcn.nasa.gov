f04b88fdd512d9969d0b9e970cf9b989109d3b7a
dakota002 <dakota.d@live.com>
2022-10-26 15:21:33 -0400

    Peer Endorsement Breakout PR 3, Circulars Backend
    Just the circulars backend
    
    Removed leftover lines from testing
    
    Separate the reading from writing, since the data should be available to anyone
    
    Moves get by id to public, simplify create
    
    Non null sub
    
    Moved the server file to a more appropriately named folder
    
    Changing put to update to try to make use of locking
    
    TEST but it works for now, fixing before pushing
    
    Removes testing code, simplify add
    
    Changes a param name
    
    Adding some simple comments to the circulars server, and a TODO comment for validation
    
    Starting validation
    
    Updates to remove single use functons, expands on comments, some validation
    
    temp changes, will delete comments shortly
    
    Updates the circulars server to check user's groups for adding and deleting, includes user data for authors
    
    Update app/routes/circulars/circulars.server.ts
    
    Update app/routes/circulars/circulars.server.ts
    
    Update app/routes/circulars/circulars.server.ts
    
    Update app/routes/circulars/circulars.server.ts
    
    Just one class, removes linked events and corrected Id, rename getCircualrById -> get
    
    Updates comments about thrown errors
    
    Trying to implement the new method
    
    Updates the circulars backend to use the new auto increment function with updates from the unit tests branch
    
    Removes test lines
    
    Set dangerous based on host

---
+++ app/routes/circulars/circulars.server.ts
@@ -0,0 +1,212 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+import { tables } from '@architect/functions'
+import type { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { dynamoDBAutoIncrement } from '~/lib/dynamoDBAutoIncrement'
+import { getUser } from '../__auth/user.server'
+
+export type CircularModel = {
+  circularId?: number
+  sub?: string
+  createdOn: number
+  subject: string
+  body: string
+  submitter: string
+}
+
+export class CircularsServer {
+  #sub?: string
+  #email?: string
+  #groups?: string[]
+  #name?: string
+  #affiliation?: string
+  #isLocal: boolean
+
+  private constructor(
+    isLocal: boolean,
+    sub?: string,
+    email?: string,
+    groups?: string[],
+    name?: string,
+    affiliation?: string
+  ) {
+    this.#affiliation = affiliation
+    this.#email = email
+    this.#groups = groups
+    this.#name = name
+    this.#sub = sub
+    this.#isLocal = isLocal
+  }
+
+  static async create(request: Request) {
+    const user = await getUser(request)
+    const isLocal = new URL(request.url).hostname == 'localhost'
+    return new this(
+      isLocal,
+      user?.sub,
+      user?.email,
+      user?.groups,
+      user?.name,
+      user?.affiliation
+    )
+  }
+
+  /**
+   * Gets all available circulars
+   * @param searchString - a string that will be checked against subjects,
+   * bodies, and authors of saved circulars. A blank value returns all.
+   * @returns an array of CircularModel objects
+   */
+  async getCirculars(searchString?: string) {
+    let result = await this.#scanCircularsTable(searchString)
+    let items: CircularModel[] = result.Items
+    while (result.LastEvaluatedKey) {
+      result = await this.#scanCircularsTable(
+        searchString,
+        result.LastEvaluatedKey
+      )
+      items = [...items, ...result.Items]
+    }
+    return items.sort((a, b) => (b.circularId ?? 0) - (a.circularId ?? 0))
+  }
+
+  async #scanCircularsTable(searchString?: string, lastEvaluatedKey?: any) {
+    const db = await tables()
+    const result = await db.gcn_circulars.scan({
+      ExclusiveStartKey: lastEvaluatedKey,
+      ExpressionAttributeValues: searchString
+        ? {
+            ':searchTerm': searchString,
+          }
+        : undefined,
+      FilterExpression: searchString
+        ? 'contains(subject, :searchTerm) OR contains(body, :searchTerm) OR contains(submitter, :searchTerm)'
+        : undefined,
+    })
+    return result
+  }
+
+  /**
+   * Get a specified Circular by Id.
+   *
+   * Throws an HTTP error if:
+   *  - the provided Id does not match any existing circulars
+   *
+   * @param id - the number Id (ex. 12345) of a specific Circular
+   * @returns a CircularModel object
+   */
+  async get(circularId: number): Promise<CircularModel> {
+    const db = await tables()
+    const result = await db.gcn_circulars.get({
+      circularId: circularId,
+    })
+    if (!result)
+      throw new Response('The requested circular does not exist', {
+        status: 404,
+      })
+    return result
+  }
+
+  /**
+   * Adds a new entry into the GCN Circulars table
+   *
+   * Throws an HTTP error if:
+   *  - The current user is not signed in, verified by the class's #sub and #groups properties
+   *  - The current user is not in the submitters group
+   *  - Body or Subject are blank
+   * @param body - main content of the Circular
+   * @param subject - the title/subject line of the Circular
+   */
+  async createNewCircular(body: string, subject: string) {
+    if (!this.#sub || !this.#groups)
+      throw new Response('User is not signed in', { status: 403 })
+    if (!this.#groups.includes('gcn.nasa.gov/circular-submitter'))
+      throw new Response('User is not in the submitters group', {
+        status: 403,
+      })
+    if (!body || !subject)
+      throw new Response('Subject and Body cannot be blank', { status: 400 })
+
+    const db = await tables()
+    const doc = db._doc as unknown as DynamoDBDocument
+
+    const tableName = db.name('gcn_circulars')
+    const indexTableName = db.name('auto_increment_metadata')
+
+    const submitter =
+      this.#name +
+      (this.#affiliation ? ` at ${this.#affiliation} ` : ' ') +
+      this.#email
+
+    await dynamoDBAutoIncrement({
+      doc,
+      counterTableName: indexTableName,
+      counterTableKey: { tableName: 'gcn_circulars' },
+      counterTableAttributeName: 'circularsIDCounter',
+      tableName: tableName,
+      tableAttributeName: 'circularId',
+      initialValue: 1,
+      dangerously: this.#isLocal,
+    })({
+      createdOn: Date.now(),
+      subject,
+      body,
+      sub: this.#sub,
+      submitter,
+    })
+  }
+
+  /**
+   * Gets all of the GCN Circualrs submitted by the current user.
+   *
+   * Throws and HTTP error if:
+   *  - The current user is not signed in, verified by the class's #sub and #groups properties
+   *
+   * @returns an array of CircularModel objects
+   */
+  async getUserSubmittedCirculars() {
+    if (!this.#sub) throw new Response('User is not signed in', { status: 403 })
+
+    const db = await tables()
+    const result = await db.gcn_circulars.scan({
+      ExpressionAttributeNames: {
+        '#sub': 'sub',
+      },
+      ExpressionAttributeValues: {
+        ':sub': this.#sub,
+      },
+      FilterExpression: '#sub = :sub',
+      ProjectionExpression:
+        'subject, body, createdOn, circularId, #sub, submitter',
+    })
+
+    return result.Items as CircularModel[]
+  }
+
+  /**
+   * Deletes the circular corresponding to the given circularId
+   *
+   * Throws an HTTP error if:
+   *  - The current user is not signed in, verified by the class's #sub and #groups properties
+   *  - The current user is not in the moderator group
+   * @param circularId - the ID of the circular to be deleted
+   */
+  async deleteCircular(circularId: number) {
+    if (!this.#sub || !this.#groups)
+      throw new Response('User is not signed in', { status: 403 })
+
+    if (!this.#groups.includes('gcn.nasa.gov/circular-moderator'))
+      throw new Response('User is not a moderator', {
+        status: 403,
+      })
+
+    const db = await tables()
+    await db.gcn_circulars.delete({ circularId: circularId })
+  }
+}
