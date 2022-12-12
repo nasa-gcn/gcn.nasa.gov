3cd8cb43a121980dfe8c552ef0fe3083aeaa3adc
Leo Singer <leo.singer@ligo.org>
2022-12-12 14:17:34 -0500

    Fix Remix live reloading
    Work around Workaround for Remix live reload bug, remix-run/remix#198

---
+++ src/plugins/remixLiveReload.js
@@ -0,0 +1,16 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+// Workaround for Remix live reload bug, https://github.com/remix-run/remix/issues/198
+module.exports = {
+  set: {
+    env() {
+      return { testing: { NODE_ENV: 'development' } }
+    },
+  },
+}
