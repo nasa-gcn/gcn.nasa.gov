598e9d1de8a02ed8b2c5894493bb9912081cd3ba
Leo Singer <leo.singer@ligo.org>
2022-12-14 17:05:30 -0500

    Cognito PreTokenGeneration lambda ignores claims containing `custom:`

--- app/routes/__auth/user.server.ts
+++ app/routes/__auth/user.server.ts
@@ -55,11 +55,11 @@ export function parseTokenSet(tokenSet: TokenSet): {
     (group) => group.startsWith('gcn.nasa.gov/')
   )
   const name = claims.name
-  const affiliation = claims['custom:affiliation'] as string | undefined
+  const affiliation = claims['affiliation'] as string | undefined
   const accessToken = tokenSet.access_token
   const refreshToken = tokenSet.refresh_token
   const expiresAt = tokenSet.expires_at
-  const existingIdp = claims['dev:custom:existingIdp'] as string | undefined
+  const existingIdp = claims['existingIdp'] as string | undefined
 
   if (!email) throw new Error('email claim must be present')
   if (typeof cognitoUserName !== 'string')
