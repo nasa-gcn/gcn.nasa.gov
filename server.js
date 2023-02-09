2c02493019c6dbe29231b5b3f60487a1b787300f
Leo Singer <leo.singer@ligo.org>
2023-02-09 10:54:56 -0500

    Revert "Set callbackWaitsForEmptyEventLoop to false"
    This reverts commit e965cd1ac5de4ca16d5ebc4407fef96aa6085236.

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 import { createRequestHandler } from '@remix-run/architect'
 import * as build from '@remix-run/dev/server-build'
 
-const handler = createRequestHandler({
+// FIXME: must use module.exports here for OpenTelemetry shim to work correctly.
+// See https://dev.to/heymarkkop/how-to-solve-cannot-redefine-property-handler-on-aws-lambda-3j67
+module.exports.handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
 })
-
-// FIXME: must use module.exports here for OpenTelemetry shim to work correctly.
-// See https://dev.to/heymarkkop/how-to-solve-cannot-redefine-property-handler-on-aws-lambda-3j67
-module.exports.handler = async function (event, context) {
-  // Tell Lambda runtime to send response to invoker immediately without
-  // waiting for pending callbacks to finish. I suspect that OpenTelemetry was
-  // blocking responses.
-  context.callbackWaitsForEmptyEventLoop = false
-  return await handler(event)
-}
