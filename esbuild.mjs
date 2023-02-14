94afb724bd35ccaa58414496e9605b4feed24031
Leo Singer <leo.singer@ligo.org>
2023-02-14 11:42:09 -0500

    Exclude node_modules from esbuild bundling
    Architect "hydrates" external packages, so there is no need for
    esbuild to bundle them.

---
+++ esbuild.mjs
@@ -0,0 +1,23 @@
+import { build } from 'esbuild'
+
+const args = process.argv.slice(2)
+const watch = args.includes('--watch')
+
+await build({
+  bundle: true,
+  entryPoints: ['./app/email-incoming/index.js'],
+  logLevel: 'info',
+  outfile: 'email-incoming/index.js',
+  plugins: [
+    {
+      name: 'exclude node_modules from bundling',
+      setup(build) {
+        build.onResolve({ filter: /^(?:[^./~]|~[^/])/ }, () => ({
+          external: true,
+        }))
+      },
+    },
+  ],
+  platform: 'node',
+  watch,
+})
