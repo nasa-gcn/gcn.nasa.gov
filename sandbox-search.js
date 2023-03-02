c71501fa64bc431bda78f0f60131405e2a708e51
Leo Singer <leo.singer@ligo.org>
2023-03-02 00:23:17 -0500

    OpenSearch bulk command cannot be used to create indices

--- sandbox-search.js
+++ sandbox-search.js
@@ -13,22 +13,8 @@ const _index = 'circulars'
 module.exports = async function () {
   const text = await readFile('sandbox-seed.json', { encoding: 'utf-8' })
   const { circulars } = JSON.parse(text)
-  return [
-    { create: { _index } },
-    {
-      mappings: {
-        properties: {
-          circularId: { type: 'integer' },
-          createdOn: { type: 'date', format: 'epoch_millis' },
-          submitter: { type: 'text' },
-          subject: { type: 'text' },
-          body: { type: 'text' },
-        },
-      },
-    },
-    ...circulars.flatMap((item) => {
-      delete item.dummy
-      return [{ index: { _index, _id: item.circularId.toString() } }, item]
-    }),
-  ]
+  return circulars.flatMap((item) => {
+    delete item.dummy
+    return [{ index: { _index, _id: item.circularId.toString() } }, item]
+  })
 }
