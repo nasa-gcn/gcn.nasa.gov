a75777b75d354da79c0f009f94e8f49f2ada4300
Leo Singer <leo.singer@ligo.org>
2023-03-06 16:38:15 -0500

    Bundle unified schema, server from https://gcn.nasa.gov/schema

---
+++ app/routes/schema/$.ts
@@ -0,0 +1,43 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+import { readFile } from 'node:fs/promises'
+import { extname, join, dirname } from 'node:path'
+import type { DataFunctionArgs } from '@remix-run/node'
+import '@nasa-gcn/schema'
+
+function isErrnoException(e: unknown): e is NodeJS.ErrnoException {
+  return e instanceof Error && 'code' in e && 'errno' in e
+}
+
+/* Make all JSON files at https://github.com/nasa-gcn/gcn-schema available from
+ * https://gcn.nasa.gov/schema */
+export async function loader({ params: { '*': subPath } }: DataFunctionArgs) {
+  if (!subPath) throw new Error('path must be defined')
+
+  if (extname(subPath) !== '.json')
+    throw new Response('not found', { status: 404 })
+
+  const path = join(dirname(require.resolve('@nasa-gcn/schema')), subPath)
+
+  let body
+  try {
+    body = await readFile(path, {
+      encoding: 'utf-8',
+    })
+  } catch (e) {
+    if (isErrnoException(e) && e.code === 'ENOENT') {
+      throw new Response('Not found', { status: 404 })
+    }
+    throw e
+  }
+
+  return new Response(body, { headers: { 'Content-Type': 'application/json' } })
+}
+
+export default function () {}
