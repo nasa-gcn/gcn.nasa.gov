3c22377206ccaf7f9275280e657f5fbac1abf1cf
jracusin <judith.racusin@nasa.gov>
2023-03-28 17:53:13 -1000

    updating circulars documentation and style guide

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -24,7 +24,10 @@ export default function () {
               About GCN
             </NavLink>,
             <NavLink key="client" to="client">
-              Client Configuration
+              Kafka Client Configuration
+            </NavLink>,
+            <NavLink key="circulars" to="circulars">
+              Circulars
             </NavLink>,
             <NavLink key="styleguide" to="styleguide">
               Circulars Style Guide
