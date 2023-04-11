7d08303dc717171c490b287255c6373e2b45ab61
Leo Singer <leo.singer@ligo.org>
2023-04-11 10:33:35 -0400

    Migrate to Remix v2 error boundary
    See https://remix.run/docs/en/1.15.0/pages/v2#catchboundary-and-errorboundary

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,6 @@
  */
 import type {
   DataFunctionArgs,
-  ErrorBoundaryComponent,
   LinksFunction,
   MetaFunction,
 } from '@remix-run/node'
@@ -32,10 +31,11 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch,
+  isRouteErrorResponse,
   useLocation,
   useMatches,
   useNavigation,
+  useRouteError,
 } from '@remix-run/react'
 import { ButtonGroup, GovBanner, GridContainer } from '@trussworks/react-uswds'
 import type { ReactNode } from 'react'
@@ -46,7 +46,6 @@ import AnnounceBanner, { AnnouncementEvent } from './components/AnnounceBanner'
 import { DevBanner } from './components/DevBanner'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
-import { Highlight } from './components/Highlight'
 import {
   getEnvOrDieInProduction,
   getFeatures,
@@ -255,52 +254,11 @@ function Document({ children }: { children?: React.ReactNode }) {
   )
 }
 
-export function CatchBoundary() {
-  const { status } = useCatch()
-  const url = useUrl()
-  if (status == 403) {
-    return (
-      <Document>
-        <h1>Unauthorized</h1>
-        <p className="usa-intro">
-          We're sorry, you must log in to access the page you're looking for.
-        </p>
-        <p className="usa-paragraph">Log in to access that page, or go home.</p>
-        <ButtonGroup>
-          <Link
-            to={`/login?redirect=${encodeURIComponent(url)}`}
-            className="usa-button"
-          >
-            Log in and take me there
-          </Link>
-          <Link to="/" className="usa-button usa-button--outline">
-            Go home
-          </Link>
-        </ButtonGroup>
-      </Document>
-    )
-  } else {
-    return (
-      <Document>
-        <h1>Unexpected error (HTTP {status})</h1>
-        <p className="usa-intro">An unexpected error occurred.</p>
-        <ButtonGroup>
-          <Link to="/" className="usa-button">
-            Go home
-          </Link>
-        </ButtonGroup>
-      </Document>
-    )
-  }
-}
-
-export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
-  console.error(error)
+function ErrorUnexpected({ children }: { children?: ReactNode }) {
   return (
     <Document>
-      <h1>Unexpected error</h1>
-      <p className="usa-intro">An unexpected error occurred:</p>
-      <Highlight language="text" code={error.stack || error.toString()} />
+      <h1>Unexpected error {children}</h1>
+      <p className="usa-intro">An unexpected error occurred.</p>
       <ButtonGroup>
         <Link to="/" className="usa-button">
           Go home
@@ -308,6 +266,71 @@ export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
       </ButtonGroup>
     </Document>
   )
+}
+
+function ErrorUnauthorized() {
+  const url = useUrl()
+  return (
+    <Document>
+      <h1>Unauthorized</h1>
+      <p className="usa-intro">
+        We're sorry, you must log in to access the page you're looking for.
+      </p>
+      <p className="usa-paragraph">Log in to access that page, or go home.</p>
+      <ButtonGroup>
+        <Link
+          to={`/login?redirect=${encodeURIComponent(url)}`}
+          className="usa-button"
+        >
+          Log in and take me there
+        </Link>
+        <Link to="/" className="usa-button usa-button--outline">
+          Go home
+        </Link>
+      </ButtonGroup>
+    </Document>
+  )
+}
+
+function ErrorNotFound() {
+  return (
+    <Document>
+      <h1>Page not found</h1>
+      <p className="usa-intro">
+        We're sorry, we can't find the page you're looking for. It might have
+        been removed, changed its name, or is otherwise unavailable.
+      </p>
+      <p className="usa-paragraph">
+        Visit our homepage for helpful tools and resources, or contact us and
+        we'll point you in the right direction.
+      </p>
+      <ButtonGroup>
+        <Link to="/" className="usa-button">
+          Visit homepage
+        </Link>
+        <Link to="/contact" className="usa-button usa-button--outline">
+          Contact us
+        </Link>
+      </ButtonGroup>
+    </Document>
+  )
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError()
+  if (isRouteErrorResponse(error)) {
+    switch (error.status) {
+      case 403:
+        return <ErrorUnauthorized />
+      case 404:
+        return <ErrorNotFound />
+      default:
+        return <ErrorUnexpected>HTTP {error.status}</ErrorUnexpected>
+    }
+  } else {
+    console.error(error)
+    return <ErrorUnexpected />
+  }
 }
 
 export default function App() {
--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,6 +1,4 @@
 import type { LoaderFunction } from '@remix-run/node'
-import { Link } from '@remix-run/react'
-import { ButtonGroup } from '@trussworks/react-uswds'
 
 export const handle = {
   breadcrumb: 'Page Not Found',
@@ -9,30 +7,6 @@ export const handle = {
 
 export const loader: LoaderFunction = function () {
   throw new Response(null, { status: 404 })
-}
-
-export function CatchBoundary() {
-  return (
-    <>
-      <h1>Page not found</h1>
-      <p className="usa-intro">
-        We're sorry, we can't find the page you're looking for. It might have
-        been removed, changed its name, or is otherwise unavailable.
-      </p>
-      <p className="usa-paragraph">
-        Visit our homepage for helpful tools and resources, or contact us and
-        we'll point you in the right direction.
-      </p>
-      <ButtonGroup>
-        <Link to="/" className="usa-button">
-          Visit homepage
-        </Link>
-        <Link to="/contact" className="usa-button usa-button--outline">
-          Contact us
-        </Link>
-      </ButtonGroup>
-    </>
-  )
 }
 
 export default function () {}
