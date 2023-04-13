a8b7cca10de98c9beeec6ffec7f3ed68b08219d7
Leo Singer <leo.singer@ligo.org>
2023-04-13 08:51:08 -0400

    Redirect to schema, don't bundle it
    This way, it is always up to date.
    
    Fixes nasa-gcn/gcn-schema#49.

--- app/routes/schema/$.ts
+++ app/routes/schema/$.ts
@@ -5,36 +5,12 @@
  *
  * SPDX-License-Identifier: NASA-1.3
  */
-import '@nasa-gcn/schema'
-import type { DataFunctionArgs } from '@remix-run/node'
-import { readFile } from 'node:fs/promises'
-import { dirname, extname, join } from 'node:path'
-
-function isErrnoException(e: unknown): e is NodeJS.ErrnoException {
-  return e instanceof Error && 'code' in e && 'errno' in e
-}
+import { type DataFunctionArgs, redirect } from '@remix-run/node'
 
 /* Make all JSON files at https://github.com/nasa-gcn/gcn-schema available from
  * https://gcn.nasa.gov/schema */
-export async function loader({ params: { '*': subPath } }: DataFunctionArgs) {
-  if (!subPath) throw new Error('path must be defined')
-
-  if (extname(subPath) !== '.json')
-    throw new Response('not found', { status: 404 })
-
-  const path = join(dirname(require.resolve('@nasa-gcn/schema')), subPath)
-
-  let body
-  try {
-    body = await readFile(path, {
-      encoding: 'utf-8',
-    })
-  } catch (e) {
-    if (isErrnoException(e) && e.code === 'ENOENT') {
-      throw new Response('Not found', { status: 404 })
-    }
-    throw e
-  }
-
-  return new Response(body, { headers: { 'Content-Type': 'application/json' } })
+export async function loader({ params: { '*': path } }: DataFunctionArgs) {
+  return redirect(
+    `https://raw.githubusercontent.com/nasa-gcn/gcn-schema/main/${path ?? ''}`
+  )
 }
