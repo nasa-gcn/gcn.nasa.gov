82bad6f47a7eef893954c66c277cae7e6ed7b523
Leo Singer <leo.singer@ligo.org>
2023-05-03 22:27:42 -0400

    Update to Remix 1.16.0

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
 import { createRequestHandler } from '@remix-run/architect'
 import * as build from '@remix-run/dev/server-build'
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 // FIXME: must use module.exports here for OpenTelemetry shim to work correctly.
 // See https://dev.to/heymarkkop/how-to-solve-cannot-redefine-property-handler-on-aws-lambda-3j67
