198714f7de1744ac87fd5f095287d79429a51f9c
dakota002 <dakota.d@live.com>
2023-06-15 13:24:47 -0400

    Temp solution for hiding the side nav when on /docs/schema-browser/* routes

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -5,9 +5,9 @@
  *
  * SPDX-License-Identifier: NASA-1.3
  */
-import { Link, NavLink, Outlet, useOutletContext } from '@remix-run/react'
+import { Link, NavLink, Outlet } from '@remix-run/react'
 import { GridContainer } from '@trussworks/react-uswds'
-import { useState } from 'react'
+import { useMatch } from 'react-router'
 
 import { SideNav, SideNavSub } from '~/components/SideNav'
 
@@ -15,129 +15,123 @@ export const handle = {
   breadcrumb: 'Documentation',
 }
 
-type ContextType = {
-  showSideNav: boolean
-  setShowSideNav: (val: boolean) => void
-}
-
-export function useSideNavContext() {
-  return useOutletContext<ContextType>()
-}
-
 export default function () {
-  const [showSideNav, setShowSideNav] = useState(true)
+  const isSchemaBrowser = useMatch('/docs/schema-browser/*')
 
   return (
     <GridContainer className="usa-section">
       <div className="grid-row grid-gap">
-        <div className="desktop:grid-col-3" hidden={!showSideNav}>
-          <SideNav
-            items={[
-              <NavLink key="." to="." end>
-                About GCN
-              </NavLink>,
-              <>
-                <NavLink key="circulars" to="circulars" end>
-                  Circulars
-                </NavLink>
-                <SideNavSub
-                  base="circulars"
-                  items={[
-                    <NavLink key="subscribing" to="circulars/subscribing">
-                      Subscribing
-                    </NavLink>,
-                    <NavLink key="submitting" to="circulars/submitting">
-                      Submitting
-                    </NavLink>,
-                    <NavLink key="styleguide" to="circulars/styleguide">
-                      Style Guide
-                    </NavLink>,
-                    <NavLink key="archive" to="circulars/archive">
-                      Archive
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <>
-                <NavLink key="contributing" to="contributing">
-                  Contributing
-                </NavLink>
-                <SideNavSub
-                  base="contributing"
-                  key="contributing-sub"
-                  items={[
-                    <NavLink key="index" to="contributing" end>
-                      Getting Started
-                    </NavLink>,
-                    <NavLink
-                      key="feature-flags"
-                      to="contributing/feature-flags"
-                    >
-                      Feature Flags
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <>
-                <NavLink key="faq" to="faq">
-                  Frequently Asked Questions
-                </NavLink>
-                <SideNavSub
-                  base="faq"
-                  key="faq-sub"
-                  items={[
-                    <Link key="kafka" to="faq#kafka">
-                      Kafka
-                    </Link>,
-                    <Link key="circulars" to="faq#circulars">
-                      Circulars
-                    </Link>,
-                    <Link key="accounts" to="faq#accounts">
-                      Accounts
-                    </Link>,
-                  ]}
-                />
-              </>,
-              <NavLink key="schema-browser" to="schema-browser">
-                Schema-Browser
-              </NavLink>,
-              <NavLink key="history" to="history">
-                History
-              </NavLink>,
-              <>
-                <NavLink key="client" to="client">
-                  Kafka Client Setup
-                </NavLink>
-                <SideNavSub
-                  base="client"
-                  key="client-sub"
-                  items={[
-                    <Link key="python" to="client#python">
-                      Python
-                    </Link>,
-                    <Link key="nodejs" to="client#nodejs">
-                      Node.js
-                    </Link>,
-                    <Link key="c" to="client#c">
-                      C
-                    </Link>,
-                    <Link key="c-1" to="client#c-1">
-                      C#
-                    </Link>,
-                  ]}
-                />
-              </>,
-              <NavLink key="producers" to="producers">
-                New Notice Producers
-              </NavLink>,
-              <NavLink key="roadmap" to="roadmap">
-                Road Map
-              </NavLink>,
-            ]}
-          />
-        </div>
-        <div className={showSideNav ? 'desktop:grid-col-9' : 'grid-col-12'}>
-          <Outlet context={{ showSideNav, setShowSideNav }} />
+        {!isSchemaBrowser && (
+          <div className="desktop:grid-col-3">
+            <SideNav
+              items={[
+                <NavLink key="." to="." end>
+                  About GCN
+                </NavLink>,
+                <>
+                  <NavLink key="circulars" to="circulars" end>
+                    Circulars
+                  </NavLink>
+                  <SideNavSub
+                    base="circulars"
+                    items={[
+                      <NavLink key="subscribing" to="circulars/subscribing">
+                        Subscribing
+                      </NavLink>,
+                      <NavLink key="submitting" to="circulars/submitting">
+                        Submitting
+                      </NavLink>,
+                      <NavLink key="styleguide" to="circulars/styleguide">
+                        Style Guide
+                      </NavLink>,
+                      <NavLink key="archive" to="circulars/archive">
+                        Archive
+                      </NavLink>,
+                    ]}
+                  />
+                </>,
+                <>
+                  <NavLink key="contributing" to="contributing">
+                    Contributing
+                  </NavLink>
+                  <SideNavSub
+                    base="contributing"
+                    key="contributing-sub"
+                    items={[
+                      <NavLink key="index" to="contributing" end>
+                        Getting Started
+                      </NavLink>,
+                      <NavLink
+                        key="feature-flags"
+                        to="contributing/feature-flags"
+                      >
+                        Feature Flags
+                      </NavLink>,
+                    ]}
+                  />
+                </>,
+                <>
+                  <NavLink key="faq" to="faq">
+                    Frequently Asked Questions
+                  </NavLink>
+                  <SideNavSub
+                    base="faq"
+                    key="faq-sub"
+                    items={[
+                      <Link key="kafka" to="faq#kafka">
+                        Kafka
+                      </Link>,
+                      <Link key="circulars" to="faq#circulars">
+                        Circulars
+                      </Link>,
+                      <Link key="accounts" to="faq#accounts">
+                        Accounts
+                      </Link>,
+                    ]}
+                  />
+                </>,
+                <NavLink key="schema-browser" to="schema-browser">
+                  Schema-Browser
+                </NavLink>,
+                <NavLink key="history" to="history">
+                  History
+                </NavLink>,
+                <>
+                  <NavLink key="client" to="client">
+                    Kafka Client Setup
+                  </NavLink>
+                  <SideNavSub
+                    base="client"
+                    key="client-sub"
+                    items={[
+                      <Link key="python" to="client#python">
+                        Python
+                      </Link>,
+                      <Link key="nodejs" to="client#nodejs">
+                        Node.js
+                      </Link>,
+                      <Link key="c" to="client#c">
+                        C
+                      </Link>,
+                      <Link key="c-1" to="client#c-1">
+                        C#
+                      </Link>,
+                    ]}
+                  />
+                </>,
+                <NavLink key="producers" to="producers">
+                  New Notice Producers
+                </NavLink>,
+                <NavLink key="roadmap" to="roadmap">
+                  Road Map
+                </NavLink>,
+              ]}
+            />
+          </div>
+        )}
+
+        <div className={isSchemaBrowser ? 'grid-col-12' : 'desktop:grid-col-9'}>
+          <Outlet />
         </div>
       </div>
     </GridContainer>
--- app/routes/docs/schema-browser.tsx
+++ app/routes/docs/schema-browser.tsx
@@ -5,12 +5,11 @@
  *
  * SPDX-License-Identifier: NASA-1.3
  */
-import { NavLink, Outlet, useLoaderData } from '@remix-run/react'
-import { Button, Icon } from '@trussworks/react-uswds'
+import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
+import { Icon } from '@trussworks/react-uswds'
 import dirTree from 'directory-tree'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import { useSideNavContext } from '../docs'
 import { SideNav, SideNavSub } from '~/components/SideNav'
 import { feature } from '~/lib/env.server'
 
@@ -32,37 +31,21 @@ export async function loader() {
 
 export default function Schema() {
   const { localDataTree } = useLoaderData<typeof loader>()
-  const { showSideNav, setShowSideNav } = useSideNavContext()
 
   const items: React.ReactNode[] = ([localDataTree] as SchemaTreeItem[])
     .filter((x) => !x.name.includes('.example.json'))
     .map(RenderSchemaTreeItem)
 
-  useEffect(() => {
-    setShowSideNav(false)
-  }, [setShowSideNav])
-
   return (
     <>
       <div className="grid-row grid-gap">
         <div className="desktop:grid-col-3">
-          <Button
-            type="button"
-            onClick={() => setShowSideNav(!showSideNav)}
-            className="margin-bottom-1"
-            unstyled
-          >
+          <Link to="/docs" className="margin-bottom-1">
             <div className="position-relative">
-              {showSideNav ? (
-                <Icon.ArrowBack className="position-absolute top-0 left-0" />
-              ) : (
-                <Icon.ArrowForward className="position-absolute top-0 left-0" />
-              )}
+              <Icon.ArrowBack className="position-absolute top-0 left-0" />
             </div>
-            <span className="padding-left-2">
-              {showSideNav ? 'Hide' : 'Show'} Docs Nav
-            </span>
-          </Button>
+            <span className="padding-left-2">Back</span>
+          </Link>
           <SideNav items={items} />
         </div>
         <div className="desktop:grid-col-9">
