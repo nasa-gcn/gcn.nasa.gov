6ae2a559fcc3e9cfd7272898a1db0d29c9c8685b
Tyler Barna <57914086+tylerbarna@users.noreply.github.com>
2023-07-20 14:52:33 -0400

    Update app/routes/circulars/index.tsx
    Co-authored-by: Leo Singer <leo.p.singer@nasa.gov>

--- app/routes/circulars/index.tsx
+++ app/routes/circulars/index.tsx
@@ -285,9 +285,10 @@ export default function () {
                     submit(form)
                   }}
                 >
+                  <option value="10">10</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
                   <option value="100">100</option>
-                  <option value="250">250</option>
-                  <option value="500">500</option>
                 </Dropdown>
               </div>
             </div>
