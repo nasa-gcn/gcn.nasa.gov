48b9808b22ec4d20266fcadecad7655fdd281f90
Tyler Barna <57914086+tylerbarna@users.noreply.github.com>
2023-07-26 10:31:54 -0500

    Choose number of results to display on circulars search page (#1104)
    Fixes #1103. 
    
    Co-authored-by: dakota002 <dakota.d@live.com>
    Co-authored-by: Leo Singer <leo.p.singer@nasa.gov>

--- app/routes/circulars/index.tsx
+++ app/routes/circulars/index.tsx
@@ -19,9 +19,11 @@ import {
   ButtonGroup,
   Icon,
   Label,
+  Select,
   TextInput,
 } from '@trussworks/react-uswds'
 import classNames from 'classnames'
+import clamp from 'lodash/clamp'
 import { useState } from 'react'
 
 import type { action } from '../circulars'
@@ -30,8 +32,6 @@ import Hint from '~/components/Hint'
 import { usePagination } from '~/lib/pagination'
 
 import searchImg from 'app/theme/img/usa-icons-bg/search--white.svg'
-
-const limit = 100
 
 export async function loader({ request: { url } }: DataFunctionArgs) {
   const { searchParams } = new URL(url)
@@ -42,6 +42,7 @@ export async function loader({ request: { url } }: DataFunctionArgs) {
   const startDate = searchParams.get('startDate') || undefined
   const endDate = searchParams.get('endDate') || undefined
   const page = parseInt(searchParams.get('page') || '1')
+  const limit = clamp(parseInt(searchParams.get('limit') || '100'), 1, 100)
   const results = await search({
     query,
     page: page - 1,
@@ -55,17 +56,20 @@ export async function loader({ request: { url } }: DataFunctionArgs) {
 
 function getPageLink({
   page,
+  limit,
   query,
   startDate,
   endDate,
 }: {
   page: number
+  limit?: number
   query?: string
   startDate?: string
   endDate?: string
 }) {
   const searchParams = new URLSearchParams()
   if (page > 1) searchParams.set('page', page.toString())
+  if (limit && limit != 100) searchParams.set('limit', limit.toString())
   if (query) searchParams.set('query', query)
   if (startDate) searchParams.set('startDate', startDate)
   if (endDate) searchParams.set('endDate', endDate)
@@ -81,6 +85,7 @@ function Pagination({
 }: {
   page: number
   totalPages: number
+  limit?: number
   query?: string
   startDate?: string
   endDate?: string
@@ -177,9 +182,10 @@ export default function () {
   const allItems = [...(newItem ? [newItem] : []), ...(items || [])]
 
   const [searchParams] = useSearchParams()
-  const query = searchParams.get('query') ?? undefined
-  const startDate = searchParams.get('startDate') ?? undefined
-  const endDate = searchParams.get('endDate') ?? undefined
+  const limit = searchParams.get('limit') || '100'
+  const query = searchParams.get('query') || undefined
+  const startDate = searchParams.get('startDate') || undefined
+  const endDate = searchParams.get('endDate') || undefined
 
   let searchString = searchParams.toString()
   if (searchString) searchString = `?${searchString}`
@@ -208,6 +214,7 @@ export default function () {
         <Form
           className="display-inline-block usa-search usa-search--small"
           role="search"
+          id="searchForm"
         >
           <Label srOnly={true} htmlFor="query">
             Search
@@ -263,15 +270,39 @@ export default function () {
               </li>
             ))}
           </ol>
-          {totalPages > 1 && (
-            <Pagination
-              query={query}
-              page={page}
-              totalPages={totalPages}
-              startDate={startDate}
-              endDate={endDate}
-            />
-          )}
+          <div className="display-flex flex-row flex-wrap">
+            <div className="display-flex flex-align-self-center margin-right-2 width-auto">
+              <div>
+                <Select
+                  id="limit"
+                  className="width-auto height-5 padding-y-0 margin-y-0"
+                  name="limit"
+                  defaultValue="100"
+                  form="searchForm"
+                  onChange={({ target: { form } }) => {
+                    submit(form)
+                  }}
+                >
+                  <option value="10">10 / page</option>
+                  <option value="20">20 / page</option>
+                  <option value="50">50 / page</option>
+                  <option value="100">100 / page</option>
+                </Select>
+              </div>
+            </div>
+            <div className="display-flex flex-fill">
+              {totalPages > 1 && (
+                <Pagination
+                  query={query}
+                  page={page}
+                  limit={parseInt(limit)}
+                  totalPages={totalPages}
+                  startDate={startDate}
+                  endDate={endDate}
+                />
+              )}
+            </div>
+          </div>
         </>
       )}
     </>
