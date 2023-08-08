58cde3ae28cd0739c7dba8dc7f2e6dbff7fca709
Vidushi-GitHub <vid.sharma.20@gmail.com>
2023-08-08 17:30:33 -0400

    Schema Best Practices

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -19,159 +19,129 @@ export default function () {
   return (
     <GridContainer className="usa-section">
       <div className="grid-row grid-gap">
-        <div className="desktop:grid-col-3">
-          <SideNav
-            items={[
-              <NavLink key="." to="." end>
-                About GCN
-              </NavLink>,
-              <>
-                <NavLink key="circulars" to="circulars">
-                  Circulars
-                </NavLink>
-                <SideNavSub
-                  base="circulars"
-                  items={[
-                    <NavLink key="subscribing" to="circulars" end>
-                      About
-                    </NavLink>,
-                    <NavLink key="subscribing" to="circulars/subscribing">
-                      Subscribing
-                    </NavLink>,
-                    <NavLink key="submitting" to="circulars/submitting">
-                      Submitting
-                    </NavLink>,
-                    <NavLink key="styleguide" to="circulars/styleguide">
-                      Style Guide
-                    </NavLink>,
-                    <NavLink key="archive" to="circulars/archive">
-                      Archive
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <>
-                <NavLink key="contributing" to="contributing">
-                  Contributing
-                </NavLink>
-                <SideNavSub
-                  base="contributing"
-                  key="contributing-sub"
-                  items={[
-                    <NavLink key="index" to="contributing" end>
-                      Getting Started
-                    </NavLink>,
-                    <NavLink key="github" to="contributing/github">
-                      GitHub
-                    </NavLink>,
-                    <NavLink
-                      key="configuration"
-                      to="contributing/configuration"
-                    >
-                      Configuration
-                    </NavLink>,
-                    <NavLink
-                      key="feature-flags"
-                      to="contributing/feature-flags"
-                    >
-                      Feature Flags
-                    </NavLink>,
-                    <NavLink key="deployment" to="contributing/deployment">
-                      Deployment
-                    </NavLink>,
-                    <NavLink key="email" to="contributing/email">
-                      Email Data Flow
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <>
-                <NavLink key="faq" to="faq">
-                  Frequently Asked Questions
-                </NavLink>
-                <SideNavSub
-                  base="faq"
-                  key="faq-sub"
-                  items={[
-                    <Link key="kafka" to="faq#kafka">
-                      Kafka
-                    </Link>,
-                    <Link key="circulars" to="faq#circulars">
-                      Circulars
-                    </Link>,
-                    <Link key="accounts" to="faq#accounts">
-                      Accounts
-                    </Link>,
-                    <Link key="accounts" to="faq#operations">
-                      Operations
-                    </Link>,
-                  ]}
-                />
-              </>,
-              <NavLink key="history" to="history">
-                History
-              </NavLink>,
-              <>
-                <NavLink key="client" to="client">
-                  Kafka Client Setup
-                </NavLink>
-                <SideNavSub
-                  base="client"
-                  key="client-sub"
-                  items={[
-                    <Link key="python" to="client#python">
-                      Python
-                    </Link>,
-                    <Link key="nodejs" to="client#nodejs">
-                      Node.js
-                    </Link>,
-                    <Link key="c" to="client#c">
-                      C
-                    </Link>,
-                    <Link key="c-1" to="client#c-1">
-                      C#
-                    </Link>,
-                    <NavLink key="code-samples" to="client/samples">
-                      Sample Code
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <>
-                <NavLink key="notices" to="notices">
-                  Notices
-                </NavLink>
-                <SideNavSub
-                  base="notices"
-                  items={[
-                    <NavLink key="about" to="notices" end>
-                      About
-                    </NavLink>,
-                    <NavLink key="consuming" to="notices/consuming">
-                      Consuming
-                    </NavLink>,
-                    <NavLink key="producing" to="notices/producers">
-                      Producing
-                    </NavLink>,
-                    <NavLink key="schema" to="notices/schema">
-                      Unified Schema
-                    </NavLink>,
-                    <NavLink key="archive" to="notices/archive">
-                      Archive
-                    </NavLink>,
-                  ]}
-                />
-              </>,
-              <NavLink key="roadmap" to="roadmap">
-                Road Map
-              </NavLink>,
-              <NavLink key="schema" to="schema">
-                Schema Browser
-              </NavLink>,
-            ]}
-          />
-        </div>
-        <div className="desktop:grid-col-9">
+        {!isSchemaBrowser && (
+          <div className="desktop:grid-col-3">
+            <SideNav
+              items={[
+                <NavLink key="." to="." end>
+                  About GCN
+                </NavLink>,
+                <>
+                  <NavLink key="circulars" to="circulars">
+                    Circulars
+                  </NavLink>
+                  <SideNavSub
+                    base="circulars"
+                    items={[
+                      <NavLink key="subscribing" to="circulars" end>
+                        About
+                      </NavLink>,
+                      <NavLink key="subscribing" to="circulars/subscribing">
+                        Subscribing
+                      </NavLink>,
+                      <NavLink key="submitting" to="circulars/submitting">
+                        Submitting
+                      </NavLink>,
+                      <NavLink key="styleguide" to="circulars/styleguide">
+                        Style Guide
+                      </NavLink>,
+                      <NavLink key="archive" to="circulars/archive">
+                        Archive
+                      </NavLink>,
+                    ]}
+                  />
+                </>,
+                <>
+                  <NavLink key="contributing" to="contributing">
+                    Contributing
+                  </NavLink>
+                  <SideNavSub
+                    base="contributing"
+                    key="contributing-sub"
+                    items={[
+                      <NavLink key="index" to="contributing" end>
+                        Getting Started
+                      </NavLink>,
+                      <NavLink
+                        key="feature-flags"
+                        to="contributing/feature-flags"
+                      >
+                        Feature Flags
+                      </NavLink>,
+                      <NavLink key="deployment" to="contributing/deployment">
+                        Deployment
+                      </NavLink>,
+                      <NavLink key="email" to="contributing/email">
+                        Email Data Flow
+                      </NavLink>,
+                    ]}
+                  />
+                </>,
+                <>
+                  <NavLink key="faq" to="faq">
+                    Frequently Asked Questions
+                  </NavLink>
+                  <SideNavSub
+                    base="faq"
+                    key="faq-sub"
+                    items={[
+                      <Link key="kafka" to="faq#kafka">
+                        Kafka
+                      </Link>,
+                      <Link key="circulars" to="faq#circulars">
+                        Circulars
+                      </Link>,
+                      <Link key="accounts" to="faq#accounts">
+                        Accounts
+                      </Link>,
+                    ]}
+                  />
+                </>,
+                ...(enableSchemaBrowser
+                  ? [
+                      <NavLink key="schema" to="schema/latest/gcn">
+                        Schema Browser
+                      </NavLink>,
+                    ]
+                  : []),
+                <NavLink key="history" to="history">
+                  History
+                </NavLink>,
+                <>
+                  <NavLink key="client" to="client">
+                    Kafka Client Setup
+                  </NavLink>
+                  <SideNavSub
+                    base="client"
+                    key="client-sub"
+                    items={[
+                      <Link key="python" to="client#python">
+                        Python
+                      </Link>,
+                      <Link key="nodejs" to="client#nodejs">
+                        Node.js
+                      </Link>,
+                      <Link key="c" to="client#c">
+                        C
+                      </Link>,
+                      <Link key="c-1" to="client#c-1">
+                        C#
+                      </Link>,
+                    ]}
+                  />
+                </>,
+                <NavLink key="producers" to="producers">
+                  New Notice Producers
+                </NavLink>,
+                <NavLink key="roadmap" to="roadmap">
+                  Road Map
+                </NavLink>,
+              ]}
+            />
+          </div>
+        )}
+
+        <div className={isSchemaBrowser ? 'grid-col-12' : 'desktop:grid-col-9'}>
           <Outlet />
         </div>
       </div>
