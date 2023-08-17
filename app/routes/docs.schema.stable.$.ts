d130370e58007743ca08287cf7110c0dd47e369d
Leo Singer <leo.singer@ligo.org>
2023-08-17 10:54:40 -0400

    Rename schema-data.ts to schema-data.server.ts
    Ensures that it is excluded from the client-side bundle.

--- app/routes/docs.schema.stable.$.ts
+++ app/routes/docs.schema.stable.$.ts
@@ -1,7 +1,7 @@
 import type { DataFunctionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 
-import { getLatestRelease } from '~/lib/schema-data'
+import { getLatestRelease } from '~/lib/schema-data.server'
 
 export async function loader({ params: { '*': path } }: DataFunctionArgs) {
   return redirect(`/docs/schema/${await getLatestRelease()}/${path}`)
