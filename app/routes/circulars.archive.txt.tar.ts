96a43fb009d8f6cf07c96c623328680c0b115692
Courey Elliott <coureyelliott@gmail.com>
2023-08-23 10:37:59 -0400

    fixing comment

--- app/routes/circulars.archive.txt.tar.ts
+++ app/routes/circulars.archive.txt.tar.ts
@@ -1,10 +1,10 @@
-// /*!
-//  * Copyright © 2022 United States Government as represented by the Administrator
-//  * of the National Aeronautics and Space Administration. No copyright is claimed
-//  * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
-//  *
-//  * SPDX-License-Identifier: NASA-1.3
-//  */
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
 import type { DataFunctionArgs } from '@remix-run/node'
 
 import { makeTarFile } from './circulars/circulars.server'
