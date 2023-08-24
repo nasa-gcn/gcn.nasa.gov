ddda07e9e8d10ed148578c09c202679feb2dad6c
dakota002 <dakota.d@live.com>
2023-08-24 14:50:53 -0400

    Updates c# code sample

--- app/components/ClientSampleCode.tsx
+++ app/components/ClientSampleCode.tsx
@@ -479,6 +479,10 @@ export function ClientSampleCode({
                   {
                     throw;
                   }
+                  else
+                  {
+                    Console.WriteLine(ex.Message);
+                  }
                 }
               }
             }
