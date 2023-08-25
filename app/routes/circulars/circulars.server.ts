c3b53c2086d550457813a3acfd5a0ad772db2e0b
Leo Singer <leo.singer@ligo.org>
2023-08-24 22:09:08 -0400

    Minor clode cleanup in makeTarFile

--- app/routes/circulars/circulars.server.ts
+++ app/routes/circulars/circulars.server.ts
@@ -270,30 +270,36 @@ export async function circularRedirect(query: string) {
   }
 }
 
-async function* getAllRecords(): AsyncGenerator<Circular[], void, unknown> {
+async function* getAllRecords() {
   const db = await tables()
   const client = db._doc as unknown as DynamoDBDocument
   const TableName = db.name('circulars')
   const pages = paginateScan({ client }, { TableName })
 
   for await (const page of pages) {
-    const items: Circular[] = page.Items as Circular[]
-    yield items
+    yield page.Items as Circular[]
   }
+}
+
+const formatters: Record<string, (circular: Circular) => string> = {
+  json({ sub, ...circular }) {
+    return JSON.stringify(circular, null, 2)
+  },
+  txt: formatCircular,
 }
 
 export async function makeTarFile(fileType: 'json' | 'txt'): Promise<Blob> {
   return new Promise(async (resolve, reject) => {
+    const formatter = formatters[fileType]
     const pack = tarPack()
-    const chunks: Uint8Array[] = []
+    const chunks: Buffer[] = []
 
     pack.on('data', (chunk) => {
       chunks.push(chunk)
     })
 
     pack.on('end', () => {
-      const tarData = new Uint8Array(Buffer.concat(chunks))
-      resolve(new Blob([tarData], { type: 'application/x-tar' }))
+      resolve(new Blob(chunks, { type: 'application/x-tar' }))
     })
 
     pack.on('error', (err) => {
@@ -302,20 +308,8 @@ export async function makeTarFile(fileType: 'json' | 'txt'): Promise<Blob> {
 
     for await (const circularArray of getAllRecords()) {
       for (const circular of circularArray) {
-        if (fileType === 'txt') {
-          const txt_entry = pack.entry(
-            { name: `archive.txt/${circular.circularId}.txt` },
-            formatCircular(circular)
-          )
-          txt_entry.end()
-        } else if (fileType === 'json') {
-          delete circular.sub
-          const json_entry = pack.entry(
-            { name: `archive.json/${circular.circularId}.json` },
-            JSON.stringify(circular, null, 2)
-          )
-          json_entry.end()
-        }
+        const name = `archive.${fileType}/${circular.circularId}.${fileType}`
+        pack.entry({ name }, formatter(circular)).end()
       }
     }
 
