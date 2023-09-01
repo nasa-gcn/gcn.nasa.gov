314a667e80f03a9b85e0e4d857030abc91406376
Leo Singer <leo.singer@ligo.org>
2023-08-31 22:42:16 -0400

    Deploy static assets to a subdir of the static bucket
    Upload built assets to the app/ directory of the static bucket.
    
    The Lambdas themselves can now write to the bucket with the
    exception of the app/ directory.
    
    See #1316.

--- src/plugins/lambdaMayNotWriteToStaticBucket.mjs
+++ src/plugins/lambdaMayNotWriteToStaticBucket.mjs
@@ -6,15 +6,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// The Lambda function should not be able to modify the static bucket.
+// The Lambda function should not be able to modify the deployment folder in the static bucket.
 export const deploy = {
   start({ cloudformation }) {
-    cloudformation.Resources.Role.Properties.Policies =
-      cloudformation.Resources.Role.Properties.Policies.map((policy) => {
-        if (policy.PolicyName == 'ArcStaticBucketPolicy')
-          policy.PolicyDocument.Statement[0].Action = ['s3:GetObject']
-        return policy
-      })
+    cloudformation.Resources.Role.Properties.Policies.push({
+      PolicyName: 'ArcStaticBucketDenyWriteAppDirectoryPolicy',
+      PolicyDocument: {
+        Statement: [
+          {
+            Effect: 'Deny',
+            Action: ['s3:PutObject', 's3:PutObjectAcl', 's3:DeleteObject'],
+            Resource: [
+              {
+                'Fn::Sub': [
+                  `arn:aws:s3:::\${bukkit}/app/*`,
+                  {
+                    bukkit: {
+                      Ref: 'StaticBucket',
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    })
     return cloudformation
   },
 }
