57a384a31ede7bf7aa1c0c337bbc98a991c07681
Courey Elliott <courey.elliott@gmail.com>
2023-09-01 14:42:26 -0400

    adding missing file

---
+++ app/scheduled/circulars/circularAction.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import type { Circular } from '~/routes/circulars/circulars.lib'
+
+export interface CircularAction<CircularActionContext> {
+  initialContext: () => CircularActionContext
+  currentContext: CircularActionContext
+  callback: (
+    circularArray: Circular[],
+    context: CircularActionContext
+  ) => Promise<CircularActionContext>
+  finalize: (context: CircularActionContext) => Promise<void>
+}
+
+export interface CircularActionContext {
+  context: object
+}
