141c4da54c5cdbb010de2e66711cee55da2ab298
Dakota Dutko <dakota.d@live.com>
2023-09-06 09:52:58 -0400

    Add GitHub instructions (#1317)

--- app/routes/docs.tsx
+++ app/routes/docs.tsx
@@ -64,6 +64,9 @@ export default function () {
                     <NavLink key="index" to="contributing" end>
                       Getting Started
                     </NavLink>,
+                    <NavLink key="github" to="contributing/github">
+                      GitHub
+                    </NavLink>,
                     <NavLink
                       key="configuration"
                       to="contributing/configuration"
