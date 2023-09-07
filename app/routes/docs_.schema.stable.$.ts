a0799b63142506cc4af59fd3eb1290893066c6f6
Leo Singer <leo.singer@ligo.org>
2023-09-07 09:23:39 -0400

    Add cache control headers to redirects

--- app/routes/docs_.schema.stable.$.ts
+++ app/routes/docs_.schema.stable.$.ts
@@ -1,8 +1,11 @@
 import type { DataFunctionArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
 
+import { publicStaticShortTermCacheControlHeaders } from '~/lib/headers.server'
 import { getLatestRelease } from '~/lib/schema-data.server'
 
 export async function loader({ params: { '*': path } }: DataFunctionArgs) {
-  return redirect(`/docs/schema/${await getLatestRelease()}/${path}`)
+  return redirect(`/docs/schema/${await getLatestRelease()}/${path}`, {
+    headers: publicStaticShortTermCacheControlHeaders,
+  })
 }
--- app/routes/schema.stable.$.ts
+++ app/routes/schema.stable.$.ts
@@ -7,10 +7,12 @@
  */
 import { type DataFunctionArgs, redirect } from '@remix-run/node'
 
+import { publicStaticShortTermCacheControlHeaders } from '~/lib/headers.server'
 import { getLatestRelease } from '~/lib/schema-data.server'
 
 export async function loader({ params: { '*': path } }: DataFunctionArgs) {
   return redirect(
-    `https://raw.githubusercontent.com/nasa-gcn/gcn-schema/${await getLatestRelease()}/${path}`
+    `https://raw.githubusercontent.com/nasa-gcn/gcn-schema/${await getLatestRelease()}/${path}`,
+    { headers: publicStaticShortTermCacheControlHeaders }
   )
 }
