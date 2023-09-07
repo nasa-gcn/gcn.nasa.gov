5dc2410a9cbd6f9dfd3e59e8e3d2bb9c33ca4c07
Leo Singer <leo.singer@ligo.org>
2023-09-07 13:18:52 -0400

    Clean up GCN Circulars archive Lambda
    - Refactor the action into a closure to simplify the code
    - Refactor the endpoint for retrieving an individual GCN Circular
      as JSON so that the formatting is consistent with the tarball
    - Stream the tarball to S3 to decrease memory usage
    - Allow, but do not require, action callbacks to be promises

--- app/routes/circulars.$circularId[.]json.ts
+++ app/routes/circulars.$circularId[.]json.ts
@@ -6,20 +6,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import type { DataFunctionArgs } from '@remix-run/node'
-import { json } from '@remix-run/node'
+import invariant from 'tiny-invariant'
 
+import { formatCircularJson } from './circulars/circulars.lib'
 import { get } from './circulars/circulars.server'
 import { origin } from '~/lib/env.server'
 import { getCanonicalUrlHeaders } from '~/lib/headers.server'
 
 export async function loader({ params: { circularId } }: DataFunctionArgs) {
-  if (!circularId)
-    throw new Response('circularId must be defined', { status: 400 })
+  invariant(circularId)
   const result = await get(parseFloat(circularId))
-  delete result.sub
-  return json(result, {
-    headers: getCanonicalUrlHeaders(
-      new URL(`/circulars/${circularId}`, origin)
-    ),
+  return new Response(formatCircularJson(result), {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      ...getCanonicalUrlHeaders(new URL(`/circulars/${circularId}`, origin)),
+    },
   })
 }
