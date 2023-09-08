c5b4794159a4ace9c7b6cfddcd4cb9062d29ec56
Leo Singer <leo.singer@ligo.org>
2023-09-08 10:56:01 -0400

    Switch to using static routes for archive redirects
    The dynamic route `circulars.$circularId.tsx` was conflicting with
    the dynamic route `circulars.archive[.]$suffix[.].tar.ts`.

---
+++ app/routes/circulars.archive[.]txt[.]tar.ts
@@ -0,0 +1,17 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { redirect } from '@remix-run/node'
+
+import { publicStaticShortTermCacheControlHeaders } from '~/lib/headers.server'
+import { getArchiveURL } from '~/scheduled/circulars/uploadTar'
+
+export function loader() {
+  return redirect(getArchiveURL('txt'), {
+    headers: publicStaticShortTermCacheControlHeaders,
+  })
+}
