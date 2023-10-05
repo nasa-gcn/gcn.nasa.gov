e3f301a8ffa0786bc75eca5a5017a339dc3c3a6a
Courey Elliott <coureyelliott@gmail.com>
2023-10-05 13:40:33 -0400

    removing comment

--- app/routes/circulars.$circularId/route.tsx
+++ app/routes/circulars.$circularId/route.tsx
@@ -44,7 +44,7 @@ export async function loader({
   return {
     hasSynonyms,
     isModerator,
-    ...result, // Include the existing data
+    ...result,
     headers: getCanonicalUrlHeaders(
       new URL(`/circulars/${circularId}`, origin)
     ),
