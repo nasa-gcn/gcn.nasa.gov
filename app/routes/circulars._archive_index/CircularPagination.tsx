ec454ea3a88a94dd5a17deb8d73790101a09b7ec
Courey Elliott <coureyelliott@gmail.com>
2023-10-13 12:03:53 -0400

    breaking apart circulars._index route separately

---
+++ app/routes/circulars._archive_index/CircularPagination.tsx
@@ -0,0 +1,145 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { Link } from '@remix-run/react'
+import { Icon } from '@trussworks/react-uswds'
+import classNames from 'classnames'
+
+import { usePagination } from '~/lib/pagination'
+
+function getPageLink({
+  page,
+  limit,
+  query,
+  startDate,
+  endDate,
+}: {
+  page: number
+  limit?: number
+  query?: string
+  startDate?: string
+  endDate?: string
+}) {
+  const searchParams = new URLSearchParams()
+  if (page > 1) searchParams.set('page', page.toString())
+  if (limit && limit != 100) searchParams.set('limit', limit.toString())
+  if (query) searchParams.set('query', query)
+  if (startDate) searchParams.set('startDate', startDate)
+  if (endDate) searchParams.set('endDate', endDate)
+
+  const searchString = searchParams.toString()
+  return searchString && `?${searchString}`
+}
+
+export default function ({
+  page,
+  totalPages,
+  ...queryStringProps
+}: {
+  page: number
+  totalPages: number
+  limit?: number
+  query?: string
+  startDate?: string
+  endDate?: string
+}) {
+  const pages = usePagination({ currentPage: page, totalPages })
+  return (
+    <nav aria-label="Pagination" className="usa-pagination">
+      <ul className="usa-pagination__list">
+        {pages.map((pageProps, i) => {
+          switch (pageProps.type) {
+            case 'prev':
+              if (totalPages >= page) {
+                return (
+                  <li
+                    className="usa-pagination__item usa-pagination__arrow"
+                    key={i}
+                  >
+                    <Link
+                      to={getPageLink({
+                        page: pageProps.number,
+                        ...queryStringProps,
+                      })}
+                      className="usa-pagination__link usa-pagination__previous-page"
+                      aria-label="Previous page"
+                    >
+                      <Icon.NavigateBefore role="presentation" />
+                      <span className="usa-pagination__link-text">
+                        Previous
+                      </span>
+                    </Link>
+                  </li>
+                )
+              } else {
+                return null
+              }
+            case 'overflow':
+              if (totalPages >= page) {
+                return (
+                  <li
+                    className="usa-pagination__item usa-pagination__overflow"
+                    role="presentation"
+                    key={i}
+                  >
+                    <span>…</span>
+                  </li>
+                )
+              } else {
+                return null
+              }
+            case 'next':
+              if (totalPages > page) {
+                return (
+                  <li
+                    className="usa-pagination__item usa-pagination__arrow"
+                    key={i}
+                  >
+                    <Link
+                      to={getPageLink({
+                        page: pageProps.number,
+                        ...queryStringProps,
+                      })}
+                      className="usa-pagination__link usa-pagination__next-page"
+                      aria-label="Next page"
+                    >
+                      <Icon.NavigateNext role="presentation" />
+                      <span className="usa-pagination__link-text">Next</span>
+                    </Link>
+                  </li>
+                )
+              } else {
+                return null
+              }
+            default:
+              return (
+                <li
+                  className="usa-pagination__item usa-pagination__page-no"
+                  key={i}
+                >
+                  <Link
+                    to={getPageLink({
+                      page: pageProps.number,
+                      ...queryStringProps,
+                    })}
+                    className={classNames('usa-pagination__button', {
+                      'usa-current': pageProps.isCurrent,
+                    })}
+                    prefetch="render"
+                    aria-label={`Page ${pageProps.number}`}
+                    aria-current={pageProps.isCurrent}
+                  >
+                    {pageProps.number}
+                  </Link>
+                </li>
+              )
+          }
+        })}
+      </ul>
+    </nav>
+  )
+}
---
+++ app/routes/circulars._archive_index/route.tsx
@@ -0,0 +1,181 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import type { DataFunctionArgs } from '@remix-run/node'
+import {
+  Form,
+  Link,
+  useActionData,
+  useLoaderData,
+  useSearchParams,
+  useSubmit,
+} from '@remix-run/react'
+import {
+  Button,
+  ButtonGroup,
+  Icon,
+  Label,
+  Select,
+  TextInput,
+} from '@trussworks/react-uswds'
+import clamp from 'lodash/clamp'
+import { useState } from 'react'
+
+import type { Circular } from '../circulars/circulars.lib'
+import { circularRedirect, search } from '../circulars/circulars.server'
+import type { action } from '../circulars/route'
+import CircularPagination from './CircularPagination'
+import CircularsHeader from './CircularsHeader'
+import CircularsIndex from './CircularsIndex'
+import DateSelectorButton from './DateSelectorButton'
+import Hint from '~/components/Hint'
+import { useFeature } from '~/root'
+
+import searchImg from 'nasawds/src/img/usa-icons-bg/search--white.svg'
+
+export async function loader({ request: { url } }: DataFunctionArgs) {
+  const { searchParams } = new URL(url)
+  const query = searchParams.get('query') || undefined
+  if (query) {
+    await circularRedirect(query)
+  }
+  const startDate = searchParams.get('startDate') || undefined
+  const endDate = searchParams.get('endDate') || undefined
+  const page = parseInt(searchParams.get('page') || '1')
+  const limit = clamp(parseInt(searchParams.get('limit') || '100'), 1, 100)
+  const results = await search({
+    query,
+    page: page - 1,
+    limit,
+    startDate,
+    endDate,
+  })
+
+  return { page, ...results }
+}
+
+export default function () {
+  const newItem = useActionData<typeof action>()
+  const { items, page, totalPages, totalItems } = useLoaderData<typeof loader>()
+  const featureCircularsFilterByDate = useFeature('CIRCULARS_FILTER_BY_DATE')
+
+  // Concatenate items from the action and loader functions
+  const allItems = [
+    ...(newItem ? [newItem] : []),
+    ...(items || []),
+  ] as Circular[]
+
+  const [searchParams] = useSearchParams()
+  const limit = searchParams.get('limit') || '100'
+  const query = searchParams.get('query') || undefined
+  const startDate = searchParams.get('startDate') || undefined
+  const endDate = searchParams.get('endDate') || undefined
+
+  let searchString = searchParams.toString()
+  if (searchString) searchString = `?${searchString}`
+
+  const [inputQuery, setInputQuery] = useState(query)
+  const clean = inputQuery === query
+
+  const submit = useSubmit()
+
+  return (
+    <>
+      <CircularsHeader />
+      <ButtonGroup className="position-sticky top-0 bg-white margin-bottom-1 padding-top-1">
+        <Form
+          className="display-inline-block usa-search usa-search--small"
+          role="search"
+          id="searchForm"
+        >
+          <Label srOnly={true} htmlFor="query">
+            Search
+          </Label>
+          <TextInput
+            autoFocus
+            id="query"
+            name="query"
+            type="search"
+            defaultValue={inputQuery}
+            placeholder="Search"
+            aria-describedby="searchHint"
+            onChange={({ target: { form, value } }) => {
+              setInputQuery(value)
+              if (!value) submit(form)
+            }}
+          />
+          <Button type="submit">
+            <img
+              src={searchImg}
+              className="usa-search__submit-icon"
+              alt="Search"
+            />
+          </Button>
+        </Form>
+        {featureCircularsFilterByDate && (
+          <DateSelectorButton startDate={startDate} endDate={endDate} />
+        )}
+        <Link to={`/circulars/new${searchString}`}>
+          <Button
+            type="button"
+            className="height-4 padding-top-0 padding-bottom-0"
+          >
+            <Icon.Edit role="presentation" /> New
+          </Button>
+        </Link>
+      </ButtonGroup>
+      <Hint id="searchHint">
+        Search for Circulars by submitter, subject, or body text (e.g. 'Fermi
+        GRB'). <br />
+        To navigate to a specific circular, enter the associated Circular ID
+        (e.g. 'gcn123', 'Circular 123', or '123').
+      </Hint>
+      {clean && (
+        <>
+          <CircularsIndex
+            allItems={allItems}
+            searchString={searchString}
+            totalItems={totalItems}
+          />
+          <div className="display-flex flex-row flex-wrap">
+            <div className="display-flex flex-align-self-center margin-right-2 width-auto">
+              <div>
+                <Select
+                  id="limit"
+                  className="width-auto height-5 padding-y-0 margin-y-0"
+                  name="limit"
+                  defaultValue="100"
+                  form="searchForm"
+                  onChange={({ target: { form } }) => {
+                    submit(form)
+                  }}
+                >
+                  <option value="10">10 / page</option>
+                  <option value="20">20 / page</option>
+                  <option value="50">50 / page</option>
+                  <option value="100">100 / page</option>
+                </Select>
+              </div>
+            </div>
+            <div className="display-flex flex-fill">
+              {totalPages > 1 && (
+                <CircularPagination
+                  query={query}
+                  page={page}
+                  limit={parseInt(limit)}
+                  totalPages={totalPages}
+                  startDate={startDate}
+                  endDate={endDate}
+                />
+              )}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
