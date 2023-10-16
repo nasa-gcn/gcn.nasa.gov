5ebdd15a0fe8807eabbecd8340a998d140f04912
tylerbarna <tylerpbarna@gmail.com>
2023-10-16 11:19:36 -0500

    restored button group z-order

--- app/routes/circulars._archive._index/route.tsx
+++ app/routes/circulars._archive._index/route.tsx
@@ -128,7 +128,7 @@ export default function () {
   return (
     <>
       <CircularsHeader />
-      <ButtonGroup className="position-sticky top-0 bg-white margin-bottom-1 padding-top-1">
+      <ButtonGroup className="position-sticky top-0 bg-white margin-bottom-1 padding-top-1 z-top">
         <Form
           className="display-inline-block usa-search usa-search--small"
           role="search"
