c25a3f51ddb7efd4fb46acc45498e9688a5d586d
Courey Elliott <coureyelliott@gmail.com>
2023-10-16 09:13:15 -0400

    Refactor circulars._index into a route module folder (#1524)

---
+++ app/routes/circulars._archive._index/DateSelectorButton.tsx
@@ -0,0 +1,54 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { Button, ButtonGroup, Icon } from '@trussworks/react-uswds'
+
+const dateSelectorLabels: Record<string, string> = {
+  hour: 'Last hour',
+  day: 'Last day',
+  week: 'Last week',
+  month: 'Last month',
+  year: 'Last year',
+  today: 'Today',
+  mtd: 'Month to date',
+  ytd: 'Year to date',
+}
+
+export default function ({
+  startDate,
+  endDate,
+  expanded,
+  ...props
+}: {
+  startDate?: string
+  endDate?: string
+  expanded?: boolean
+} & Omit<Parameters<typeof ButtonGroup>[0], 'segmented' | 'children'>) {
+  const slimClasses = 'height-4 padding-y-0'
+
+  return (
+    <ButtonGroup type="segmented" {...props}>
+      <Button type="button" className={`${slimClasses} padding-x-2`}>
+        {(startDate && dateSelectorLabels[startDate]) ||
+          (startDate && endDate && (
+            <>
+              {startDate}—{endDate}
+            </>
+          )) ||
+          'Filter by date'}
+      </Button>
+      <Button type="button" className={`${slimClasses} padding-x-1`}>
+        <Icon.CalendarToday role="presentation" />
+        {expanded ? (
+          <Icon.ExpandLess role="presentation" />
+        ) : (
+          <Icon.ExpandMore role="presentation" />
+        )}
+      </Button>
+    </ButtonGroup>
+  )
+}
