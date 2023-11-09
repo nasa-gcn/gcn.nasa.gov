f79cbfc507b52710357f35e547b502543aa4195b
Leo Singer <leo.singer@ligo.org>
2023-11-09 12:53:06 -0500

    Add labs path with layout distinct from GCN

---
+++ app/routes/labs.tsx
@@ -0,0 +1,30 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { Outlet } from '@remix-run/react'
+
+import { feature } from '~/lib/env.server'
+import { type BreadcrumbHandle } from '~/root/Title'
+
+export const handle: BreadcrumbHandle = {
+  breadcrumb: 'ACROSS',
+}
+
+export function loader() {
+  return feature('LABS') || new Response(null, { status: 404 })
+}
+
+export default function () {
+  return (
+    <>
+      <h1>ACROSS</h1>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
