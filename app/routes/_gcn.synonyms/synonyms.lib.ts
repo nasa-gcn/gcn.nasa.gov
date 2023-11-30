d19e45a4a137aab4456d5d2f4fb30df0fe84a3f2
Courey Elliott <coureyelliott@gmail.com>
2023-11-30 11:16:10 -0500

    changes in response to review

--- app/routes/_gcn.synonyms/synonyms.lib.ts
+++ app/routes/_gcn.synonyms/synonyms.lib.ts
@@ -1,10 +1,10 @@
-// /*!
-//  * Copyright © 2023 United States Government as represented by the
-//  * Administrator of the National Aeronautics and Space Administration.
-//  * All Rights Reserved.
-//  *
-//  * SPDX-License-Identifier: Apache-2.0
-//  */
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 export interface Synonym {
   eventId: string
   synonymId: string
