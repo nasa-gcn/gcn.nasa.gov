a327acf11d70745a864af346a9cb198e28897899
Courey Elliott <coureyelliott@gmail.com>
2023-12-06 14:01:44 -0500

    using less css

--- app/routes/labs.missions/route.tsx
+++ app/routes/labs.missions/route.tsx
@@ -1,9 +1,21 @@
-import { Grid, GridContainer } from '@trussworks/react-uswds'
+import {
+  Grid,
+  GridContainer,
+  Identifier,
+  IdentifierIdentity,
+  IdentifierLogos,
+  IdentifierMasthead,
+} from '@trussworks/react-uswds'
 
 import fleet2 from './AstrophysicsFleet2.jpg'
 import fermiHeroImage from './Fermi_Earth_GWs.jpg'
+import type { BreadcrumbHandle } from '~/root/Title'
 
 import meatball from '~/components/meatball/logo.svg'
+
+export const handle: BreadcrumbHandle = {
+  breadcrumb: 'Missions',
+}
 
 export default function () {
   return (
@@ -13,15 +25,12 @@ export default function () {
         src={fermiHeroImage}
       />
       <GridContainer>
-        <Grid
-          row
-          className="margin-bottom-2 margin-top-5 border-bottom border-gray-50"
-        >
+        <Grid row className="border-bottom border-gray-50">
           <h1>Mutimessenger missions</h1>
         </Grid>
         <Grid row>
           <h3>Overview</h3>
-          <p className="line-height-mono-4">
+          <p>
             The joint discovery of gravitational waves and electromagnetic
             radiation from the binary neutron star merger GW170817 was a
             watershed moment for astrophysics. NASA missions played a critical
@@ -42,50 +51,44 @@ export default function () {
             will contribute significantly to the major science questions in this
             field.
           </p>
-          <div className="text-center margin-bottom-2">
+        </Grid>
+        <Grid row>
+          <div>
             <img
               alt="a diagram depicting all the missions in the fleet over time shaped as a swirl"
               src={fleet2}
-            ></img>
-            <figcaption className="text-thin text-italic">
+            />
+            <figcaption className="text-italic">
               The current NASA Astrophysics Fleet, flying and in development.
             </figcaption>
           </div>
         </Grid>
-        <div className="border-left-05 border-indigo">
-          <p className="measure-6 padding-left-1">
-            "The complementary nature of these missions makes the overall
-            capability of the portfolio more than the sum of its parts, and many
-            of the most exciting developments in contemporary astrophysics draw
-            on observations from several of these observatories simultaneously."
-          </p>
-        </div>
-        <section className="usa-identifier__section usa-identifier__section--masthead">
-          <div className="usa-identifier__container">
-            <div className="usa-identifier__logos">
-              <div className="usa-identifier__logo">
-                <img
-                  alt="nasa meatball logo"
-                  width="50"
-                  height="50"
-                  className="usa-identifier__logo-img"
-                  src={meatball}
-                ></img>
-              </div>
-            </div>
-            <section
-              className="usa-identifier__identity"
-              aria-label="quote source identifier"
-            >
-              <p className="usa-identifier__identity-disclaimer">
-                2019 Astrophysics Senior Review
-              </p>
-              <p className="usa-identifier__identity-domain">
-                Executive Summary, Page 2
-              </p>
-            </section>
+        <Grid>
+          <div className="border-left-05 border-indigo">
+            <p className="padding-left-1">
+              "The complementary nature of these missions makes the overall
+              capability of the portfolio more than the sum of its parts, and
+              many of the most exciting developments in contemporary
+              astrophysics draw on observations from several of these
+              observatories simultaneously."
+            </p>
+            <Identifier>
+              <IdentifierMasthead aria-label="Agency identifier">
+                <IdentifierLogos>
+                  <img
+                    alt="nasa meatball logo"
+                    width="50"
+                    height="50"
+                    src={meatball}
+                  />
+                </IdentifierLogos>
+                <IdentifierIdentity domain="Executive Summary, Page 2">
+                  2019 Astrophysics Senior Review
+                </IdentifierIdentity>
+              </IdentifierMasthead>
+            </Identifier>
           </div>
-        </section>
+        </Grid>
       </GridContainer>
     </>
   )
