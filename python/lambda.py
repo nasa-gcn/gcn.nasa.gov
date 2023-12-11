4b2cb7a9f80e8887f9629780d5a22371eb2c8fef
Jamie Kennea <kennea@swift.psu.edu>
2023-12-11 10:27:57 -0500

    Remove all but BurstCube from lambda.py

--- python/lambda.py
+++ python/lambda.py
@@ -8,8 +8,6 @@ from across_api.base.api import app
 from across_api.across.api import *  # noqa F401
 from across_api.across.api import *  # noqa F401
 from across_api.burstcube.api import *  # noqa F401
-from across_api.nicer.api import *  # noqa F401
-from across_api.swift.api import *  # noqa F401
 from env import feature
 
 if feature("LABS"):
