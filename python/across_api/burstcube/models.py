e8b324ee1b6a2e21a6d66f541e8652d1fb6ce6df
Jamie Kennea <jak51@psu.edu>
2023-12-12 07:22:15 -0500

    Swift BurstCubeTOO to use DynamoDB table rather than postgres

--- python/across_api/burstcube/models.py
+++ python/across_api/burstcube/models.py
@@ -1,43 +1,88 @@
-from datetime import datetime
+import hashlib
+from decimal import Decimal
+from typing import Optional
 
-from sqlalchemy import DateTime, Enum, Float, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from pydantic import computed_field
 
-from ..api_db import Base
-from ..base.models import TLEEntryModelBase
-from ..base.schema import BaseSchema
-from .schema import TOOReason, TOOStatus
+from ..api_db import dydbtable, dynamodb
+from ..base.models import DynamoDBBase, TLEEntryModelBase
+from ..base.schema import BaseSchema, OptionalCoordSchema
+
+# class BurstCubeTOOModel(Base):
+#     """SQLAlchemy model for BurstCubeTOO"""
+
+#     __tablename__ = "burstcube_too"
+
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     timestamp: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+#     username: Mapped[str] = mapped_column(String(20), nullable=False)
+#     trigger_mission: Mapped[str] = mapped_column(String(30), nullable=False)
+#     trigger_instrument: Mapped[str] = mapped_column(String(30), nullable=False)
+#     trigger_id: Mapped[str] = mapped_column(String(30), nullable=False)
+#     trigger_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+#     trigger_duration: Mapped[float] = mapped_column(Float, nullable=True)
+#     classification: Mapped[str] = mapped_column(String(30), nullable=True)
+#     justification: Mapped[str] = mapped_column(String(1000), nullable=True)
+#     ra: Mapped[float] = mapped_column(Float, nullable=True)
+#     dec: Mapped[float] = mapped_column(Float, nullable=True)
+#     error: Mapped[float] = mapped_column(Float, nullable=True)
+#     begin: Mapped[datetime] = mapped_column(DateTime)
+#     end: Mapped[datetime] = mapped_column(DateTime)
+#     exposure: Mapped[float] = mapped_column(Float)
+#     offset: Mapped[float] = mapped_column(Float)
+#     reason: Mapped[TOOReason] = mapped_column(
+#         Enum(TOOReason), nullable=False, default=TOOReason.none
+#     )
+#     too_status: Mapped[TOOStatus] = mapped_column(
+#         Enum(TOOStatus), nullable=False, default=TOOStatus.requested
+#     )
+#     too_info: Mapped[str] = mapped_column(String(), nullable=False)
 
 
-class BurstCubeTOOModel(Base):
-    """SQLAlchemy model for BurstCubeTOO"""
-
+class BurstCubeTOOModel(OptionalCoordSchema, DynamoDBBase):
     __tablename__ = "burstcube_too"
+    username: str
+    timestamp: str
+    trigger_mission: str
+    trigger_instrument: str
+    trigger_id: str
+    trigger_time: str
+    trigger_duration: Optional[Decimal] = None
+    classification: Optional[str] = None
+    justification: Optional[str] = None
+    begin: Optional[str] = None
+    end: Optional[str] = None
+    exposure: Decimal = Decimal(200)
+    offset: Decimal = Decimal(-50)
+    reason: str = "None"
+    too_status: str = "Requested"
+    too_info: str = ""
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    timestamp: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    username: Mapped[str] = mapped_column(String(20), nullable=False)
-    trigger_mission: Mapped[str] = mapped_column(String(30), nullable=False)
-    trigger_instrument: Mapped[str] = mapped_column(String(30), nullable=False)
-    trigger_id: Mapped[str] = mapped_column(String(30), nullable=False)
-    trigger_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    trigger_duration: Mapped[float] = mapped_column(Float, nullable=True)
-    classification: Mapped[str] = mapped_column(String(30), nullable=True)
-    justification: Mapped[str] = mapped_column(String(1000), nullable=True)
-    ra: Mapped[float] = mapped_column(Float, nullable=True)
-    dec: Mapped[float] = mapped_column(Float, nullable=True)
-    error: Mapped[float] = mapped_column(Float, nullable=True)
-    begin: Mapped[datetime] = mapped_column(DateTime)
-    end: Mapped[datetime] = mapped_column(DateTime)
-    exposure: Mapped[float] = mapped_column(Float)
-    offset: Mapped[float] = mapped_column(Float)
-    reason: Mapped[TOOReason] = mapped_column(
-        Enum(TOOReason), nullable=False, default=TOOReason.none
-    )
-    too_status: Mapped[TOOStatus] = mapped_column(
-        Enum(TOOStatus), nullable=False, default=TOOStatus.requested
-    )
-    too_info: Mapped[str] = mapped_column(String(), nullable=False)
+    @computed_field  # type: ignore
+    @property
+    def id(self) -> str:
+        return hashlib.md5(
+            f"{self.trigger_time}{self.trigger_mission}".encode()
+        ).hexdigest()
+
+    @classmethod
+    def create_table(cls):
+        # Create this DynamoDB table
+
+        dynamodb.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "id", "AttributeType": "S"},
+            ],
+            TableName=cls.__tablename__,
+            KeySchema=[
+                {"AttributeName": "id", "KeyType": "HASH"},
+            ],
+            ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+        )
+
+    @classmethod
+    def delete_table(cls):
+        dydbtable(cls.__tablename__).delete()
 
 
 class BurstCubeTLEEntryModel(BaseSchema, TLEEntryModelBase):
