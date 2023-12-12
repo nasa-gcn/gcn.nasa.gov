30315fc1202e00f5093a3ac5c1be9dc8c21c8a03
Courey Elliott <coureyelliott@gmail.com>
2023-12-12 12:41:18 -0500

    using className instead of components

--- app/routes/labs.missions/route.tsx
+++ app/routes/labs.missions/route.tsx
@@ -24,75 +24,65 @@ export default function () {
         alt="Fermi satellite with a GRB in the background, both positioned over earth"
         src={fermiHeroImage}
       />
-      <GridContainer>
-        <Grid row className="border-bottom border-gray-50">
-          <h1>Mutimessenger missions</h1>
-        </Grid>
-        <Grid row>
-          <h3>Overview</h3>
-          <p>
-            The joint discovery of gravitational waves and electromagnetic
-            radiation from the binary neutron star merger GW170817 was a
-            watershed moment for astrophysics. NASA missions played a critical
-            role in this discovery, from constraining the speed of gravity, to
-            determining the site of heavy (r-process) element formation, to
-            furthering our understanding of the formation and structure of
-            relativistic jets. The recent detection of a neutrino correlated in
-            space and time with a flare from gamma-ray blazar has also provided
-            a tantalizing clue to the origin of high-energy cosmic neutrinos.
-            These studies of astrophysical transients and time-domain and
-            multimessenger phenomena are perhaps the most rapidly growing field
-            of astrophysics, rich with opportunities for exciting discoveries.
-            In the present/near-future, NASA is well-positioned to capitalize on
-            the exciting scientific opportunities in time-domain and
-            multimessenger astrophysics. As highlighted in the 2019 Astrophysics
-            Senior Review of Operating Missions, the portfolio provides a suite
-            of capabilities that is “greater than the sum of its parts”, and
-            will contribute significantly to the major science questions in this
-            field.
+      <GridContainer className="usa-section">
+        <h1>Mutimessenger missions</h1>
+        <h3>Overview</h3>
+        <p>
+          The joint discovery of gravitational waves and electromagnetic
+          radiation from the binary neutron star merger GW170817 was a watershed
+          moment for astrophysics. NASA missions played a critical role in this
+          discovery, from constraining the speed of gravity, to determining the
+          site of heavy (r-process) element formation, to furthering our
+          understanding of the formation and structure of relativistic jets. The
+          recent detection of a neutrino correlated in space and time with a
+          flare from gamma-ray blazar has also provided a tantalizing clue to
+          the origin of high-energy cosmic neutrinos. These studies of
+          astrophysical transients and time-domain and multimessenger phenomena
+          are perhaps the most rapidly growing field of astrophysics, rich with
+          opportunities for exciting discoveries. In the present/near-future,
+          NASA is well-positioned to capitalize on the exciting scientific
+          opportunities in time-domain and multimessenger astrophysics. As
+          highlighted in the 2019 Astrophysics Senior Review of Operating
+          Missions, the portfolio provides a suite of capabilities that is
+          “greater than the sum of its parts”, and will contribute significantly
+          to the major science questions in this field.
+        </p>
+        <div>
+          <img
+            alt="a diagram depicting all the missions in the fleet over time shaped as a swirl"
+            src={fleet}
+          />
+          <figcaption className="text-italic">
+            The current NASA Astrophysics Fleet, flying and in development.
+          </figcaption>
+        </div>
+        <div className="border-left-05 border-indigo">
+          <p className="padding-left-1">
+            "The complementary nature of these missions makes the overall
+            capability of the portfolio more than the sum of its parts, and many
+            of the most exciting developments in contemporary astrophysics draw
+            on observations from several of these observatories simultaneously."
           </p>
-        </Grid>
-        <Grid row>
           <div>
-            <img
-              alt="a diagram depicting all the missions in the fleet over time shaped as a swirl"
-              src={fleet}
-            />
-            <figcaption className="text-italic">
-              The current NASA Astrophysics Fleet, flying and in development.
-            </figcaption>
-          </div>
-        </Grid>
-        <Grid>
-          <div className="border-left-05 border-indigo">
-            <p className="padding-left-1">
-              "The complementary nature of these missions makes the overall
-              capability of the portfolio more than the sum of its parts, and
-              many of the most exciting developments in contemporary
-              astrophysics draw on observations from several of these
-              observatories simultaneously."
-            </p>
-            <div>
-              <div className="">
-                <div className="float-left">
-                  <img
-                    alt="nasa meatball logo"
-                    width="50"
-                    height="50"
-                    src={meatball}
-                    className="margin-x-1"
-                  />
-                </div>
+            <div className="">
+              <div className="float-left">
+                <img
+                  alt="nasa meatball logo"
+                  width="50"
+                  height="50"
+                  src={meatball}
+                  className="margin-x-1"
+                />
               </div>
-              <GridContainer>
-                <Grid row>2019 Astrophysics Senior Review</Grid>
-                <Grid row className="text-italic">
-                  Executive Summary, Page 2
-                </Grid>
-              </GridContainer>
             </div>
+            <GridContainer>
+              <Grid row>2019 Astrophysics Senior Review</Grid>
+              <Grid row className="text-italic">
+                Executive Summary, Page 2
+              </Grid>
+            </GridContainer>
           </div>
-        </Grid>
+        </div>
       </GridContainer>
     </>
   )
