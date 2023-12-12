4b36c66e9dca59506e4cffb1eaf5648e4370c312
Jamie Kennea <jak51@psu.edu>
2023-12-12 10:20:49 -0500

    Remove unneeded file

--- python/across_api/api_secrets.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Database Login Information
-ACROSS_DB_USER = os.environ["ACROSS_DB_USER"]
-ACROSS_DB_PASSWD = os.environ["ACROSS_DB_PASSWD"]
-ACROSS_DB_HOST = os.environ["ACROSS_DB_HOST"]
-ACROSS_DB_PORT = os.environ["ACROSS_DB_PORT"]
-ACROSS_DB_NAME = os.environ["ACROSS_DB_NAME"]
