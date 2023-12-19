133cf7a5ebdbaf0837ab603dab2fe6a815c275d8
Jamie Kennea <kennea@swift.psu.edu>
2023-12-19 12:47:06 -0500

    Remove unneccess S3 bucket code

--- python/across_api/api_db.py
+++ python/across_api/api_db.py
@@ -1,9 +1,3 @@
 import arc  # type: ignore
-import boto3  # type: ignore
 
 dydbtable = arc.tables.table
-
-# FIXME: Can this be done with arc?
-session = boto3.session.Session(profile_name="across")
-# Conntect to S3 bucket
-s3 = session.resource("s3", region_name="us-east-1")
