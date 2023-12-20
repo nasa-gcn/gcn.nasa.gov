74ade4be3ffc7789c3b82089078cf2453fbad253
Jamie Kennea <jak574@gmail.com>
2023-12-20 08:27:39 -0500

    Remove definition of version from api
    Co-authored-by: Leo Singer <leo.p.singer@nasa.gov>

--- python/across_api/base/api.py
+++ python/across_api/base/api.py
@@ -4,7 +4,6 @@ app = FastAPI(
     title="ACROSS API",
     summary="Astrophysics Cross-Observatory Science Support (ACROSS).",
     description="API providing information on various NASA missions to aid in coordination of large observation efforts.",
-    version="1.0.0",
     contact={
         "email": "support@gcn.nasa.gov",
     },
