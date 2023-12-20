aadc267df6c352d203124428f5fd72edd3eabdf8
Jamie Kennea <jak51@psu.edu>
2023-12-20 08:48:02 -0500

    Remove version.py

--- python/across_api/__init__.py
+++ python/across_api/__init__.py
@@ -1,5 +1,3 @@
 from astropy.utils import iers  # type: ignore
 
-from .version import __version__  # noqa: F401
-
 iers.conf.auto_download = False
--- python/across_api/version.py
+++
@@ -1,4 +0,0 @@
-__version__ = version = "1.0"
-__version_tuple__ = version_tuple = (1, 0)
-
-API_VERSION = f"{__version_tuple__[0]}.{__version_tuple__[1]}"
