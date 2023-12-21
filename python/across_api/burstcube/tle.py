044e1817d1242f7e33d4616dd902a9917c1a4db0
Jamie Kennea <jak574@gmail.com>
2023-12-21 17:05:13 -0500

    Update docstrings

--- python/across_api/burstcube/tle.py
+++ python/across_api/burstcube/tle.py
@@ -8,9 +8,10 @@ from ..base.tle import TLEBase
 @cached(cache=TTLCache(maxsize=128, ttl=3600))
 class BurstCubeTLE(TLEBase):
     """
-    Class that reads and updates BurstCubeTLEs for a given Satellite in a database,
-    and returns a skyfield EarthSatellite based on BurstCubeTLE file for the correct epoch.
-
+    Class for retrieving BurstCube TLEs from the TLE database. If the TLEs are not
+    found in the database, they are retrieved from either the supplied `tle_url`,
+    or from the URL specified in the `tle_heasarc` attribute (in the HEASARC
+    concatenated TLE format), then written to the database.
 
     Parameters
     ----------
@@ -23,6 +24,18 @@ class BurstCubeTLE(TLEBase):
         List of BurstCubeTLEs for given epoch
     tle
         BurstCubeTLE entry for given epoch
+    tle_name
+        Name of the spacecraft as it appears in the TLE
+    tle_url
+        URL to retrieve the TLE from
+    tle_heasarc
+        URL to retrieve the TLE from in HEASARC format
+    tle_bad
+        If the TLE is this many days old, it is considered outdated,
+        and a new TLE will be retrieved
+    tle_min_epoch
+        Minimum epoch for which TLEs are available, typically this will
+        correspond to a date after the launch of the spacecraft
 
     Methods
     -------
@@ -31,7 +44,7 @@ class BurstCubeTLE(TLEBase):
 
     """
 
-    # Configuration options for BurstCubeTLE
+    # Configuration options for BurstCubeTLE (FIXME: Placeholder pre-launch)
     tle_name = "ISS (ZARYA)"
     tle_url = "https://celestrak.com/NORAD/elements/stations.txt"
     tle_heasarc = None
