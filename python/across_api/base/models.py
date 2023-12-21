bdc33608b14eaaefa1b991f381d2afac542c3751
Jamie Kennea <jak574@gmail.com>
2023-12-21 16:59:06 -0500

    Update TLE model

--- python/across_api/base/models.py
+++ python/across_api/base/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from typing import Any
 
 from boto3.dynamodb.conditions import Key  # type: ignore
 from pydantic import computed_field
@@ -8,21 +7,10 @@ from arc import tables  # type: ignore
 from .schema import BaseSchema
 
 
-class DynamoDBBase:
-    __tablename__: str
-
-    def write(self):
-        table = tables.table(self.__tablename__)
-        table.put_item(Item=self.model_dump(mode="json"))
-
-    @classmethod
-    def delete(cls, value: Any, key: str) -> bool:
-        table = tables.table(cls.__tablename__)
-        return table.delete_item(Key={key: value})
-
-
-class TLEEntry(BaseSchema, DynamoDBBase):
-    """Base for TLEEntry"""
+class TLEEntry(BaseSchema):
+    """
+    Represents a TLE entry in the database.
+    """
 
     __tablename__ = "acrossapi_tle"
     name: str  # Partition Key
@@ -32,13 +20,34 @@ class TLEEntry(BaseSchema, DynamoDBBase):
     @computed_field  # type: ignore[misc]
     @property
     def epoch(self) -> datetime:
-        """Calculate Epoch of TLE (Sort Key)"""
+        """
+        Calculate the Epoch of the TLE (Sort Key).
+
+        Returns
+        -------
+            The calculated epoch of the TLE.
+        """
         tleepoch = self.tle1.split()[3]
         year, day_of_year = int(f"20{tleepoch[0:2]}"), float(tleepoch[2:])
         return datetime(year, 1, 1) + timedelta(day_of_year - 1)
 
     @classmethod
     def find_tles_between_epochs(cls, name, start_epoch, end_epoch):
+        """Find TLE entries between two epochs.
+
+        Arguments
+        ---------
+        name
+            The name of the TLE entry.
+        start_epoch
+            The start time over which to search for TLE entries.
+        end_epoch
+            The end time over which to search for TLE entries.
+
+        Returns
+        -------
+            A list of TLEEntry objects between the specified epochs.
+        """
         table = tables.table(cls.__tablename__)
 
         response = table.query(
@@ -49,3 +58,8 @@ class TLEEntry(BaseSchema, DynamoDBBase):
         tles = [cls(**item) for item in items]
         tles.sort(key=lambda x: x.epoch)
         return tles
+
+    def write(self):
+        """Write the TLE entry to the database."""
+        table = tables.table(self.__tablename__)
+        table.put_item(Item=self.model_dump(mode="json"))
