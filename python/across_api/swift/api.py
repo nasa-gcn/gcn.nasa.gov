54e542c28b5aa44b795d3362ed90b85198ac1f26
Jamie Kennea <jak574@gmail.com>
2023-12-29 13:52:29 -0500

    Add Swift Ephemeris endpoint

--- python/across_api/swift/api.py
+++ python/across_api/swift/api.py
@@ -2,9 +2,23 @@
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
 
-from ..base.api import EpochDep, app
-from ..base.schema import TLESchema
+from ..base.api import DateRangeDep, EpochDep, StepSizeDep, app
+from ..base.schema import EphemSchema, TLESchema
+from .ephem import SwiftEphem
 from .tle import SwiftTLE
+
+
+@app.get("/swift/ephem")
+async def swift_ephemeris(
+    daterange: DateRangeDep,
+    stepsize: StepSizeDep,
+) -> EphemSchema:
+    """
+    Calculates the ephemeris for Swift.
+    """
+    return SwiftEphem(
+        begin=daterange["begin"], end=daterange["end"], stepsize=stepsize
+    ).schema
 
 
 @app.get("/swift/tle")
