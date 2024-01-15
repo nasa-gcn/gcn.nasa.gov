4af377ce5818d3480e22c1a9825c83b2c1d00cf9
tylerbarna <tylerpbarna@gmail.com>
2024-01-15 13:35:31 -0600

    add feature flag
    the table format can now be enabled or disabled by including CIRCULARS_TABLE_FORMAT as a feature flag in ones .env file. If not provided, defaults to the existing list.

--- app/routes/_gcn.circulars._archive._index/CircularsIndex.tsx
+++ app/routes/_gcn.circulars._archive._index/CircularsIndex.tsx
@@ -7,8 +7,9 @@
  */
 import { Link } from '@remix-run/react'
 
-import type { Circular} from '../_gcn.circulars/circulars.lib';
+import type { Circular } from '../_gcn.circulars/circulars.lib'
 import { formatDateISO } from '../_gcn.circulars/circulars.lib'
+import { useFeature } from '~/root'
 
 export default function ({
   allItems,
@@ -21,53 +22,73 @@ export default function ({
   totalItems: number
   query?: string
 }) {
+  const featureCircularsTableFormat = useFeature('CIRCULARS_TABLE_FORMAT')
   return (
     <>
       {query && (
         <h3>
-          {totalItems} result{totalItems !== 1 && 's'} found.
+          {totalItems} result{totalItems != 1 && 's'} found.
         </h3>
       )}
-      <div className="usa-table-container--scrollable" tabIndex={0}>
-        <table
-          className="usa-table usa-table--striped usa-table--borderless"
-          data-sortable
-        >
-          <thead>
-            <tr>
-              <th scope="col" role="columnheader" data-sortable>
-                Circular ID
-              </th>
-              <th scope="col" role="columnheader" data-sortable>
-                Subject
-              </th>
-              <th scope="col" role="columnheader" data-sortable>
-                Created On
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {allItems.map(({ circularId, subject, createdOn }) => (
-              <tr key={circularId}>
-                <td data-sort-value={circularId}>{circularId}</td>
-                <td data-sort-value={subject}>
-                  <Link
-                    className="usa-link"
-                    to={`/circulars/${circularId}${searchString}`}
-                  >
-                    {subject}
-                  </Link>
-                </td>
-                <td data-sort-value={createdOn}>{formatDateISO(createdOn)}</td>
+      {!featureCircularsTableFormat && (
+        <ol>
+          {allItems.map(({ circularId, subject }) => (
+            <li key={circularId} value={circularId}>
+              <Link
+                className="usa-link"
+                to={`/circulars/${circularId}${searchString}`}
+              >
+                {subject}
+              </Link>
+            </li>
+          ))}
+        </ol>
+      )}
+
+      {featureCircularsTableFormat && (
+        <div className="usa-table-container--scrollable" tabIndex={0}>
+          <table
+            className="usa-table usa-table--striped usa-table--borderless"
+            data-sortable
+          >
+            <thead>
+              <tr>
+                <th scope="col" role="columnheader" data-sortable>
+                  Circular ID
+                </th>
+                <th scope="col" role="columnheader" data-sortable>
+                  Subject
+                </th>
+                <th scope="col" role="columnheader" data-sortable>
+                  Created On
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div
-          className="usa-sr-only usa-table__announcement-region"
-          aria-live="polite"
-        ></div>
-      </div>
+            </thead>
+            <tbody>
+              {allItems.map(({ circularId, subject, createdOn }) => (
+                <tr key={circularId}>
+                  <td data-sort-value={circularId}>{circularId}</td>
+                  <td data-sort-value={subject}>
+                    <Link
+                      className="usa-link"
+                      to={`/circulars/${circularId}${searchString}`}
+                    >
+                      {subject}
+                    </Link>
+                  </td>
+                  <td data-sort-value={createdOn}>
+                    {formatDateISO(createdOn)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div
+            className="usa-sr-only usa-table__announcement-region"
+            aria-live="polite"
+          ></div>
+        </div>
+      )}
     </>
   )
 }
