7aa230ec5b51b1cc945222aaffbf7151834fd6ce
dakota002 <dakota.d@live.com>
2024-01-29 16:12:18 -0500

    Adds group CRUD methods
    Use Paginators

--- app/lib/cognito.server.ts
+++ app/lib/cognito.server.ts
@@ -8,14 +8,24 @@
 import type {
   AttributeType,
   CognitoIdentityProviderServiceException,
+  GroupType,
   UserType,
 } from '@aws-sdk/client-cognito-identity-provider'
 import {
+  AdminAddUserToGroupCommand,
+  AdminRemoveUserFromGroupCommand,
   CognitoIdentityProviderClient,
+  CreateGroupCommand,
+  DeleteGroupCommand,
+  GetGroupCommand,
+  UpdateGroupCommand,
+  paginateAdminListGroupsForUser,
+  paginateListGroups,
   paginateListUsersInGroup,
 } from '@aws-sdk/client-cognito-identity-provider'
 
 export const cognito = new CognitoIdentityProviderClient({})
+const UserPoolId = process.env.COGNITO_USER_POOL_ID
 
 /**
  * Returns the value of a specified Attribute if it exists on a user object
@@ -52,7 +62,6 @@ export async function listUsersInGroup(GroupName: string) {
   console.warn(
     'using a paginator; replace with alternative API calls that avoid large result sets'
   )
-  const UserPoolId = process.env.COGNITO_USER_POOL_ID
   const pages = paginateListUsersInGroup(
     { client: cognito },
     { GroupName, UserPoolId }
@@ -83,4 +92,81 @@ export function maybeThrow(e: any, warning: string) {
       `Cognito threw ${name}. This would be an error in production. Since we are in ${process.env.NODE_ENV}, ${warning}.`
     )
   }
+}
+
+export async function createGroup(GroupName: string, Description: string) {
+  const command = new CreateGroupCommand({
+    GroupName,
+    UserPoolId,
+    Description,
+  })
+  await cognito.send(command)
+}
+
+export async function getGroup(GroupName: string) {
+  const command = new GetGroupCommand({
+    GroupName,
+    UserPoolId,
+  })
+  return await cognito.send(command)
+}
+
+export async function getGroups() {
+  const pages = paginateListGroups({ client: cognito }, { UserPoolId })
+  const groups: GroupType[] = []
+  for await (const page of pages) {
+    const nextGroups = page.Groups
+    if (nextGroups) groups.push(...nextGroups)
+  }
+
+  return groups
+}
+
+export async function updateGroup(GroupName: string, Description: string) {
+  const command = new UpdateGroupCommand({
+    GroupName,
+    UserPoolId,
+    Description,
+  })
+  await cognito.send(command)
+}
+
+export async function deleteGroup(GroupName: string) {
+  const command = new DeleteGroupCommand({
+    GroupName,
+    UserPoolId,
+  })
+  await cognito.send(command)
+}
+
+export async function addUserToGroup(Username: string, GroupName: string) {
+  const command = new AdminAddUserToGroupCommand({
+    UserPoolId,
+    Username,
+    GroupName,
+  })
+  await cognito.send(command)
+}
+
+export async function listGroupsForUser(Username: string) {
+  const pages = paginateAdminListGroupsForUser(
+    { client: cognito },
+    { UserPoolId, Username }
+  )
+  const groups: GroupType[] = []
+  for await (const page of pages) {
+    const nextGroups = page.Groups
+    if (nextGroups) groups.push(...nextGroups)
+  }
+
+  return groups
+}
+
+export async function removeUserFromGroup(Username: string, GroupName: string) {
+  const command = new AdminRemoveUserFromGroupCommand({
+    UserPoolId,
+    Username,
+    GroupName,
+  })
+  await cognito.send(command)
 }
