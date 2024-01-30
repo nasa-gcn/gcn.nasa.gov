936d2ac446a2d84fad47326fe68f917ec0d7c7bf
Jamie Kennea <jak574@gmail.com>
2024-01-29 19:35:35 -0500

    Goodbye endpoints

--- python/across_api/burstcube/api.py
+++ python/across_api/burstcube/api.py
@@ -1,29 +1,3 @@
 # Copyright © 2023 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
-
-from ..base.api import DateRangeDep, EpochDep, StepSizeDep, app
-from ..base.schema import SAASchema, TLESchema
-from .saa import BurstCubeSAA
-from .tle import BurstCubeTLE
-
-
-@app.get("/burstcube/saa")
-async def burstcube_saa(
-    daterange: DateRangeDep,
-    stepsize: StepSizeDep,
-) -> SAASchema:
-    """
-    Endpoint to retrieve BurstCubeSAA data for a given date range and step size.
-    """
-    return BurstCubeSAA(stepsize=stepsize, **daterange).schema
-
-
-@app.get("/burstcube/tle")
-def burstcube_tle(
-    epoch: EpochDep,
-) -> TLESchema:
-    """
-    Returns the best TLE for BurstCube for a given epoch.
-    """
-    return BurstCubeTLE(epoch=epoch).schema
--- python/across_api/swift/api.py
+++ python/across_api/swift/api.py
@@ -1,29 +1,3 @@
 # Copyright © 2023 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
-
-from ..base.api import DateRangeDep, EpochDep, StepSizeDep, app
-from ..base.schema import SAASchema, TLESchema
-from .saa import SwiftSAA
-from .tle import SwiftTLE
-
-
-@app.get("/swift/saa")
-async def swift_saa(
-    daterange: DateRangeDep,
-    stepsize: StepSizeDep,
-) -> SAASchema:
-    """
-    Endpoint for retrieving the times of Swift SAA passages for a given date range.
-    """
-    return SwiftSAA(stepsize=stepsize, **daterange).schema
-
-
-@app.get("/swift/tle")
-def swift_tle(
-    epoch: EpochDep,
-) -> TLESchema:
-    """
-    Returns the best TLE for Swift for a given epoch.
-    """
-    return SwiftTLE(epoch=epoch).schema
