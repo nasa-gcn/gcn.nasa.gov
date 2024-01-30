77c66c457cd816f9dd4c53bbd32961542fe78eb1
Jamie Kennea <jak574@gmail.com>
2024-01-30 07:13:54 -0500

    Add back in API endpoints but only for testing mode

--- python/across_api/burstcube/api.py
+++ python/across_api/burstcube/api.py
@@ -1,3 +1,43 @@
 # Copyright © 2023 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
+
+from .ephem import BurstCubeEphem
+from .saa import BurstCubeSAA
+from ..base.api import DateRangeDep, EpochDep, StepSizeDep, app
+from ..base.schema import EphemGetSchema, SAASchema, TLESchema
+from .tle import BurstCubeTLE
+import os
+
+if os.environ.get("ARC_ENV") == "testing":
+
+    @app.get("/testing/burstcube/ephem")
+    async def burstcube_ephem(
+        date_range: DateRangeDep, stepsize: StepSizeDep
+    ) -> EphemGetSchema:
+        """
+        Returns the best TLE for BurstCube for a given epoch.
+        """
+        return BurstCubeEphem(
+            begin=date_range["begin"], end=date_range["end"], stepsize=stepsize
+        ).schema
+
+    @app.get("/testing/burstcube/tle")
+    async def burstcube_tle(
+        epoch: EpochDep,
+    ) -> TLESchema:
+        """
+        Returns the best TLE for BurstCube for a given epoch.
+        """
+        return BurstCubeTLE(epoch=epoch).schema
+
+    @app.get("/testing/burstcube/saa")
+    async def burstcube_saa(
+        date_range: DateRangeDep, stepsize: StepSizeDep
+    ) -> SAASchema:
+        """
+        Returns the SAA entries for BurstCube for a given time range.
+        """
+        return BurstCubeSAA(
+            begin=date_range["begin"], end=date_range["end"], stepsize=stepsize
+        ).schema
--- python/across_api/swift/api.py
+++ python/across_api/swift/api.py
@@ -1,3 +1,41 @@
 # Copyright © 2023 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
+
+from .ephem import SwiftEphem
+from .saa import SwiftSAA
+from ..base.api import DateRangeDep, EpochDep, StepSizeDep, app
+from ..base.schema import EphemGetSchema, SAASchema, TLESchema
+from .tle import SwiftTLE
+import os
+
+if os.environ.get("ARC_ENV") == "testing":
+
+    @app.get("/testing/swift/ephem")
+    async def swift_ephem(
+        date_range: DateRangeDep, stepsize: StepSizeDep
+    ) -> EphemGetSchema:
+        """
+        Returns the best TLE for Swift for a given epoch.
+        """
+        return SwiftEphem(
+            begin=date_range["begin"], end=date_range["end"], stepsize=stepsize
+        ).schema
+
+    @app.get("/testing/swift/tle")
+    async def swift_tle(
+        epoch: EpochDep,
+    ) -> TLESchema:
+        """
+        Returns the best TLE for Swift for a given epoch.
+        """
+        return SwiftTLE(epoch=epoch).schema
+
+    @app.get("/testing/swift/saa")
+    async def swift_saa(date_range: DateRangeDep, stepsize: StepSizeDep) -> SAASchema:
+        """
+        Returns the SAA entries for Swift for a given time range.
+        """
+        return SwiftSAA(
+            begin=date_range["begin"], end=date_range["end"], stepsize=stepsize
+        ).schema
