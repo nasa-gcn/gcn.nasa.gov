e9ac62f4c56cf22808f86e9a79b738e2abc1c180
Jamie Kennea <kennea@swift.psu.edu>
2024-01-30 13:51:45 -0500

    Cleanup

--- python/tests/test_burstcube_ephem.py
+++ python/tests/test_burstcube_ephem.py
@@ -34,92 +34,58 @@ eph = BurstCubeEphem(
 # posvec = gcrs.position.km
 # posvec
 
-# array(
-#     [
-#         [3102.62364411, 2799.95875402, 2477.25872901, 2136.82885638, 1781.10319625],
-#         [5981.53068666, 6175.7074466, 6325.69376331, 6430.40680333, 6489.08998679],
-#         [-870.78766934, -422.62861603, 28.56295111, 479.54929747, 927.09298308],
-#     ]
-# )
+skyfield_posvec = np.array(
+    [
+        [3102.62364411, 2799.95875402, 2477.25872901, 2136.82885638, 1781.10319625],
+        [5981.53068666, 6175.7074466, 6325.69376331, 6430.40680333, 6489.08998679],
+        [-870.78766934, -422.62861603, 28.56295111, 479.54929747, 927.09298308],
+    ]
+)
 
 
 # Check that the Skyfield posvec and BurstCubeEphem posvec match to < 0.1 cm
 def test_burstcube_ephem_posvec():
-    assert abs(
-        eph.posvec.xyz.value
-        - np.array(
-            [
-                [
-                    3102.62364411,
-                    2799.95875402,
-                    2477.25872901,
-                    2136.82885638,
-                    1781.10319625,
-                ],
-                [
-                    5981.53068666,
-                    6175.7074466,
-                    6325.69376331,
-                    6430.40680333,
-                    6489.08998679,
-                ],
-                [-870.78766934, -422.62861603, 28.56295111, 479.54929747, 927.09298308],
-            ]
-        )
-        < 1e-6
+    assert (
+        abs(eph.posvec.xyz.value - skyfield_posvec) < 1e-6
     ).all(), "GCRS position vector values off by more than 0.1 cm"
 
 
 # Skyfield calculate velocity
 # gcrs.velocity.km_per_s
-# array([[-3.89219421, -4.17406822, -4.42613928, -4.64657634, -4.83377889],
-#       [ 2.87775303,  2.29718947,  1.70004715,  1.09062677,  0.47332214],
-#       [ 5.94091899,  6.00285951,  6.02173398,  5.99738762,  5.92998321]])
+skyfield_velocity = np.array(
+    [
+        [-3.89219421, -4.17406822, -4.42613928, -4.64657634, -4.83377889],
+        [2.87775303, 2.29718947, 1.70004715, 1.09062677, 0.47332214],
+        [5.94091899, 6.00285951, 6.02173398, 5.99738762, 5.92998321],
+    ]
+)
 
 
 def test_burstcube_ephem_velvec():
     # Check that the Skyfield calculated velocity matches the BurstCubeEphem value to < 0.1 cm/s
-    assert abs(
-        eph.velvec.xyz.value
-        - np.array(
-            [
-                [-3.89219421, -4.17406822, -4.42613928, -4.64657634, -4.83377889],
-                [2.87775303, 2.29718947, 1.70004715, 1.09062677, 0.47332214],
-                [5.94091899, 6.00285951, 6.02173398, 5.99738762, 5.92998321],
-            ]
-        )
-        < 1e-6
-    ).all()
+    assert abs(eph.velvec.xyz.value - skyfield_velocity < 1e-6).all()
 
 
 # Calculate lat/lon using Skyfield
 # lat, lon = wgs84.latlon_of(gcrs)
 # lat.degrees
-# array([-7.34620853, -3.5319745 ,  0.292994  ,  4.11759842,  7.93070336])
+skyfield_lat = np.array([-7.34620853, -3.5319745, 0.292994, 4.11759842, 7.93070336])
 
 
 def test_burstcube_ephem_latitude():
     # Check Skyfield latitude matches BurstCubeEphem value by < 0.3 arcseconds
-    assert (
-        abs(
-            eph.latitude.deg
-            - np.array([-7.34620853, -3.5319745, 0.292994, 4.11759842, 7.93070336])
-        )
-        < 0.3 / 3600
-    ).all()
+    assert (abs(eph.latitude.deg - skyfield_lat) < 0.3 / 3600).all()
+
+
+# lon.degrees
+skyfield_lon = np.array(
+    [-37.27604624, -34.55468253, -31.85753823, -29.15798066, -26.42931064]
+)
 
 
 def test_burstcube_ephem_longitude():
     # Astropy ITRS and SkyField longitude disagree by 0.13 arcseconds, so we set our tolerance to <0.3 arcseconds.
-    assert (
-        abs(
-            eph.longitude.deg
-            - np.array(
-                [-37.27604624, -34.55468253, -31.85753823, -29.15798066, -26.42931064]
-            )
-        )
-        < 0.3 / 3600
-    ).all()
+    assert (abs(eph.longitude.deg - skyfield_lon) < 0.3 / 3600).all()
 
 
 # Calculate Sun position as seen from the satellite orbiting the Earth
@@ -127,69 +93,43 @@ def test_burstcube_ephem_longitude():
 # sunra = sunpos.apparent().radec()[0]._degrees
 #
 # sunra
-# array([280.55657134, 280.55756145, 280.55856877, 280.55959297,
-#        280.56063359])
+skyfield_sunra = np.array(
+    [280.55657134, 280.55756145, 280.55856877, 280.55959297, 280.56063359]
+)
 # sundec = sunpos.apparent().radec()[0]._degrees
 #
 # sundec
-# array([-23.07915326, -23.07916203, -23.07918388, -23.07921822,
-#       -23.07926433])
+skyfield_sundec = np.array(
+    [-23.07915326, -23.07916203, -23.07918388, -23.07921822, -23.07926433]
+)
 
 
 def test_burstcube_ephem_sunradec():
     # Assert Skyfield Sun RA/Dec is within 5 arc-seconds of Astropy. These numbers don't match,
     # but 5 arc-seconds difference is not a huge offset.
-    assert (
-        abs(
-            eph.sun.ra.deg
-            - np.array(
-                [280.55657134, 280.55756145, 280.55856877, 280.55959297, 280.56063359]
-            )
-        )
-        < 5 / 3600.0
-    ).all()
-    assert (
-        abs(
-            eph.sun.dec.deg
-            - np.array(
-                [-23.07915326, -23.07916203, -23.07918388, -23.07921822, -23.07926433]
-            )
-        )
-        < 5 / 3600.0
-    ).all()
+    assert (abs(eph.sun.ra.deg - skyfield_sunra) < 5 / 3600.0).all()
+    assert (abs(eph.sun.dec.deg - skyfield_sundec) < 5 / 3600.0).all()
 
 
 # Calculate Moon position as seen from the satellite orbiting the Earth
 # moonpos = (bodies["Earth"] + satellite).at(nowts).observe(bodies["Moon"])
 #
 # moonpos.apparent().radec()[0]._degrees
-# array([159.77137018, 159.79223935, 159.80612276, 159.81296941, 159.81277874])
+skyfield_moonra = np.array(
+    [159.77137018, 159.79223935, 159.80612276, 159.81296941, 159.81277874]
+)
 #
 # moonpos.apparent().radec()[1]._degrees
-# array([12.84719405, 12.79202216, 12.73641424, 12.68073706, 12.62535993])
+skyfield_moondec = np.array(
+    [12.84719405, 12.79202216, 12.73641424, 12.68073706, 12.62535993]
+)
 
 
 def test_burstcube_ephem_moonradec():
     # Assert Skyfield Moon RA/Dec is within 5 arc-seconds of Astropy. These numbers don't match,
     # but 5 arc-seconds difference is not a huge offset.
-    assert (
-        abs(
-            eph.moon.ra.deg
-            - np.array(
-                [159.77137018, 159.79223935, 159.80612276, 159.81296941, 159.81277874]
-            )
-        )
-        < 5 / 3600
-    ).all()
-    assert (
-        abs(
-            eph.moon.dec.deg
-            - np.array(
-                [12.84719405, 12.79202216, 12.73641424, 12.68073706, 12.62535993]
-            )
-        )
-        < 5 / 3600
-    ).all()
+    assert (abs(eph.moon.ra.deg - skyfield_moonra) < 5 / 3600).all()
+    assert (abs(eph.moon.dec.deg - skyfield_moondec) < 5 / 3600).all()
 
 
 def test_ephem_epoch():
