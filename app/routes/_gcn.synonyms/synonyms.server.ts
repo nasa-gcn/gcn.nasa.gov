b831727810085093233328b610cfc30600220b9b
Courey Elliott <coureyelliott@gmail.com>
2024-02-22 09:39:04 -0500

    using batchWrite who delegates to AWS.DynamoDB.batchWriteItem()

--- app/routes/_gcn.synonyms/synonyms.server.ts
+++ app/routes/_gcn.synonyms/synonyms.server.ts
@@ -6,21 +6,37 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { tables } from '@architect/functions'
+import type { DynamoDBDocument } from '@aws-sdk/lib-dynamodb/dist-types/DynamoDBDocument'
 import crypto from 'crypto'
 
 /*
  * If an eventId already has a synonym and is passed in, it will unlink the
  * eventId from the old synonym and the only remaining link will be to the
  * new synonym.
+ *
+ * BatchWriteItem has a limit of 25 items, so the user may not add more than
+ * 25 synonyms at a time.
  */
 export async function createSynonyms(...synonymousEventIds: string[]) {
   const uuid = crypto.randomUUID()
   const db = await tables()
-  await Promise.all(
-    synonymousEventIds.map((eventId) => {
-      if (!eventId) return null
-      return db.synonyms.put({ uuid, eventId })
-    })
-  )
+  const client = db._doc as unknown as DynamoDBDocument
+
+  const writeRequests = synonymousEventIds
+    .filter((eventId) => Boolean(eventId))
+    .map((eventId) => ({
+      PutRequest: {
+        Item: { uuid, eventId },
+      },
+    }))
+
+  const params = {
+    RequestItems: {
+      synonyms: writeRequests,
+    },
+  }
+
+  await client.batchWrite(params)
+
   return uuid
 }
