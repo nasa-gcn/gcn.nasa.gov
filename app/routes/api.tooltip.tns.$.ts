b4c00d411bc7274490186e10cd9e59a80e0acbad
Leo Singer <leo.singer@ligo.org>
2024-04-20 10:55:50 -0400

    Add tooltips for TNS

---
+++ app/routes/api.tooltip.tns.$.ts
@@ -0,0 +1,57 @@
+/*!
+ * Copyright © 2023 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import invariant from 'tiny-invariant'
+
+import { getEnvOrDie } from '~/lib/env.server'
+import { publicStaticShortTermCacheControlHeaders } from '~/lib/headers.server'
+
+const splitter = /[:.]/
+
+export async function loader({ params: { '*': value } }: LoaderFunctionArgs) {
+  invariant(value)
+
+  const tnsBotName = getEnvOrDie('TNS_BOT_NAME')
+  const tnsBotKey = getEnvOrDie('TNS_BOT_KEY')
+  const tnsBotID = getEnvOrDie('TNS_BOT_ID')
+
+  const url = new URL('https://www.wis-tns.org/api/get/object')
+  const formData = new FormData()
+  formData.set('api_key', tnsBotKey)
+  formData.set('data', JSON.stringify({ objname: value }))
+
+  const response = await fetch(url, {
+    headers: {
+      'User-Agent': `tns_marker${JSON.stringify({ tns_id: tnsBotID, type: 'bot', name: tnsBotName })}`,
+    },
+    method: 'POST',
+    body: formData,
+  })
+
+  if (!response.ok) {
+    console.error(response)
+    throw new Error('TNS request failed')
+  }
+
+  const {
+    ra,
+    dec,
+    internal_names: names,
+  }: {
+    ra?: string
+    dec?: string
+    internal_names?: string
+  } = (await response.json()).data.reply
+
+  if (!(ra && dec && names)) throw new Response(null, { status: 404 })
+
+  return json(
+    { ra: ra.split(splitter), dec: dec.split(splitter), names },
+    { headers: publicStaticShortTermCacheControlHeaders }
+  )
+}
--- app/routes/circulars.$circularId.($version)/AstroData.components.tsx
+++ app/routes/circulars.$circularId.($version)/AstroData.components.tsx
@@ -8,10 +8,13 @@
 import { type LoaderFunction } from '@remix-run/node'
 import { type useLoaderData } from '@remix-run/react'
 
-import { AstroDataLink, AstroDataLinkWithTooltip } from './AstroDataContext'
+import { AstroDataLinkWithTooltip } from './AstroDataContext'
 import { type loader as arxivTooltipLoader } from '~/routes/api.tooltip.arxiv.$'
 import { type loader as circularTooltipLoader } from '~/routes/api.tooltip.circular.$'
 import { type loader as doiTooltipLoader } from '~/routes/api.tooltip.doi.$'
+import type { loader as tnsTooltipLoader } from '~/routes/api.tooltip.tns.$'
+
+import styles from './AstroData.components.module.css'
 
 async function fetchTooltipData<loader extends LoaderFunction>(
   className: string,
@@ -87,8 +90,58 @@ export function Doi({ children, value }: JSX.IntrinsicElements['data']) {
 
 export function Tns({ children, value }: JSX.IntrinsicElements['data']) {
   return (
-    <AstroDataLink to={`https://www.wis-tns.org/object/${value}`}>
+    <AstroDataLinkWithTooltip
+      to={`https://www.wis-tns.org/object/${value}`}
+      fetch={() => fetchTooltipData<typeof tnsTooltipLoader>('tns', value)}
+      label={({ ra, dec, names }) => (
+        <>
+          <div>{names}</div>
+          <div className={styles.ra}>
+            <span className={styles.h}>{ra[0]}</span>
+            <span className={styles.sep}>
+              <sup>h</sup>
+            </span>
+            <span className={styles.m}>{ra[1]}</span>
+            <span className={styles.sep}>
+              <sup>m</sup>
+            </span>
+            <span className={styles.s}>{ra[2]}</span>
+            <span className={styles.sep}>
+              <sup>s</sup>
+              <span className="position-relative">
+                <span
+                  className="position-absolute width-0"
+                  style={{ left: '-0.5ex' }}
+                >
+                  .
+                </span>
+              </span>
+            </span>
+            <span className={styles.f}>{ra[3]}</span>
+          </div>
+          <div className={styles.dec}>
+            <span className={styles.d}>{dec[0]}</span>
+            <span className={styles.sep}>°</span>
+            <span className={styles.m}>{dec[1]}</span>
+            <span className={styles.sep}>′</span>
+            <span className={styles.s}>{dec[2]}</span>
+            <span className={styles.sep}>
+              ″
+              <span className="position-relative">
+                <span
+                  className="position-absolute width-0"
+                  style={{ left: '-1ex' }}
+                >
+                  .
+                </span>
+              </span>
+            </span>
+            <span className={styles.f}>{dec[3]}</span>
+          </div>
+        </>
+      )}
+    >
       {children}
-    </AstroDataLink>
+    </AstroDataLinkWithTooltip>
   )
 }
