57a79988e796d93608947790eb94d607e6c84c38
tylerbarna <tylerpbarna@gmail.com>
2024-06-27 12:40:34 -0500

    rename prop properly

--- app/routes/circulars.$circularId.($version)/AstroDataContext.tsx
+++ app/routes/circulars.$circularId.($version)/AstroDataContext.tsx
@@ -106,7 +106,7 @@ export function AstroDataLinkWithTooltip<T>({
         </div>
       }
       asCustom={AstroDataLink}
-      isExternal={external}
+      external={external}
     >
       {children}
     </Tooltip>
