68fe1bbc1d25e3f6d7b2d397b60bf22d17a21cb5
tylerbarna <tylerpbarna@gmail.com>
2024-09-25 12:12:32 -0500

    correct typo

--- app/routes/circulars/circulars.server.ts
+++ app/routes/circulars/circulars.server.ts
@@ -161,7 +161,7 @@ export async function search({
         }
 
   const queryObj = query
-    ? feature('CICRULARS_LUCENE')
+    ? feature('CIRCULARS_LUCENE')
       ? {
           simple_query_string: {
             query,
