4877cd8ff0d8a76b6253ca0f7ba79d120810c8f1
Tyler Barna <tylerpbarna@gmail.com>
2024-12-12 13:01:39 -0600

    remove reference to "page"

--- app/routes/circulars._archive._index/LuceneMenu.tsx
+++ app/routes/circulars._archive._index/LuceneMenu.tsx
@@ -26,9 +26,9 @@ export function LuceneAccordion() {
       <div>
         To narrow the search results, use Lucene search syntax. This allows for
         specifying which circular field to search (submitter, subject, and/or
-        body). Further documentation can be found on the{' '}
+        body). Further documentation can be found{' '}
         <Link className="usa-link" to="/docs/circulars/advanced-search">
-          Lucene Search Syntax Page
+          here
         </Link>
         {'. '}
         <h4>Lucene Examples (click to copy):</h4>
