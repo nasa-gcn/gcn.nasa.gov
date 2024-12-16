nasa-gcn/gcn.nasa.gov

commit 08d587f00c17c3466ad34aeb97b17a0a6da3b265

Require Node.js >= 22
This is the version we deploy to now.

Note: remove lodash.groupBy, which now has a builtin alternative
Object.groupBy.

sandbox-search.js
@@ -6,12 +6,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { readFile } from 'fs/promises'
-import groupBy from 'lodash/groupBy.js'
 
 export default async function () {
   const text = await readFile('sandbox-seed.json', { encoding: 'utf-8' })
   const { circulars, synonyms } = JSON.parse(text)
-  const groups = Object.entries(groupBy(synonyms, 'synonymId')).flatMap(
+  const groups = Object.entries(Object.groupBy(synonyms, 'synonymId')).flatMap(
     ([synonymId, values]) => [
       {
         synonymId,

commit 8f441f43323bc28df32e5c48d5b9d26a41f834de

Fix Object.groupBy callback arg
`Object.groupBy` takes as an argument a callback to return the
grouping keys.

Fixes the following error introduced in #2792:

```
TypeError: synonymId is not a function
    at Function.groupBy (<anonymous>)
    at default (file:///Users/lpsinger/src/gcn.nasa.gov/sandbox-search.js:13:40)
    at async getData (file:///Users/lpsinger/src/gcn.nasa.gov/node_modules/@nasa-gcn/architect-plugin-search/index.js:318:16)
    at async populate (file:///Users/lpsinger/src/gcn.nasa.gov/node_modules/@nasa-gcn/architect-plugin-search/index.js:327:16)
    at async start (file:///Users/lpsinger/src/gcn.nasa.gov/node_modules/@nasa-gcn/architect-plugin-search/index.js:628:5)
    at async runPlugins (/Users/lpsinger/src/gcn.nasa.gov/node_modules/@architect/sandbox/src/sandbox/plugins/index.js:25:9)
```

sandbox-search.js
@@ -10,15 +10,15 @@ import { readFile } from 'fs/promises'
 export default async function () {
   const text = await readFile('sandbox-seed.json', { encoding: 'utf-8' })
   const { circulars, synonyms } = JSON.parse(text)
-  const groups = Object.entries(Object.groupBy(synonyms, 'synonymId')).flatMap(
-    ([synonymId, values]) => [
-      {
-        synonymId,
-        eventIds: values.map(({ eventId }) => eventId),
-        slugs: values.map(({ slug }) => slug),
-      },
-    ]
-  )
+  const groups = Object.entries(
+    Object.groupBy(synonyms, ({ synonymId }) => ({ synonymId }))
+  ).flatMap(([synonymId, values]) => [
+    {
+      synonymId,
+      eventIds: values.map(({ eventId }) => eventId),
+      slugs: values.map(({ slug }) => slug),
+    },
+  ])
   return [
     ...circulars.flatMap((item) => [
       { index: { _index: 'circulars', _id: item.circularId.toString() } },