66a0e9992aa7dc788718d2d52095e8a97e6f4a73
Courey Elliott <coureyelliott@gmail.com>
2025-02-20 15:49:33 -0500

    adding notes for clarity

--- app/backfill-eventId.js
+++ app/backfill-eventId.js
@@ -7,7 +7,7 @@ import chunk from 'lodash/chunk.js'
 // THIS CODE IS CUT AND PASTED FROM THE APP CODE IN /routes/circulars/circulars.lib.ts
 // running as a node script it would not import the module from the file
 const subjectMatchers = [
-  [/GRB[.\s_-]*(\d{6}[a-z|.]\d*)/i, ([, id]) => `GRB ${id.toUpperCase()}`],
+  [/GRB[.\s_-]*(\d{6}[a-z|.]?\d*)/i, ([, id]) => `GRB ${id.toUpperCase()}`],
   [/SGR[.\s_-]*(J*\d{4}\.?\d*\+\d{4})/i, ([, id]) => `SGR ${id.toUpperCase()}`],
   [
     /SGR[.\s_-]*Swift[.\s_-]*(J*\d{4}\.?\d*\+\d{4})/i,
@@ -29,6 +29,7 @@ const subjectMatchers = [
   ],
   [/EP[.\s_-]*(\d{6}[a-z])/i, ([, id]) => `EP${id}`],
   [/FRB[.\s_-]*(\d{8}[a-z])/i, ([, id]) => `FRB ${id}`.toUpperCase()],
+  [/sb[.\s_-]*(\d{8})/i, ([, id]) => `sb${id}`],
 ]
 
 export function parseEventFromSubject(value) {
@@ -45,60 +46,64 @@ export function parseEventFromSubject(value) {
 
 async function getTableNameFromSSM(dynamoTableName) {
   const ssmClient = new SSMClient({ region: 'us-east-1' })
-
-  try {
-    const command = new GetParameterCommand({ Name: dynamoTableName })
-    const response = await ssmClient.send(command)
-
-    if (!response.Parameter?.Value) {
-      throw new Error('dynamoTableName not found in SSM')
-    }
-
-    return response.Parameter.Value
-  } catch (error) {
-    console.error('Error fetching table name from SSM:', error)
-    throw error
-  }
+  const command = new GetParameterCommand({ Name: dynamoTableName })
+  const response = await ssmClient.send(command)
+  return response.Parameter.Value
 }
 
 export async function backfillEventIds() {
   const startTime = new Date()
   console.log('Starting EVENT ID backfill...', startTime)
-  const writeLimit = 1000
+  const writeLimit = 10
   const dynamoTableName = '/RemixGcnProduction/tables/circulars'
   const TableName = await getTableNameFromSSM(dynamoTableName)
   const client = new DynamoDBClient({ region: 'us-east-1' })
-  const pages = paginateScan({ client }, { TableName })
+  // get the circulars without eventIds
+  const pages = paginateScan(
+    { client },
+    { TableName, FilterExpression: 'attribute_not_exists(eventId)' }
+  )
   let totalWriteCount = 0
   let limitHit = false
 
+  // keeping the pages loop so we can bail quickly once we hit our limit
   for await (const page of pages) {
-    if (limitHit) break
+    if (limitHit) break // no need to carry on if we've hit the limit for the day
+    // break the page into chunks of 25 since that is the limit for batchwrite
     const chunked = chunk(page.Items || [], 25)
 
+    // loop over in chunks of 25 to keep the batch write happy
     for (const chunk of chunked) {
-      if (limitHit) break
+      if (limitHit) break // no need to carry on if we've hit the limit for the day
       let writes = []
 
+      // take each record in the chunk and process it
       for (const record of chunk) {
-        if (limitHit) break
+        if (limitHit) break // again, just to be safe, don't do anything if we've hit the limit
 
         const circular = unmarshall(record)
         const parsedEventId = parseEventFromSubject(circular.subject)
 
+        // if the record doesn't have an eventId (which it shouldn't), add one and add it to the write queue
         if (!circular.eventId && parsedEventId) {
           circular.eventId = parsedEventId
           writes.push(circular)
         }
       }
 
+      // check to see if we are over the daily limit with this chunk.
       if (writes.length + totalWriteCount > writeLimit) {
+        // we find how far over the limit it puts us
         const overage = writes.length + totalWriteCount - writeLimit
+        // we figure out how many we need to remove to keep it under the limit
         const writesToLimitTo = writes.length - overage
+        // we slice off the ones that would put us over it
         writes = writes.slice(0, writesToLimitTo)
+        // we set the limit hit to true so no further processing will happen after
+        // the proper number of writes
         limitHit = true
       }
-
+      // if there are writes, it batch writes them. it can only handle 25 at a time, that's why we do chunks
       if (writes.length > 0) {
         const command = new BatchWriteCommand({
           RequestItems: {
