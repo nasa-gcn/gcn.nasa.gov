233e7e0fb40182bfca9c3a5185ec2c62897fdccf
Courey Elliott <caellio1@gs66-synchrotron.ndc.nasa.gov>
2025-03-12 15:12:16 -0400

    adapting backfill

--- app/migrations/synonyms/index.ts
+++ app/migrations/synonyms/index.ts
@@ -1,14 +1,18 @@
 import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb'
 import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
-import { BatchWriteCommand, paginateScan } from '@aws-sdk/lib-dynamodb'
+import {
+  BatchWriteCommand,
+  DynamoDBDocumentClient,
+  paginateScan,
+} from '@aws-sdk/lib-dynamodb'
 import { slug } from 'github-slugger'
-import { orderBy } from 'lodash'
+import { orderBy, uniqBy } from 'lodash'
 
 import type { Circular } from '~/routes/circulars/circulars.lib'
 
 export interface WriteData {
   eventId: string
-  initialDate: string
+  initialDate: number
 }
 
 async function getTableNameFromSSM(dynamoTableName: string) {
@@ -27,10 +31,10 @@ export async function backfillSynonyms() {
   const dynamoSynonymsTableName = '/RemixGcnProduction/tables/synonyms'
   const TableName = await getTableNameFromSSM(dynamoSynonymsTableName)
   const client = new DynamoDBClient({ region: 'us-east-1' })
+  const docClient = DynamoDBDocumentClient.from(client)
   const pages = paginateScan(
-    { client },
-    { TableName: circularTableName },
-    { pageSize: 25 }
+    { client: docClient },
+    { TableName: circularTableName, Limit: 25 }
   )
 
   for await (const page of pages) {
@@ -56,33 +60,41 @@ export async function backfillSynonyms() {
         }))
 
         const initialDate = orderBy(items, ['circularId'], ['asc'])[0].createdOn
+
         if (!initialDate) {
           throw Error
         }
-        console.log(initialDate)
-        writes.push({ eventId: circular.eventId, initialDate })
-      }
-      if (writes.length > 0 && TableName) {
-        const command = new BatchWriteCommand({
-          RequestItems: {
-            [TableName]: writes.map(({ eventId, initialDate }) => ({
-              PutRequest: {
-                Item: {
-                  synonymId: crypto.randomUUID(),
-                  eventId,
-                  slug: slug(eventId),
-                  initialDate,
-                },
-              },
-            })),
-          },
+        writes.push({
+          eventId: circular.eventId,
+          initialDate: parseInt(initialDate),
         })
-        await client.send(command)
       }
     }
-    const endTime = new Date()
-    console.log('... End SYNONYM backfill... ', endTime)
+    if (writes.length > 0 && TableName) {
+      const dedupedWrites = uniqBy(writes, 'eventId')
+
+      console.log(`Writing ${dedupedWrites.length} records`)
+
+      const command = new BatchWriteCommand({
+        RequestItems: {
+          [TableName]: dedupedWrites.map(({ eventId, initialDate }) => ({
+            PutRequest: {
+              Item: {
+                synonymId: crypto.randomUUID(),
+                eventId,
+                slug: slug(eventId),
+                initialDate,
+              },
+            },
+          })),
+        },
+      })
+
+      await client.send(command)
+    }
   }
+  const endTime = new Date()
+  console.log('... End SYNONYM backfill... ', endTime)
 }
 
 backfillSynonyms()
