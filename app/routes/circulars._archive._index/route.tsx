42e87d098c6619aaff7a46b06b7e3438eea465ad
Judy Racusin <judith.racusin@nasa.gov>
2025-03-31 14:19:08 -0400

    updated documentation for event groups

--- app/routes/circulars._archive._index/route.tsx
+++ app/routes/circulars._archive._index/route.tsx
@@ -336,10 +336,19 @@ export default function () {
         )}
       </ToolbarButtonGroup>
       <Hint id="searchHint">
-        Search for Circulars by submitter, subject, or body text (e.g. 'Fermi
-        GRB'). <br />
-        To navigate to a specific circular, enter the associated Circular ID
-        (e.g. 'gcn123', 'Circular 123', or '123').
+        {isGroupView ? (
+          <>
+            Search for Event Groups by event name (e.g. 'GRB 123456A',
+            'GRB123456A', '123456A'). <br />
+          </>
+        ) : (
+          <>
+            Search for Circulars by submitter, subject, or body text (e.g.
+            'Fermi GRB'). <br />
+            To navigate to a specific circular, enter the associated Circular ID
+            (e.g. 'gcn123', 'Circular 123', or '123').
+          </>
+        )}
       </Hint>
       {useFeature('CIRCULARS_LUCENE') && <LuceneAccordion />}
       {clean && (
