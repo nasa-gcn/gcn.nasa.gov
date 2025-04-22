0e28350cc446e295b20030bd1838f0c59147c660
tylerbarna <tylerpbarna@gmail.com>
2025-04-22 11:51:43 -0500

    remove reference to lucene

--- app/routes/circulars._archive._index/LuceneMenu.tsx
+++ app/routes/circulars._archive._index/LuceneMenu.tsx
@@ -14,7 +14,7 @@ export function LuceneAccordion() {
     <details>
       <summary>Advanced Search</summary>
       <p className="usa-paragraph">
-        To narrow the search results, use Lucene search syntax. This allows for
+        To narrow the search results, use advanced search syntax. This allows for
         specifying which circular field to search (submitter, subject, and/or
         body). For additional information, refer to{' '}
         <Link className="usa-link" to="/docs/circulars/archive#advanced-search">
