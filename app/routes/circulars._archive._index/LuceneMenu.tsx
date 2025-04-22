a8ca180c1f8a8992ac68109d5ae76e34715660d7
tylerbarna <tylerpbarna@gmail.com>
2025-04-22 10:06:43 -0500

    update docs description

--- app/routes/circulars._archive._index/LuceneMenu.tsx
+++ app/routes/circulars._archive._index/LuceneMenu.tsx
@@ -16,9 +16,9 @@ export function LuceneAccordion() {
       <p className="usa-paragraph">
         To narrow the search results, use Lucene search syntax. This allows for
         specifying which circular field to search (submitter, subject, and/or
-        body). Further documentation can be found{' '}
+        body). For additional information, refer to{' '}
         <Link className="usa-link" to="/docs/circulars/archive#advanced-search">
-          here
+          the advanced search documentation
         </Link>
         {'. '}
       </p>
