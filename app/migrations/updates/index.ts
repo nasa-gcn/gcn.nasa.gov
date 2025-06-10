bb39efc60658838535f5ce19334a6e61bfe0996d
Courey Elliott <coureyelliott@gmail.com>
2025-06-10 10:39:35 -0400

    adds more documentation

--- app/migrations/updates/index.ts
+++ app/migrations/updates/index.ts
@@ -7,6 +7,10 @@
  * 4. Run `npm run build`
  * 5. Run `node build/migrations/updates/index.mjs`
  * 6. Your file with circulars that need to be updated will be at the top level directory.
+ * 7. MAKE SURE TO REMOVE THE ARCHIVE FOLDER FROM THE APP WHEN YOU ARE DONE!
+ *    If you forget, it will break local dev in a very difficult to debug way. The container will start,
+ *    but will crash and be removed. It does this because the watcher tries to watch all the files in
+ *    the archive directory and it gets overwhelmed and OOMs.
  *
  * If you are using visual studio code, you can open the `event_id_updates.json` and make it more readable
  * by pushing command + shift + P and typing `Format Document`.
@@ -38,6 +42,7 @@ function processFiles() {
       const eventId = parseEventFromSubject(circular.subject)
       const previousEventId = circular.eventId ? circular.eventId : undefined
       if (eventId && eventId != previousEventId) {
+        // if it already has a GRB eventId, don't worry about updating it
         if (!previousEventId || previousEventId.split(' ')[0] != 'GRB') {
           results.push({
             circularId: circular.circularId,
