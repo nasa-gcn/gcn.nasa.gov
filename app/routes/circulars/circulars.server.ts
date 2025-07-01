11534613ee98e12ab479281e8499ace502618aa4
Courey Elliott <coureyelliott@gmail.com>
2025-07-01 13:50:07 -0400

    adds comment block for clarity

--- app/routes/circulars/circulars.server.ts
+++ app/routes/circulars/circulars.server.ts
@@ -24,7 +24,7 @@ import { dedent } from 'ts-dedent'
 
 import { type User, getUser } from '../_auth/user.server'
 import {
-  manageSynonymVersionUpdates,
+  manageSynonymOnVersionUpdates,
   tryInitSynonym,
 } from '../synonyms/synonyms.server'
 import {
@@ -388,7 +388,7 @@ export async function putVersion(
   const newVersionNumber =
     await circularVersionsAutoIncrement.put(newCircularVersion)
 
-  await manageSynonymVersionUpdates(
+  await manageSynonymOnVersionUpdates(
     newCircularVersion.createdOn,
     newCircularVersion.eventId,
     oldCircular.eventId
@@ -606,7 +606,7 @@ export async function approveChangeRequest(
 
   await Promise.all(promises)
 
-  await manageSynonymVersionUpdates(
+  await manageSynonymOnVersionUpdates(
     newVersion.createdOn,
     newVersion.eventId,
     circular.eventId
