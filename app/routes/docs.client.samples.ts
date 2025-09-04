234e346c476a5f3e0c03382a730a2bf897ff143e
Leo Singer <leo.singer@ligo.org>
2025-09-04 09:09:07 -0400

    Make redirect for moved page permanent (status 301)

--- app/routes/docs.client.samples.ts
+++ app/routes/docs.client.samples.ts
@@ -9,5 +9,5 @@ import { redirect } from '@remix-run/node'
 import type { LoaderFunction } from '@remix-run/node'
 
 export const loader: LoaderFunction = async () => {
-  return redirect('/docs/sample')
+  return redirect('/docs/sample', { status: 301 })
 }
