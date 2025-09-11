c158c7a432e382ed6cdbe378efb770d6a80307a3
Leo Singer <leo.singer@ligo.org>
2025-09-11 07:37:50 -0400

    Refactor user permissions hooks
    - Replace ambiguous abbervation "mod" with "moderator"
    - Replace ambiguous term "status" with "permission"
    - Remove redundant lookup in one loader

--- app/routes/circulars.$circularId/route.tsx
+++ app/routes/circulars.$circularId/route.tsx
@@ -7,8 +7,7 @@
  */
 import { type HeadersFunction, type LoaderFunctionArgs } from '@remix-run/node'
 
-import { getUser } from '../_auth/user.server'
-import { getVersions, moderatorGroup } from '../circulars/circulars.server'
+import { getVersions } from '../circulars/circulars.server'
 import { pickHeaders } from '~/lib/headers.server'
 import type { BreadcrumbHandle } from '~/root/Title'
 
@@ -31,9 +30,7 @@ export async function loader({
   const history = await getVersions(parseFloat(circularId))
   history.reverse() // Sort by descending version
 
-  const user = await getUser(request)
-  const userIsModerator = user?.groups.includes(moderatorGroup)
-  return { circularId, history, userIsModerator }
+  return { circularId, history }
 }
 
 export const headers: HeadersFunction = ({ loaderHeaders }) =>
