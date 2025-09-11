nasa-gcn/gcn.nasa.gov

commit 99efe29a10bf7c64fe530274ebf052f1fd09e2d8

Unify interface for Circulars corrections and edits
In the view route for individual Circulars, consolidate the
buttons for requesting a correction and editing a Circular into
a single button. If the user is a moderator, then they get the
option to either submit a correction or save as a new version
on the Circulars edit form itself.

This reduces clutter in the Circulars button bar and places the
decision over what kind of change to make in a more logical
place.

app/routes/circulars.edit.$circularId/CircularEditForm.tsx
@@ -114,7 +114,6 @@ export function CircularEditForm({
   searchString,
   defaultCreatedOnDateTime,
   defaultEventId: originalEventId,
-  intent,
 }: {
   formattedContributor: string
   circularId?: number
@@ -125,7 +124,6 @@ export function CircularEditForm({
   searchString: string
   defaultCreatedOnDateTime?: string
   defaultEventId?: string
-  intent: 'correction' | 'edit' | 'new'
 }) {
   let formSearchString = '?index'
   if (searchString) {
@@ -149,22 +147,7 @@ export function CircularEditForm({
   const bodyValid = bodyIsValid(body)
   const sending = Boolean(useNavigation().formData)
   const valid = subjectValid && bodyValid && submitterValid
-  let headerText, saveButtonText
 
-  switch (intent) {
-    case 'correction':
-      headerText = 'Correct'
-      saveButtonText = 'Submit'
-      break
-    case 'edit':
-      headerText = 'Edit'
-      saveButtonText = 'Update'
-      break
-    case 'new':
-      headerText = 'New'
-      saveButtonText = 'Send'
-      break
-  }
   const bodyPlaceholder = useBodyPlaceholder()
   const changesHaveBeenMade =
     body.trim() !== defaultBody.trim() ||
@@ -175,20 +158,11 @@ export function CircularEditForm({
 
   const userIsModerator = useModStatus()
 
+  const submitDisabled = sending || !valid || !changesHaveBeenMade
+
   return (
     <AstroDataContext.Provider value={{ rel: 'noopener', target: '_blank' }}>
-      <h1>{headerText} GCN Circular</h1>
-      {intent === 'correction' && (
-        <p className="usa-paragraph">
-          See{' '}
-          <Link to="/docs/circulars/corrections">
-            documentation on Circulars moderation
-          </Link>{' '}
-          for more information on corrections.
-        </p>
-      )}
       <Form method="POST" action={`/circulars${formSearchString}`}>
-        <input type="hidden" name="intent" value={intent} />
         <input type="hidden" name="circularId" value={circularId} />
         {circularId !== undefined && userIsModerator && (
           <>
@@ -255,7 +229,7 @@ export function CircularEditForm({
         >
           <CircularsKeywords />
         </CollapsableInfo>
-        {intent !== 'new' && (
+        {circularId !== undefined && (
           <>
             {linkEventId ? (
               <InputGroup className="maxw-full usa-input---not-editable">
@@ -338,13 +312,36 @@ export function CircularEditForm({
           >
             Back
           </Link>
-          <Button
-            disabled={sending || !valid || !changesHaveBeenMade}
-            type="submit"
-            value="save"
-          >
-            {saveButtonText}
-          </Button>
+          {circularId === undefined ? (
+            <Button
+              disabled={submitDisabled}
+              type="submit"
+              name="intent"
+              value="new"
+            >
+              New
+            </Button>
+          ) : (
+            <Button
+              disabled={submitDisabled}
+              type="submit"
+              name="intent"
+              value="correction"
+            >
+              Request Correction
+            </Button>
+          )}
+          {circularId !== undefined && userIsModerator && (
+            <Button
+              secondary
+              disabled={submitDisabled}
+              type="submit"
+              name="intent"
+              value="edit"
+            >
+              Save as New Version
+            </Button>
+          )}
           {sending && (
             <div className="padding-top-1 padding-bottom-1">
               <Spinner /> Sending...

app/routes/circulars.$circularId.($version)/route.tsx
@@ -128,26 +128,16 @@ export default function () {
             </Button>
           )}
         </ButtonGroup>
-        <ButtonGroup type="segmented">
-          {useSubmitterStatus() && (
-            <Link
-              className="usa-button usa-button--outline"
-              to={`/circulars/correction/${circularId}`}
-              title="Suggest a correction to this GCN Circular."
-            >
-              Request Correction
-            </Link>
-          )}
-          {useModStatus() && (
-            <Link
-              to={`/circulars/edit/${circularId}`}
-              className="usa-button usa-button--outline"
-              title="Edit this GCN Circular, creating a new version."
-            >
-              Edit
-            </Link>
-          )}
-        </ButtonGroup>
+        {(useSubmitterStatus() || useModStatus()) && (
+          <Link
+            to={`/circulars/edit/${circularId}`}
+            className="usa-button usa-button--outline"
+            title="Submit a correction to this GCN Circular."
+          >
+            <Icon.Edit role="presentation" className="margin-y-neg-2px" />
+            Edit
+          </Link>
+        )}
         {result?.history && result.history.length > 0 && (
           <CircularsHistory circular={circularId} history={result?.history} />
         )}

app/routes/circulars.correction.$circularId.tsx
@@ -1,61 +1,0 @@
-/*!
- * Copyright © 2023 United States Government as represented by the Administrator
- * of the National Aeronautics and Space Administration. No copyright is claimed
- * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-import type { SEOHandle } from '@nasa-gcn/remix-seo'
-import type { LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-
-import { getUser } from './_auth/user.server'
-import { CircularEditForm } from './circulars.edit.$circularId/CircularEditForm'
-import { formatAuthor } from './circulars/circulars.lib'
-import {
-  get,
-  getChangeRequest,
-  submitterGroup,
-} from './circulars/circulars.server'
-import type { BreadcrumbHandle } from '~/root/Title'
-
-export const handle: BreadcrumbHandle & SEOHandle = {
-  breadcrumb: 'Correction',
-  getSitemapEntries: () => null,
-}
-
-export async function loader({
-  params: { circularId },
-  request,
-}: LoaderFunctionArgs) {
-  if (!circularId) throw new Response(null, { status: 404 })
-
-  const user = await getUser(request)
-  if (!user?.groups.includes(submitterGroup))
-    throw new Response(null, { status: 403 })
-  let existingRequest
-  try {
-    existingRequest = await getChangeRequest(parseFloat(circularId), user.sub)
-  } catch (err) {
-    if (!(err instanceof Response && err.status === 404)) throw err
-  }
-  const circular = existingRequest ?? (await get(parseFloat(circularId)))
-  const defaultDateTime = new Date(circular.createdOn ?? 0).toISOString()
-
-  return {
-    formattedContributor: user ? formatAuthor(user) : '',
-    defaultBody: circular.body,
-    defaultSubject: circular.subject,
-    defaultFormat: circular.format,
-    circularId: circular.circularId,
-    defaultSubmitter: circular.submitter,
-    defaultEventId: circular.eventId,
-    defaultCreatedOnDateTime: defaultDateTime,
-    searchString: '',
-  }
-}
-
-export default function () {
-  const data = useLoaderData<typeof loader>()
-  return <CircularEditForm {...data} intent="correction" />
-}

app/routes/circulars.edit.$circularId/route.tsx
@@ -7,16 +7,21 @@
  */
 import type { SEOHandle } from '@nasa-gcn/remix-seo'
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
 
 import { getUser } from '../_auth/user.server'
 import { formatAuthor } from '../circulars/circulars.lib'
-import { get, moderatorGroup } from '../circulars/circulars.server'
+import {
+  get,
+  getChangeRequest,
+  moderatorGroup,
+  submitterGroup,
+} from '../circulars/circulars.server'
 import { CircularEditForm } from './CircularEditForm'
 import type { BreadcrumbHandle } from '~/root/Title'
 
 export const handle: BreadcrumbHandle & SEOHandle = {
-  breadcrumb: 'Edit',
+  breadcrumb: ({ params: { circularId } }) => `Edit - ${circularId}`,
   getSitemapEntries: () => null,
 }
 
@@ -25,11 +30,25 @@ export async function loader({
   request,
 }: LoaderFunctionArgs) {
   if (!circularId) throw new Response(null, { status: 404 })
+
   const user = await getUser(request)
-  if (!user?.groups.includes(moderatorGroup))
+  if (
+    !(
+      user?.groups.includes(moderatorGroup) ||
+      user?.groups.includes(submitterGroup)
+    )
+  )
     throw new Response(null, { status: 403 })
+
+  let circular
+  try {
+    circular = await getChangeRequest(parseFloat(circularId), user.sub)
+  } catch (err) {
+    if (!(err instanceof Response && err.status === 404)) throw err
+  }
+  circular ??= await get(parseFloat(circularId))
+
   const formattedContributor = formatAuthor(user)
-  const circular = await get(parseFloat(circularId))
   const defaultDateTime = new Date(circular.createdOn ?? 0).toISOString()
 
   return {
@@ -47,5 +66,17 @@ export async function loader({
 
 export default function () {
   const data = useLoaderData<typeof loader>()
-  return <CircularEditForm {...data} intent="edit" />
+  return (
+    <>
+      <h1>Edit GCN Circular</h1>
+      <p className="usa-paragraph">
+        See{' '}
+        <Link to="/docs/circulars/corrections">
+          documentation on Circulars moderation
+        </Link>{' '}
+        for more information on corrections.
+      </p>
+      <CircularEditForm {...data} />
+    </>
+  )
 }

app/routes/circulars.new.tsx
@@ -75,7 +75,8 @@ export default function () {
 
   return (
     <>
-      <CircularEditForm {...formDefaults} intent="new" />
+      <h1>New GCN Circular</h1>
+      <CircularEditForm {...formDefaults} />
       {isAuthorized || <ModalUnauthorized isAuthenticated={isAuthenticated} />}
     </>
   )

commit a75965f1cdbda39f6d9b3b57e5d737d7774c1f0e

Fix some tests that were broken by #3325

app/routes/circulars.edit.$circularId/CircularEditForm.tsx
@@ -317,7 +317,7 @@ export function CircularEditForm({
               name="intent"
               value="new"
             >
-              New
+              Send
             </Button>
           ) : (
             <Button

__playwright__/circulars/corrections.spec.ts
@@ -45,10 +45,10 @@ test.beforeEach(async ({ page }) => {
   await page.waitForSelector('#query')
 })
 
-test.describe('Circulars correction page', () => {
+test.describe('Circulars edit page', () => {
   test('populates all fields on load', async ({ page }) => {
     test.slow()
-    await page.goto(`/circulars/correction/${loadingTestsCircular.circularId}`)
+    await page.goto(`/circulars/edit/${loadingTestsCircular.circularId}`)
     await expect(page.locator('#submitter')).toHaveValue(
       loadingTestsCircular.submitter
     )

__playwright__/circulars/edit.spec.ts
@@ -81,7 +81,9 @@ test.describe('Circulars edit page', () => {
     await page.locator('#submitter').fill(editTestsCircular.submitter)
     await page.locator('#subject').fill(testSubject)
     await page.getByTestId('textarea').fill(editTestsCircular.body)
-    await page.getByRole('button', { name: 'Update' }).click({ timeout: 10000 })
+    await page
+      .getByRole('button', { name: 'Save as New Version' })
+      .click({ timeout: 10000 })
     await page.waitForURL('/circulars?index')
     await expect(
       page.getByRole('link', {