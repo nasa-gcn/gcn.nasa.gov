d8411dbeceb7283a4cffa374247e48b9884506b5
dakota002 <dakota.d@live.com>
2025-12-19 13:42:30 -0500

    Add test for updating eventId

--- __playwright__/circulars/edit.spec.ts
+++ __playwright__/circulars/edit.spec.ts
@@ -25,6 +25,17 @@ const editTestsCircular = {
   eventId: 'ZTF23aaoohpy',
 }
 
+const eventIdTestsCircular = {
+  subject: 'GRB 230812B: GMG - GRANDMA observations',
+  submittedHow: 'web',
+  bibcode: '2023GCN.34404....1M',
+  createdOn: 1691937605563,
+  circularId: 34404,
+  submitter: 'Jirong Mao at Yunnan Obs <jirongmao@mail.ynao.ac.cn>',
+  body: 'J. Mao, K.-X. Lu, J.-M. Bai (YNAO), S. Karpov (FZU), M. C. Coughlin (UMN), A. Ugarte Postigo, S. Antier (OCA), O. Pyhsna (Univ. KieV), Z. Vidadi (Shao) on behalf of the Yunnan observatories team and the GRANDMA team:\n\nWe observed the field of GRB 230812B (Lesage et al. GCN 34387; Scotton et al. GCN 34392; Page GCN 34394; Zheng & Filippenko GCN 34395; Lipunov et al. GCN 34396; Salgundi et al. GCN 34397; Ackley et al. GCN 34398; Xiong et al. GCN 34401; Casentini et al. 34002; Frederiks et al. 34404) by the GMG telescope in Yunnan observatories. The observation began from UT 13:34:22 August 13, 2023, about 18.5 hours from the trigger. We clearly observed the optical afterglow of R~19.9+/-0.1. The further observation is ongoing.\n',
+  eventId: 'GRB 230812B',
+}
+
 test.beforeEach(async ({ page }) => {
   await page.goto('/circulars')
   await page.waitForSelector('#query')
@@ -43,6 +54,42 @@ test.describe('Circulars edit page', () => {
     await expect(page.getByTestId('textarea')).toHaveValue(
       loadingTestsCircular.body
     )
+  })
+
+  test('correctly updates EventId on subject edits', async ({ page }) => {
+    test.slow()
+    const originalEventId = 'GRB 230812B'
+    const newEventId = 'GRB 230813B'
+    // Replace subject with an updated ID that the event will parse out
+    const testSubject = eventIdTestsCircular.subject.replace(
+      originalEventId,
+      newEventId
+    )
+    await page.goto(`/circulars/new/${eventIdTestsCircular.circularId}`)
+    await page.locator('#subject').fill(testSubject)
+    await page
+      .getByRole('button', { name: 'Save as New Version' })
+      .click({ timeout: 10000 })
+    await page.waitForURL('/circulars?index')
+    // Check for new event ID
+    await page.goto(`/circulars/${eventIdTestsCircular.circularId}`)
+    await expect(
+      page.getByRole('link', {
+        name: newEventId,
+      })
+    ).toBeVisible()
+    // Reset Event ID for other browser to continue check
+    await page.goto(`/circulars/new/${eventIdTestsCircular.circularId}`)
+    await page
+      .locator('#subject')
+      .fill(testSubject.replace(newEventId, originalEventId))
+    await page.waitForURL('/circulars?index')
+    await page.goto(`/circulars/${eventIdTestsCircular.circularId}`)
+    await expect(
+      page.getByRole('link', {
+        name: originalEventId,
+      })
+    ).toBeVisible()
   })
 
   test('submits expected values', async ({ page, browserName }) => {
