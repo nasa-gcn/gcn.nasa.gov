3c2cd81db2014fa8dc859b4469fac1085ce78eae
Judy Racusin <judith.racusin@nasa.gov>
2026-02-03 11:18:22 -0500

    Combine CHIME, DSA, IceCube

--- app/routes/missions.tsx
+++ app/routes/missions.tsx
@@ -27,16 +27,12 @@ export default function () {
               <NavLink key="calet" to="calet">
                 CALET
               </NavLink>,
-              useFeature('CHIME') && (
-                <NavLink key="chime" to="chime">
-                  CHIME
-                </NavLink>
-              ),
-              useFeature('DSA110') && (
-                <NavLink key="dsa110" to="dsa110">
-                  DSA-110
-                </NavLink>
-              ),
+              <NavLink key="chime" to="chime">
+                CHIME
+              </NavLink>,
+              <NavLink key="dsa110" to="dsa110">
+                DSA-110
+              </NavLink>,
               <NavLink key="einstein-probe" to="einstein-probe">
                 Einstein Probe
               </NavLink>,
