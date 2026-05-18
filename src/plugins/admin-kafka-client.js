969b6b8f6b54499fc9f9241723b14f1895c4fe92
dakota002 <dakota.d@live.com>
2026-05-18 16:49:58 -0400

    Copy usage from circulars producer

--- src/plugins/admin-kafka-client.js
+++ src/plugins/admin-kafka-client.js
@@ -8,7 +8,14 @@
 
 export const deploy = {
   start({ cloudformation }) {
-    cloudformation.Resources.GcnKafkaAdminUserPoolClient = {
+    const clientKey = 'GcnKafkaAdminUserPoolClient'
+    const env =
+      cloudformation.Resources.AnyCatchallHTTPLambda.Properties.Environment
+        .Variables
+    const user_pool_id = env.COGNITO_USER_POOL_ID
+    if (!user_pool_id)
+      throw new Error('Environment variable COGNITO_USER_POOL_ID must be set')
+    cloudformation.Resources[clientKey] = {
       Type: 'AWS::Cognito::UserPoolClient',
       Properties: {
         AllowedOAuthFlows: ['client_credentials'],
@@ -17,6 +24,12 @@ export const deploy = {
         GenerateSecret: true,
         UserPoolId: process.env.COGNITO_USER_POOL_ID,
       },
+    }
+    env.KAFKA_ADMIN_CLIENT_ID = {
+      'Fn::GetAtt': `${clientKey}.ClientId`,
+    }
+    env.KAFKA_ADMIN_CLIENT_SECRET = {
+      'Fn::GetAtt': `${clientKey}.ClientSecret`,
     }
   },
 }
