1893c7f16a8bc9ce8a37e27a6469848aec398bf6
Leo Singer <leo.singer@ligo.org>
2026-06-29 08:57:07 -0400

    Revert "Temporarily remove trace annotations"
    This reverts commit 7257bfa6056a6116dc467e21d480f7c26ab13b36.

--- server.ts
+++ server.ts
@@ -1,3 +1,16 @@
+import { trace } from '@opentelemetry/api'
+import {
+  ATTR_HTTP_REQUEST_HEADER,
+  ATTR_HTTP_REQUEST_METHOD,
+  ATTR_HTTP_RESPONSE_STATUS_CODE,
+  ATTR_NETWORK_PEER_ADDRESS,
+  ATTR_SERVER_ADDRESS,
+  ATTR_URL_FULL,
+  ATTR_URL_PATH,
+  ATTR_URL_QUERY,
+  ATTR_URL_SCHEME,
+  ATTR_USER_AGENT_ORIGINAL,
+} from '@opentelemetry/semantic-conventions'
 import { type RequestHandler, createRequestHandler } from '@remix-run/architect'
 import * as build from '@remix-run/dev/server-build'
 import { type APIGatewayProxyStructuredResultV2 } from 'aws-lambda'
@@ -13,6 +26,37 @@ const remixHandler = createRequestHandler({
 const { CDN_SECRET } = process.env
 
 export const handler: RequestHandler = async (event, ...args) => {
+  const span = trace.getActiveSpan()
+  if (span) {
+    const scheme = 'https'
+    const url = new URL(
+      event.rawPath,
+      `${scheme}://${event.requestContext.domainName}`
+    )
+    url.search = event.rawQueryString
+    span.setAttributes({
+      [ATTR_HTTP_REQUEST_METHOD]: event.requestContext.http.method,
+      [ATTR_SERVER_ADDRESS]: event.requestContext.domainName,
+      [ATTR_URL_FULL]: url.toString(),
+      [ATTR_URL_PATH]: event.rawPath,
+      [ATTR_URL_SCHEME]: scheme,
+      [ATTR_URL_QUERY]: event.rawQueryString,
+      [ATTR_NETWORK_PEER_ADDRESS]: event.requestContext.http.sourceIp,
+      [ATTR_USER_AGENT_ORIGINAL]: event.requestContext.http.userAgent,
+      ...Object.fromEntries(
+        Object.entries(event.headers).map(([key, value]) => [
+          ATTR_HTTP_REQUEST_HEADER(key),
+          value,
+        ])
+      ),
+      'aws.xray.annotations': [
+        ATTR_URL_FULL,
+        ATTR_HTTP_REQUEST_METHOD,
+        ATTR_HTTP_RESPONSE_STATUS_CODE,
+      ],
+    })
+  }
+
   if (CDN_SECRET && event.headers['x-cdn-secret'] !== CDN_SECRET)
     // FIXME: this shouldn't need to be a promise.
     // See https://github.com/DefinitelyTyped/DefinitelyTyped/pull/69466
@@ -28,6 +72,9 @@ export const handler: RequestHandler = async (event, ...args) => {
     ...args
   )) as APIGatewayProxyStructuredResultV2
   ;(result.headers ??= {})['Cache-Control'] ??= 'max-age=0, no-store'
+
+  if (span && result.statusCode !== undefined)
+    span.setAttribute(ATTR_HTTP_RESPONSE_STATUS_CODE, result.statusCode)
 
   return result
 }
