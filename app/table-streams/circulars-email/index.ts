842f26f7fc59cd8a95a6d92f89cd1730fb66312f
Dakota <dakota.c.dutko@nasa.gov>
2026-07-05 15:51:58 -0400

    Split the indexing and emailign into separate functions

--- app/table-streams/circulars-email/index.ts
+++ app/table-streams/circulars-email/index.ts
@@ -5,8 +5,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-import { search as getSearchClient } from '@nasa-gcn/architect-functions-search'
-import { errors } from '@opensearch-project/opensearch'
 import type { DynamoDBRecord } from 'aws-lambda'
 
 import { unmarshallTrigger } from '../utils'
@@ -14,39 +12,11 @@ import { createTriggerHandler } from '~/lib/lambdaTrigger.server'
 import type { Circular } from '~/routes/circulars/circulars.lib'
 import { send } from '~/routes/circulars/circulars.server'
 
-const index = 'circulars'
-
-async function removeIndex(id: number) {
-  const client = await getSearchClient()
-  try {
-    await client.delete({ index, id: id.toString() })
-  } catch (e) {
-    if (!(e instanceof errors.ResponseError && e.body.result === 'not_found')) {
-      throw e
-    }
-  }
-}
-
-async function putIndex(circular: Circular) {
-  const client = await getSearchClient()
-  await client.index({
-    index,
-    id: circular.circularId.toString(),
-    body: circular,
-  })
-}
-
 export const handler = createTriggerHandler(
   async ({ eventName, dynamodb }: DynamoDBRecord) => {
-    const id = unmarshallTrigger(dynamodb!.Keys).circularId as number
-    if (eventName === 'REMOVE') {
-      await removeIndex(id)
-    } /* (eventName === 'INSERT' || eventName === 'MODIFY') */ else {
+    if (eventName === 'INSERT') {
       const circular = unmarshallTrigger(dynamodb!.NewImage) as Circular
-      await putIndex(circular)
-      if (eventName === 'INSERT') {
-        await send(circular)
-      }
+      await send(circular)
     }
   }
 )
