02244fdfb3b45ca6e6602cf2e44f1d914949102d
Tyler Barna <57914086+tylerbarna@users.noreply.github.com>
2026-07-23 09:30:06 -0500

    Add event type to seed.json, add "are not" matcher for retractions (#3733)
    * Add eventType to seed.json
    
    * Update retraction keywords to match ~20 circulars in archive

--- app/routes/circulars/circulars.lib.ts
+++ app/routes/circulars/circulars.lib.ts
@@ -314,6 +314,7 @@ const eventTypeMatchers: Record<EventType, RegExp[]> = {
     /\bnot\s+a\s+(?:GRB|GW|FRB|SN|SGR|neutrino)\b/i,
     /\bprobably\s+not\s+a\b/i,
     /\bis\s+not\b/i,
+    /\bare not\b/i,
     /\bDisregard\b/i,
     /\bIgnore\b/i,
     /\bFalse Trigger\b/i,
