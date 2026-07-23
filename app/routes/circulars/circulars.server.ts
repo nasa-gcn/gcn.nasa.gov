2e3d52a7973a5bd25a6afcafe89885b9bd8ddddb
Dakota <dakota.c.dutko@nasa.gov>
2026-07-23 11:22:46 -0400

    Adds emailing functions for sending circulars by event

--- app/routes/circulars/circulars.server.ts
+++ app/routes/circulars/circulars.server.ts
@@ -6,12 +6,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { tables } from '@architect/functions'
-import type { DynamoDB } from '@aws-sdk/client-dynamodb'
-import {
-  type DynamoDBDocument,
-  paginateQuery,
-  paginateScan,
-} from '@aws-sdk/lib-dynamodb'
+import { type DynamoDB } from '@aws-sdk/client-dynamodb'
+import { type DynamoDBDocument, paginateScan } from '@aws-sdk/lib-dynamodb'
 import { search as getSearch } from '@nasa-gcn/architect-functions-search'
 import {
   DynamoDBAutoIncrement,
@@ -27,10 +23,10 @@ import {
   manageSynonymOnVersionUpdates,
   tryInitSynonym,
 } from '../synonyms/synonyms.server'
+import { send } from '../user.email/email_circulars.server'
 import {
   bodyIsValid,
   formatAuthor,
-  formatCircularText,
   formatIsValid,
   parseEventFromSubject,
   parseEventTypeFromSubject,
@@ -42,9 +38,8 @@ import type {
   CircularChangeRequestKeys,
   CircularMetadata,
 } from './circulars.lib'
-import { sendEmail, sendEmailBulk } from '~/lib/email.server'
+import { sendEmail } from '~/lib/email.server'
 import { feature, origin } from '~/lib/env.server'
-import { truncateJsonMaxBytes } from '~/lib/utils'
 import { closeZendeskTicket } from '~/lib/zendesk.server'
 
 // A type with certain keys required.
@@ -52,8 +47,6 @@ type Require<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 
 export const submitterGroup = 'gcn.nasa.gov/circular-submitter'
 export const moderatorGroup = 'gcn.nasa.gov/circular-moderator'
-
-const fromName = 'GCN Circulars'
 
 const getDynamoDBAutoIncrement = memoizee(
   async function () {
@@ -731,69 +724,4 @@ export function validateCircular({
   if (!bodyIsValid(body)) throw new Response('body is invalid', { status: 400 })
   if (!(format === undefined || formatIsValid(format)))
     throw new Response('format is invalid', { status: 400 })
-}
-
-async function getEmails() {
-  const db = await tables()
-  const client = db._doc as unknown as DynamoDBDocument
-  const TableName = db.name('circulars_subscriptions')
-  const pages = paginateScan(
-    { client },
-    { AttributesToGet: ['email'], TableName }
-  )
-  const emails: string[] = []
-  for await (const page of pages) {
-    const newEmails = page.Items?.map(({ email }) => email)
-    if (newEmails) emails.push(...newEmails)
-  }
-  return emails
-}
-
-async function getLegacyEmails() {
-  const db = await tables()
-  const client = db._doc as unknown as DynamoDBDocument
-  const TableName = db.name('legacy_users')
-  const pages = paginateQuery(
-    { client },
-    {
-      IndexName: 'legacyReceivers',
-      KeyConditionExpression: 'receive = :receive',
-      ExpressionAttributeValues: {
-        ':receive': 1,
-      },
-      ProjectionExpression: 'email',
-      TableName,
-    }
-  )
-  const emails: string[] = []
-  for await (const page of pages) {
-    const newEmails = page.Items?.map(({ email }) => email)
-    if (newEmails) emails.push(...newEmails)
-  }
-  return emails
-}
-
-export async function send(circular: Circular) {
-  const [emails, legacyEmails] = await Promise.all([
-    getEmails(),
-    getLegacyEmails(),
-  ])
-  const to = [...emails, ...legacyEmails]
-
-  // There is a limit of 262144 bytes for the Amazon SES API's TemplateData argument.
-  // See https://docs.aws.amazon.com/ses/latest/APIReference-V2/API_Template.html#SES-Type-Template-TemplateData
-  const { text, truncated } = truncateJsonMaxBytes(
-    formatCircularText(circular),
-    200_000
-  )
-
-  await sendEmailBulk({
-    fromName,
-    to,
-    subject: circular.subject,
-    body: `${text}${truncated ? '...\n\n\nThis message was truncated. View the full' : '\n\n\nView this'} GCN Circular online at ${origin}/circulars/${
-      circular.circularId
-    }.`,
-    topic: 'circulars',
-  })
 }
