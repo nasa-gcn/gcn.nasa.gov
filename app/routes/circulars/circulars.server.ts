996ac352bacf9a79cb1a5e1ddfeada2c12db2214
Dakota <dakota.c.dutko@nasa.gov>
2026-07-23 11:22:46 -0400

    Adds emailing functions for sending circulars by event behind feature flag

--- app/routes/circulars/circulars.server.ts
+++ app/routes/circulars/circulars.server.ts
@@ -43,6 +43,7 @@ import type {
   CircularChangeRequestKeys,
   CircularMetadata,
 } from './circulars.lib'
+import type { EmailSubscription } from '~/lib/email.server'
 import { sendEmail, sendEmailBulk } from '~/lib/email.server'
 import { feature, origin } from '~/lib/env.server'
 import { truncateJsonMaxBytes } from '~/lib/utils'
@@ -54,7 +55,16 @@ type Require<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 export const submitterGroup = 'gcn.nasa.gov/circular-submitter'
 export const moderatorGroup = 'gcn.nasa.gov/circular-moderator'
 
-const fromName = 'GCN Circulars'
+export type CircularsEventTypeSubscriptions = EmailSubscription & {
+  eventTypes: EventTypePreference[]
+}
+
+type EventTypePreference = {
+  uuid: string // Id linking to the EmailSubscription's UUID
+  eventType: string
+  ignore: boolean
+}
+const fromName = 'GCN Circualrs'
 
 const getDynamoDBAutoIncrement = memoizee(
   async function () {
@@ -797,6 +807,10 @@ export async function send(circular: Circular) {
   ])
   const to = [...emails, ...legacyEmails]
 
+  await sendBulkCircularsTo(circular, to)
+}
+
+async function sendBulkCircularsTo(circular: Circular, to: string[]) {
   // There is a limit of 262144 bytes for the Amazon SES API's TemplateData argument.
   // See https://docs.aws.amazon.com/ses/latest/APIReference-V2/API_Template.html#SES-Type-Template-TemplateData
   const { text, truncated } = truncateJsonMaxBytes(
@@ -813,4 +827,84 @@ export async function send(circular: Circular) {
     }.`,
     topic: 'circulars',
   })
+}
+
+export async function sendEventTypedEmails(circular: Circular) {
+  if (!circular.eventType) throw new Response(null, { status: 500 })
+  const to = await getEmailsForEventTypes(circular.eventType)
+  await sendBulkCircularsTo(circular, to)
+}
+
+export async function createEventBasedCircularEmailSubscription(
+  item: CircularsEventTypeSubscriptions
+) {
+  const created = Date.now()
+  const uuid = crypto.randomUUID()
+
+  const db = await tables()
+  const main = db.circular_eventType_email.put({
+    sub: item.sub,
+    uuid,
+    name: item.name,
+    created,
+    eventNames: item.eventTypes,
+    recipient: item.recipient,
+  })
+  const subscriptionPromises = item.eventTypes.map((prefernce) =>
+    db.circular_eventType_email_subscriptions.put({
+      uuid,
+      eventType: prefernce.eventType,
+      recipient: item.recipient,
+      ignore: prefernce.ignore,
+    })
+  )
+
+  await Promise.all([main, ...subscriptionPromises])
+}
+
+/**
+ * Given a list of eventType strings from a circular, retrieve addresses from users
+ * who have created notification sets that include this event type. If the type is
+ * included, but marked as ignored, that set will not be included.
+ * @param eventType
+ * @returns
+ */
+export async function getEmailsForEventTypes(
+  eventTypes: string[]
+): Promise<string[]> {
+  const db = await tables()
+  const client = db._doc as unknown as DynamoDBDocument
+  const TableName = db.name('circular_eventType_email_subscriptions')
+  const pages = paginateScan(
+    { client },
+    {
+      TableName,
+      ProjectionExpression: '#uuid, recipient, eventType, #ignore',
+      ExpressionAttributeNames: {
+        '#uuid': 'uuid',
+        '#ignore': 'ignore',
+      },
+    }
+  )
+
+  const preferences: (EventTypePreference & { recipient: string })[] = []
+  const ignoredUuids = new Set<string>()
+
+  for await (const page of pages) {
+    if (page.Items) {
+      const items = page.Items as (EventTypePreference & {
+        recipient: string
+      })[]
+      for (const item of items) {
+        preferences.push(item)
+        if (item.ignore && eventTypes.includes(item.eventType)) {
+          ignoredUuids.add(item.uuid)
+        }
+      }
+    }
+  }
+
+  return preferences
+    .filter((item) => !ignoredUuids.has(item.uuid))
+    .map((item) => item.recipient)
 }
