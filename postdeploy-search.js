d635b759bfbec917827ffde32629cd5586e0d8d5
Dakota <dakota.c.dutko@nasa.gov>
2026-07-24 13:50:31 -0400

    Use the postdeploy-search.js hook to put template indexes into the opensearch domain

---
+++ postdeploy-search.js
@@ -0,0 +1,50 @@
+const data = {
+  index_templates: [
+    {
+      name: 'circulars_template',
+      index_template: {
+        index_patterns: ['circular*'],
+        template: {
+          settings: {
+            index: {
+              number_of_shards: '1',
+              number_of_replicas: '1',
+            },
+          },
+          mappings: {},
+        },
+        priority: 1,
+        version: 1,
+        _meta: { description: 'Circulars template' },
+      },
+    },
+    {
+      name: 'synonyms_template',
+      index_template: {
+        index_patterns: ['synonym*'],
+        template: {
+          settings: {
+            index: {
+              number_of_shards: '1',
+              number_of_replicas: '1',
+            },
+          },
+          mappings: {},
+        },
+        priority: 1,
+        version: 1,
+        _meta: { description: 'Synonyms template' },
+      },
+    },
+  ],
+}
+
+export default async function (client) {
+  const promises = data.index_templates.map(({ name, index_template }) => {
+    return client.indices.putIndexTemplate({
+      name,
+      body: index_template,
+    })
+  })
+  await Promise.all(promises)
+}
