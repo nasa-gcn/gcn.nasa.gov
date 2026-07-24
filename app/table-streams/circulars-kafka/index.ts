nasa-gcn/gcn.nasa.gov

commit ad7ad57108efad7fe67b5a63bf1ace1434d76520

create kafka event topics (#3728)

app/table-streams/circulars-kafka/index.ts
@@ -26,6 +26,14 @@ export const handler = createTriggerHandler(
           ...cleanedCircular,
         })
       )
+      await Promise.all(
+        circular.eventType?.map((eventType) =>
+          send(
+            `gcn.circulars.${eventType.toLowerCase().replaceAll(' ', '_')}`,
+            JSON.stringify(circular)
+          )
+        ) ?? []
+      )
     }
   }
 )

app/routes/circulars/circulars.lib.ts
@@ -11,6 +11,7 @@ export interface CircularMetadata {
   circularId: number
   subject: string
   eventId?: string
+  eventType?: string[]
 }
 
 export type SubmittedHow = 'web' | 'email' | 'email-legacy' | 'api'

commit 2bc74672fc3e2acf4f083767a8dbbf1636edc369

Correct several bugs introduced by #3728
- Fix accidental disclosure of submitter's `sub` claim (security).
- Fix missing schema ID (correctness).
- Fix incorrect schema version lacking eventType field (correctness).
- Fix unnecessary serial evaluation of asynchronous I/O (performance).
- Fix unnecessary repeated encoding of JSON text (performance).

app/table-streams/circulars-kafka/index.ts
@@ -19,21 +19,21 @@ export const handler = createTriggerHandler(
     if (eventName === 'INSERT' || eventName === 'MODIFY') {
       const circular = unmarshallTrigger(dynamodb!.NewImage) as Circular
       const { sub, ...cleanedCircular } = circular
-      await send(
+
+      const value = JSON.stringify({
+        $schema: circularsJsonSchemaId,
+        ...cleanedCircular,
+      })
+
+      const topics = [
         'gcn.circulars',
-        JSON.stringify({
-          $schema: circularsJsonSchemaId,
-          ...cleanedCircular,
-        })
-      )
-      await Promise.all(
-        circular.eventType?.map((eventType) =>
-          send(
-            `gcn.circulars.${eventType.toLowerCase().replaceAll(' ', '_')}`,
-            JSON.stringify(circular)
-          )
-        ) ?? []
-      )
+        ...(circular.eventType ?? []).map(
+          (eventType) =>
+            `gcn.circulars.${eventType.toLowerCase().replaceAll(' ', '_')}`
+        ),
+      ]
+
+      await Promise.all(topics.map((topic) => send(topic, value)))
     }
   }
 )