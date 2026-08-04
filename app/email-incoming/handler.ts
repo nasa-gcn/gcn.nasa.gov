c08e292a0f7b9f265370d5dac0245cc3776f94d9
Courey Elliott <coureyelliott@gmail.com>
2026-08-04 15:34:17 -0400

    only fail when DMARC fails

--- app/email-incoming/handler.ts
+++ app/email-incoming/handler.ts
@@ -30,7 +30,7 @@ export function createEmailIncomingMessageHandler(
       throw new Error('Message failed virus check')
     if (message.receipt.action.type !== 'S3')
       throw new Error('Action type must be S3')
-    if (message.receipt.dmarcVerdict.status !== 'PASS')
+    if (message.receipt.dmarcVerdict.status === 'FAIL')
       throw new Error('Message failed DMARC validation')
 
     const response = await s3.send(
