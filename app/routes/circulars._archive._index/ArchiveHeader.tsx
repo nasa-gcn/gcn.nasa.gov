a7963a8261589dc736599fbcfbfee024cb486232
Owen Wu <owenwdd01@gmail.com>
2026-09-01 20:06:04 -0400

    Clear search when switching archive views

--- app/routes/circulars._archive._index/ArchiveHeader.tsx
+++ app/routes/circulars._archive._index/ArchiveHeader.tsx
@@ -136,7 +136,7 @@ export default function ArchiveHeader({
             id="query"
             name="query"
             type="search"
-            defaultValue={inputQuery}
+            value={inputQuery}
             placeholder={searchText}
             aria-describedby="searchHint"
             onChange={({ target: { form, value } }) => {
@@ -158,6 +158,9 @@ export default function ArchiveHeader({
             to={`/circulars?view=index&limit=${limit}`}
             preventScrollReset
             className={getSelection('index')}
+            onClick={() => {
+              setInputQuery('')
+            }}
           >
             <Icon.List role="presentation" />
             Circulars
@@ -166,6 +169,9 @@ export default function ArchiveHeader({
             to={`/circulars?view=group&limit=${limit}`}
             preventScrollReset
             className={getSelection('group')}
+            onClick={() => {
+              setInputQuery('')
+            }}
           >
             <Icon.ContentCopy role="presentation" />
             Events
