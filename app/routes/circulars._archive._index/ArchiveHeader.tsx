1d2dbf1d03b1ff9d6e917c83e313ad3d09968027
Tyler Barna <tylerpbarna@gmail.com>
2026-09-11 17:26:38 +0200

    fix spacing

--- app/routes/circulars._archive._index/ArchiveHeader.tsx
+++ app/routes/circulars._archive._index/ArchiveHeader.tsx
@@ -121,7 +121,7 @@ export default function ArchiveHeader({
   return (
     <>
       {eventTypeLabel && (
-        <BreadcrumbBar className="usa-breadcrumb--wrap desktop:margin-top-neg-8 margin-top-neg-4 padding-top-0 margin-bottom-neg-2">
+        <BreadcrumbBar className="usa-breadcrumb--wrap desktop:margin-top-neg-6 margin-top-neg-4 padding-top-0 margin-bottom-neg-3">
           <Breadcrumb>
             <BreadcrumbLink href="/circulars">GCN Circulars</BreadcrumbLink>
           </Breadcrumb>
